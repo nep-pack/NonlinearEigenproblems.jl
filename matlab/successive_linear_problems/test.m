f54b2fdd296d949ecd164151de8fc27608c549fd
gmele <gmele@kth.se>
2016-12-21 14:06:49 +0100

    Minor

---
+++ matlab/successive_linear_problems/test.m
@@ -0,0 +1,23 @@
+close all
+clear all
+clc
+
+n=10;
+A0=rand(n); A1=rand(n); I=eye(n);
+
+M =@(s) s*I+A0+A1*exp(-s);
+Mp=@(s) I-A1*exp(-s);
+
+
+s=0;
+err=[];
+for i=1:10
+    [V,D] = eig(M(s),Mp(s));
+    D=diag(D);
+    [~,jj] = min(abs(D-s));
+    s=s-D(jj);
+    v=V(:,jj);
+    err=[err norm(M(s)*v)];
+end
+
+semilogy(err,'--*')
