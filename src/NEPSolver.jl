5cf57273dbfa6af053304dc77cfc6e82dd44d4b7
Parikshit Upadhyaya <pup@kth.se>
2017-01-11 17:04:17 +0100

    Removed some run_test_augmented_newton.jl

--- src/NEPSolver.jl
+++ src/NEPSolver.jl
@@ -219,59 +219,7 @@ module NEPSolver
       throw(NoConvergenceException(λ,v,err,msg))
   end
 
-#############################################################################
-function aug_newton(nep::NEP,
-                    errmeasure::Function = default_errmeasure(nep::NEP,displaylevel),
-                    tolerance=eps()*100,
-                    maxit=10,
-                    λ=0,
-                    v=randn(nep.n),
-                    c=v,
-                    displaylevel=0)
-      
-      
-      try
-        err=errmeasure(λ,v)
-        if (displaylevel>0)
-            println("Iteration:",k," errmeasure:",err)
-        end
-        if (err< tolerance)
-            return (λ,v)
-        end
 
-        # Compute NEP matrix and derivative
-        M=nep.Md(λ)
-        Md=nep.Md(λ,1)
-
-        # Create jacobian
-        J=[M Md*v; c' 0];
-        F=[M*v; c'*v-1];
-
-        # Compute update
-        delta=-J\F;
-
-        # Update eigenvalue and eigvec
-        v=v+delta[1:nep.n];
-        λ=λ+delta[nep.n+1];
-
-
-      catch e
-          isa(e, Base.LinAlg.SingularException) || rethrow(e)
-          # This should not cast an error since it means that λ is
-          # already an eigenvalue.
-          if (displaylevel>0)
-              println("We have an exact eigenvalue.")
-          end
-          if (errmeasure(λ,v)>tolerance)
-              # We need to compute an eigvec somehow
-              v=(nep.Md(λ,0)+eps()*speye(nep.n))\v; # Requires matrix access
-              v=v/dot(c,v)
-          end
-          return (λ,v)
-      end
-      msg="Number of iterations exceeded. maxit=$(maxit)."
-      throw(NoConvergenceException(λ,v,err,msg))
-end
 
 
 #############################################################################
