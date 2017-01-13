01a338dc48461e7d0a5e80cad4e326a13fdcb181
Elias Jarlebring <eliasj@kth.se>
2017-01-13 16:08:14 +0100

    First commit with NEP representation with julia-style multiple dispatch

--- src/NEPCore.jl
+++ src/NEPCore.jl
@@ -1,8 +1,91 @@
 module NEPCore
 
 export NEP
+export NEP_new
+export DEP
+export size
 export NoConvergenceException
 export LinSolver
+export compute_Mder
+
+import Base.size  # Overload for nonlinear eigenvalue problems
+
+"""
+Determines if a method is defined for the concrete class.
+False will be returned if methodname is only defined for the
+abstract superclass of s.
+"""
+macro method_concretely_defined(methodname,s)
+    return :(length(methodswith(typeof($s), $methodname, false))>0)
+end
+
+
+abstract NEP_new;
+
+
+############################################
+# Default NEP functions
+#
+"""
+    compute_Mder(nep::NEP_new,λ::Number,i::Integer=0)    
+ Computes the ith derivative of NEP evaluated in λ\\
+ Usage:\\
+   compute_Mder(nep,λ)  # Evaluate NEP in λ
+"""
+function compute_Mder(nep::NEP_new,λ::Number,i::Integer=0)
+    return 3
+end
+
+# Overload size function. Note: All NEPs must have a field n.
+function size(nep::NEP_new,dim=-1)
+    if (dim==-1)
+        return (nep.n,nep.n)
+    else
+        return nep.n
+    end
+end
+
+
+
+"""
+    Delay eigenvalue problem
+"""
+type DEP <: NEP_new
+    n::Integer
+    A   # A can be a full or sparse matrix
+    tauv::Array{Float64,1}
+    function DEP(AA,tauv=[0,1.0])
+        n=size(AA[1],1)
+        this=new(n,AA,tauv);
+        return this;
+    end
+end
+"""
+    compute_Mder(nep::DEP,λ::Number,i::Integer=0)
+ Compute the ith derivative of a DEP
+"""
+function compute_Mder(nep::DEP,λ::Number,i::Integer=0)
+    local M,I;
+    if issparse(nep.A[1])
+        M=spzeros(nep.n,nep.n)
+        I=speye(nep.n,nep.n)
+    else
+        M=zeros(nep.n,nep.n)
+        I=eye(nep.n,nep.n)        
+    end
+    if i==0; M=-λ*I;  end
+    if i==1; M=-I; end
+    for j=1:size(nep.A,1)
+         M+=nep.A[j]*(exp(-nep.tauv[j]*λ)*(-nep.tauv[j])^i)
+    end
+    return M
+end
+
+
+
+
+
+### Old stuff
 
 type NEP
     n::Int
