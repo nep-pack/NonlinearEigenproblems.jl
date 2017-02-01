4326de95b33961427d925bbdabc7d8d8133d6bfd
Elias Jarlebring <eliasj@kth.se>
2017-02-01 18:55:28 +0100

    usage of n as param in nep_gallery(dep0) default small

--- src/run_test_IAR.jl
+++ src/run_test_IAR.jl
@@ -12,7 +12,7 @@ using PyPlot
 import NEPCore.compute_Mlincomb
 
 println("Load dep0")
-nep=nep_gallery("dep0")
+nep=nep_gallery("dep0",100)
 #nep=nep_gallery("pep0");
 
 function compute_Mlincomb(nep::DEP,λ::Number,V;a=ones(size(V,2)))
