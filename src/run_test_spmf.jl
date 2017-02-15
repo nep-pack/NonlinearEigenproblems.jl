1ec0a757f6d9e46203316165dd02454f7bb73d6a
Elias Jarlebring <eliasj@kth.se>
2017-02-15 10:53:38 +0100

    More tests

--- src/run_test_spmf.jl
+++ src/run_test_spmf.jl
@@ -10,7 +10,7 @@ n=5;
 srand(1)
 A0=randn(5,5);
 A1=randn(5,5);
-t=3
+t=3.0
 nep0=DEP([A0,A1],[0,t])
 
 minusop= S-> -S
@@ -40,5 +40,15 @@ res_inv(nep1,displaylevel=1,v=v0)
 n3=norm(compute_Mder(nep0,-1,5)-compute_Mder_from_MM(nep1,-1,5))
 println("Test 3: comparing nep0 with nep1 norm=",n3)
 λ=1+0.3im
-n4=norm(compute_Mder(nep0,λ,10)-compute_Mder_from_MM(nep0,λ,10))
+n4=norm(compute_Mder(nep0,λ,20)-compute_Mder_from_MM(nep0,λ,20))
 println("Test 4: comparing Mder with Mder_from_MM nep0. norm=",n4)
+
+
+n5=norm(compute_Mder(nep0,λ,0)-compute_Mder(nep1,λ,0))
+println("Test 5:  comparing Mder (i=0) nep0 and nep1. norm=",n5)
+
+n6=norm(compute_Mder(nep0,λ,1)-compute_Mder(nep1,λ,1))
+println("Test 6:  comparing Mder (i=1) nep0 and nep1. norm=",n6)
+
+n7=norm(compute_Mder(nep0,λ,15)-compute_Mder(nep1,λ,15))
+println("Test 7:  comparing Mder (i=15) nep0 and nep1. norm=",n7)
