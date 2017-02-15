378cf105046902c5d3cccdb7f9e66fc5637cd3e2
Elias Jarlebring <eliasj@kth.se>
2017-02-15 10:54:18 +0100

    Native NLEVP tests

--- src/run_test_nlevp.jl
+++ src/run_test_nlevp.jl
@@ -19,6 +19,9 @@ M=compute_Mder(nep,160^2+3im)
 Mp=compute_Mder(nep,160^2+3im,1)
 println("It worked.")
 
+print("Makeing it native NEP-PACK. ")
+nep1=nlevp_make_native(nep)
+println("It worked.")
 ## Run it
 
 # Approximate eigenvalue of gun problem from Liao, Bai, Lee, Ko
@@ -27,44 +30,51 @@ println("It worked.")
 v0=ones(size(nep,1))
 
 println("Running aug newton")
-λ,v=aug_newton(nep,λ=λ0,v=v0,
-               displaylevel=2,maxit=30,tolerance=1e-6)
-println("Found eigenvalue \sqrt{λ}=",sqrt(λ))
-println("Running newton")
-λ,v=newton(nep,λ=λ0,v=v0,
-               displaylevel=2,maxit=30,tolerance=1e-6)
-println("Found eigenvalue \sqrt{λ}=",sqrt(λ))
-
-println("Running MSLP")
-λ,v=mslp(nep,λ=λ0,
-         displaylevel=2,maxit=30,tolerance=1e-4,eigsolver="inv_it")
+@time λ,v=aug_newton(nep,λ=λ0,v=v0,
+                     displaylevel=2,maxit=30,tolerance=1e-6)
 println("Found eigenvalue \sqrt{λ}=",sqrt(λ))
 
 
-
-println("Running one step of MSLP and then aug_newton")
-λ1=λ0
-v1=copy(v0)
-try 
-    λ1,v1=mslp(nep,λ=λ0,
-               displaylevel=2,maxit=1,tolerance=-1,
-               eigsolver="inv_it")
-catch e
-    λ1=e.λ
-    v1=copy(e.v)
-end
-v1=v1/norm(v1)
-#println("sqrt(λ1)=",sqrt(λ1))
-λ,v=aug_newton(nep,λ=λ1,v=v1,c=v0,
-               displaylevel=2,maxit=30,tolerance=1e-6)
-
-println("Found eigenvalue \sqrt{λ}=",sqrt(λ))
+#println("Running newton")
+#λ,v=newton(nep1,λ=λ0,v=v0,
+#               displaylevel=2,maxit=30,tolerance=1e-6)
+#println("Found eigenvalue \sqrt{λ}=",sqrt(λ))
 
 
 
 
+#println("Running MSLP")
+#λ,v=mslp(nep,λ=λ0,
+#         displaylevel=2,maxit=30,tolerance=1e-4,eigsolver="inv_it")
+#println("Found eigenvalue \sqrt{λ}=",sqrt(λ))
+#
+#
+#
+#println("Running one step of MSLP and then aug_newton")
+#λ1=λ0
+#v1=copy(v0)
+#try 
+#    λ1,v1=mslp(nep,λ=λ0,
+#               displaylevel=2,maxit=1,tolerance=-1,
+#               eigsolver="inv_it")
+#catch e
+#    λ1=e.λ
+#    v1=copy(e.v)
+#end
+#v1=v1/norm(v1)
+##println("sqrt(λ1)=",sqrt(λ1))
+#λ,v=aug_newton(nep,λ=λ1,v=v1,c=v0,
+#               displaylevel=2,maxit=30,tolerance=1e-6)
+#
+#println("Found eigenvalue \sqrt{λ}=",sqrt(λ))
+#
+#
+#
+#
 println("Running resinv")
-λ,v=res_inv(nep,λ=λ0,
+@time λ,v=res_inv(nep1,λ=λ0,
                displaylevel=2,maxit=20,tolerance=1e-4)
 println("Found eigenvalue \sqrt{λ}=",sqrt(λ))
+
+
 
