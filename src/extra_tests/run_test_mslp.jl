53776031ebb7f7b17ccfa48735758e8afbefed32
Elias Jarlebring <eliasj@kth.se>
2017-02-24 13:53:56 +0100

    tests which are no longer actively used

---
+++ src/extra_tests/run_test_mslp.jl
@@ -0,0 +1,36 @@
+#  This is the first code in NEP-pack
+workspace()
+push!(LOAD_PATH, pwd())	# looks for modules in the current directory
+using NEPSolver
+using NEPCore
+using NEPTypes
+using LinSolvers
+using Gallery
+println("Test MSLP")
+
+#
+println("Running MSLP sparse dep (with default eigsolver)")
+nep=nep_gallery("dep0_sparse")
+λ,x =mslp(nep,displaylevel=1,eigsolvertype=SpEigSolver);
+println(compute_resnorm(nep,λ,x))
+
+# Buggy julia eigs generates error 
+# λ,x =mslp(nep,displaylevel=1,eigsolver="eigs");
+
+
+println("Running MSLP full dep (with eigsolver=eig)")
+nep=nep_gallery("dep0")
+λ,x =mslp(nep,displaylevel=1,eigsolvertype=DefaultEigSolver);
+println(compute_resnorm(nep,λ,x))
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/extra_tests/run_test_res_inv.jl
@@ -0,0 +1,35 @@
+#  This is the first code in NEP-pack
+workspace()
+push!(LOAD_PATH, pwd())	# looks for modules in the current directory
+using NEPSolver
+using NEPCore
+using NEPTypes
+using LinSolvers
+using Gallery
+
+println("Test Res-Inv")
+
+
+nep=nep_gallery("dep0")
+
+
+λ=NaN;
+x=NaN
+try
+    λ,x =res_inv(nep,displaylevel=1,linsolvertype=BackslashLinSolver);
+catch e
+    # Only catch NoConvergence 
+    isa(e, NoConvergenceException) || rethrow(e)  
+    println("No convergence because:"*e.msg)
+    # still access the approximations
+    λ=e.λ
+    x=e.v
+end
+println(compute_resnorm(nep,λ,x))
+
+
+
+
+
+
+
