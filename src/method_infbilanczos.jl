0c9ccd13b28c9c27b6b964b6f0e220f0c49b0c1f
Elias Jarlebring <eliasj@kth.se>
2017-05-30 15:43:41 +0200

    Initial code for infbilanczos. Take transposed NEP as input, cf #14

---
+++ src/method_infbilanczos.jl
@@ -0,0 +1,18 @@
+export infbilanczos
+"""
+    The Infinite Bi-Lanczos
+"""
+    function infbilanczos(
+        nep::NEP,
+        nept::NEP;  # Transposed NEP
+        maxit=30,
+        linsolvercreator::Function=default_linsolvercreator,
+        tol=1e-12,
+        Neig=maxit,                                  
+        errmeasure::Function = default_errmeasure(nep::NEP),
+        σ=0.0,
+        γ=1,
+        displaylevel=0)   
+       return 0;
+    end
+
