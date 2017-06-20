b29f55b00aede913f060b006e9b9f6a8b0189780
Parikshit Upadhyaya <pup@kth.se>
2017-06-20 16:29:00 +0200

    RFI_B:Added two sided RFI(Bordered version) as a method in method_rfi.jl

--- src/method_rfi.jl
+++ src/method_rfi.jl
@@ -1,4 +1,5 @@
 export rfi
+export rfi_b
 """
 Two-sided Rayleigh functional Iteration, as given as Algorithm 4 in  "Nonlinear Eigenvalue Problems: Newton-type Methods and
 Nonlinear Rayleigh Functionals", by Kathrin Schreiber.
@@ -47,6 +48,80 @@ function rfi(nep::NEP,
                 #S2: (T(λ_k)^H)y_(k+1) = (T'(λ_k)^H)v_(k)
                 y = lin_solve(linsolver_t,compute_Mlincomb(nept,λ,v,[1],1),tol = tolerance);
                 v = y/norm(y);
+
+                λ =compute_rf(nep,u;y=v);
+            end
+        catch e
+            isa(e, Base.LinAlg.SingularException) || rethrow(e)
+            # This should not cast an error since it means that λ is
+            # already an eigenvalue.
+            if (displaylevel>0)
+                println("We have an exact eigenvalue.")
+            end
+            if (errmeasure(λ,u)>tolerance)
+                # We need to compute an eigvec somehow
+                u=(nep.Md(λ,0)+eps(real(T))*speye(size(nep,1)))\u; # Requires matrix access
+                u=u/norm(u);
+            end
+            return (λ,u)
+        end
+        msg="Number of iterations exceeded. maxit=$(maxit)."
+        throw(NoConvergenceException(λ,u,err,msg))
+end
+
+"""
+Two-sided Rayleigh functional Iteration(Bordered version), as given as Algorithm 5 in  "Nonlinear Eigenvalue Problems: Newton-type Methods and
+Nonlinear Rayleigh Functionals", by Kathrin Schreiber.
+"""
+function rfi_b(nep::NEP,
+            nept::NEP;
+            errmeasure::Function=default_errmeasure(nep::NEP),
+            tolerance = eps()*1000,
+            maxit=100,
+            λ = 0.0+0.0im,
+            v = randn(nep.n),
+            u = randn(nep.n),
+            linsolvercreator::Function=default_linsolvercreator,
+            displaylevel=1)
+
+        err = Inf;
+
+        #Ensure type coherence
+        T = typeof(λ);
+        v = Array{T,1}(v);
+        u = Array{T,1}(u);
+        #Normalize v and u
+        v = v/norm(v);
+        u = u/norm(u);
+
+
+        try
+            for k=1:maxit
+                err = errmeasure(λ,u);
+
+                if(err < tolerance)
+                    return λ,u
+                end
+
+                if (displaylevel>0)
+                    @printf("Iteration: %2d errmeasure:%.18e \n",k, err);
+                end
+
+                #Construct C_k
+                C = [compute_Mder(nep,λ,0) compute_Mlincomb(nep,λ,u,[1],1);v'*compute_Mder(nep,λ,1) T(0.0)];
+                #local linsolver::LinSolver = BackslashLinSolver(C);
+
+                #C[s;μ] = -[T(λ)u;0]
+                #l1 = lin_solve(linsolver,-[compute_Mlincomb(nep,λ,u,[1],0);0],tol = tolerance);
+                l1 = C\-[compute_Mlincomb(nep,λ,u,[1],0);0];
+                s = l1[1:end-1];
+                u = (u+s)/norm(u+s);
+                
+                #C[t;ν] = -[T(λ)'v;0]
+                #l2 = lin_solve(linsolver,-[compute_Mlincomb(nept,λ,v,[1],0);0],tol = tolerance);
+                l2 = C\-[compute_Mlincomb(nept,λ,v,[1],0);0];
+                t = l2[1:end-1];
+                v = (v+t)/norm(v+t);
 
                 λ =compute_rf(nep,u;y=v);
             end
--- src/run_test_rfi.jl
+++ src/run_test_rfi.jl
@@ -27,12 +27,12 @@ println(λ)
 println("Resnorm:",compute_resnorm(nep,λ,x))
 
 #####################################################################################
-println("\nRunning two-sided RFI on pep0")
+println("\nRunning two-sided RFI(bordered) on pep0")
 nep=nep_gallery("pep0");
 At = [nep.A[1]',nep.A[2]',nep.A[3]'];
 nept=PEP(At);
 try
-    λ,x =rfi(nep,nept,displaylevel=1);
+    λ,x =rfi_b(nep,nept,displaylevel=1);
 catch e
     # Only catch NoConvergence
     isa(e, NoConvergenceException) || rethrow(e)
@@ -52,7 +52,7 @@ nep=nep_gallery("real_quadratic");
 At = [nep.A[1]',nep.A[2]',nep.A[3]'];
 nept=PEP(At);
 try
-    λ,x =rfi(nep,nept,displaylevel=1,λ=-50.0+0im);
+    λ,x =rfi(nep,nept,displaylevel=1,λ=-50.0+0.0im);
 catch e
     # Only catch NoConvergence
     isa(e, NoConvergenceException) || rethrow(e)
@@ -66,12 +66,12 @@ println(λ)
 println(compute_resnorm(nep,λ,x))
 
 #####################################################################################
-println("\nRunning two-sided RFI on dep_double")
+println("\nRunning two-sided RFI(bordered) on dep_double")
 nep=nep_gallery("dep_double");
 At = [nep.A[1]',nep.A[2]'];
 nept=DEP(At,nep.tauv);
 try
-    λ,x =rfi(nep,nept,displaylevel=1, λ=2.7994400031644808e-8 + 9.424777903529886im);
+    λ,x =rfi_b(nep,nept,displaylevel=1, λ=9.424777903529886im);
 catch e
     # Only catch NoConvergence
     isa(e, NoConvergenceException) || rethrow(e)
