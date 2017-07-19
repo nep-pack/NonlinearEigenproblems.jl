5de76d2670ef6f5a92ce135fe38b559d5bd34afd
Elias Jarlebring <eliasj@kth.se>
2017-07-19 10:38:25 +0200

    commenting

--- src/NEPCore.jl
+++ src/NEPCore.jl
@@ -47,14 +47,18 @@ abstract superclass of s.
     # Default NEP functions
     #
 
+    """
+    abstract NEP
+NEP represents a nonlinear eigenvalue problem
+"""
     abstract NEP;
 
 
     """
     compute_Mder(nep::NEP,λ::Number,i::Integer=0)
- Computes the ith derivative of NEP evaluated in λ\\
- Usage:\\
-   `compute_Mder(nep,λ)`  # Evaluate NEP in λ
+Computes the ith derivative of NEP evaluated in λ\\
+Usage:\\
+    `compute_Mder(nep,λ)`  # Evaluate NEP in λ
 """
     function compute_Mder(nep::NEP,λ::Number,i::Integer=0)
 
@@ -69,8 +73,8 @@ abstract superclass of s.
 
     """
     compute_Mlincomb(nep::NEP,λ::Number,V;a=ones(size(V,2)))
- Computes the linear combination of derivatives\\
- ``Σ_i a_i M^{(i)}(λ) v_i``
+Computes the linear combination of derivatives\\
+``Σ_i a_i M^{(i)}(λ) v_i``
 """
     function compute_Mlincomb(nep::NEP,λ::Number,V;a=ones(size(V,2)))
         #println(typeof(λ))
@@ -100,8 +104,7 @@ if you want efficiency (for aug_newton, IAR, ..).
 
     """
     compute_MM(nep::NEP,S,V)
- Computes the sum ``Σ_i M_i V f_i(S)`` for a NEP,\\
- where `S` and `V` are matrices.
+Computes the sum ``Σ_i M_i V f_i(S)`` for a NEP, where `S` and `V` are matrices.
 """
     function compute_MM(nep::NEP,S,V)
         error("No procedure to compute MM")
@@ -123,7 +126,12 @@ if you want efficiency (for aug_newton, IAR, ..).
         z=compute_MM(nep,S,V)[:,1];
         return a[1]*reshape(z,size(z,1))
     end
-
+    """
+    compute_Mlincomb_from_Mder(nep::NEP,λ::Number,V,a)
+The function computes Mlincomb by a call to to compute_Mder. This function is slow since it requires the construction of the matrices. 
+Usage normally by overloading:
+    compute_Mder(nep::MyNEP,λ::Number,V,a)=compute_Mlincomb_from_Mder(nep,λ,V,a)
+"""
     function compute_Mlincomb_from_Mder(nep::NEP,λ::Number,V,a)
         #println("Using poor-man's compute_Mder -> compute_Mlincomb")
         z=zeros(size(nep,1))
@@ -132,7 +140,8 @@ if you want efficiency (for aug_newton, IAR, ..).
         end
         return z
     end
-  """
+    """
+    compute_Mder_from_MM(nep::NEP,λ::Number,i::Integer=0)
 Computes the Mder function from MM using the fact that MM of
 a jordan block becomes derivatives
 """
@@ -144,9 +153,11 @@ a jordan block becomes derivatives
         W=compute_MM(nep,S,V)
         return W[1:n,1:n]
     end
-
+    """
+    compute_resnorm(nep::NEP,λ,v)
+Computes the residual norm ||M(λ)v|| of the nep.
+"""
     function compute_resnorm(nep::NEP,λ,v)
-        #println(typeof(λ))
         return norm(compute_Mlincomb(nep,λ,reshape(v,size(nep,1),1)))
     end
 
@@ -176,9 +187,9 @@ Computes the rayleigh functional of nep, i.e., computes λ such that
 
    """
     size(nep::NEP,dim=-1)
- Overloads the size functions for NEP.\\
- Size returns the size of the matrix defining the NEP.
- Note: All NEPs must implement this function.
+Overloads the size functions for NEP.\\
+Size returns the size of the matrix defining the NEP.
+Note: All NEPs must implement this function.
 """
     function size(nep::NEP,dim=-1)
         error("You need to provide an implementation of size for this NEP.")
@@ -188,10 +199,10 @@ Computes the rayleigh functional of nep, i.e., computes λ such that
 
    """
     issparse(nep::NEP)
- Overloads the issparse functions for NEP.\\
- Issparse returns `true` if the undelying type of the NEP is\\
- sparse, and `false` if it is dense.\\
- Default behaviour: Check sparsity of `compute_Mder(nep,0)`
+Overloads the issparse functions for NEP.\\
+Issparse returns `true` if the undelying type of the NEP is\\
+sparse, and `false` if it is dense.\\
+Default behaviour: Check sparsity of `compute_Mder(nep,0)`
 
 """
     function issparse(nep::NEP)
@@ -207,12 +218,12 @@ Computes the rayleigh functional of nep, i.e., computes λ such that
     #
 
     """
- ### NoConvergenceException
- Exeption thrown in case an iterative method does not converge\\
- `λ` = current eigenvalue(s) approximation\\
- `v` = current eigenvector(s) approximation\\
- `errmeasure` = The error measure of the current eigenpair(s) approximation\\
- `msg`
+    type NoConvergenceException
+Exeption thrown in case an iterative method does not converge\\
+`λ` = current eigenvalue(s) approximation\\
+`v` = current eigenvector(s) approximation\\
+`errmeasure` = The error measure of the current eigenpair(s) approximation\\
+`msg`
 """
     type NoConvergenceException <: Exception
         "current eigenvalue(s) approximation"
@@ -223,20 +234,24 @@ Computes the rayleigh functional of nep, i.e., computes λ such that
         errmeasure
         msg
     end
-    Base.showerror(io::IO, e::NoConvergenceException) = print(io, "No convergence.. '",e.msg,"' eigenvalue approx:",e.λ,", errmeasure:",e.errmeasure)
+    # Avoid dumping the huge eigenvectors in case of exception
+    Base.showerror(io::IO, e::NoConvergenceException) =
+        print(io, "No convergence: '",e.msg,"' ",
+              "eigenvalue approx:",e.λ,", errmeasure:",e.errmeasure)
 
 
 """ Returns a Jordan matrix """
     jordan_matrix(n::Integer,λ::Number)=jordan_matrix(Complex128,n,λ)
     function jordan_matrix{T<:Number}(::Type{T},n::Integer,λ::Number)
-        #println(typeof(λ)," ",Type{T})
         Z=T(λ)*eye(T,n)+diagm(ones(T,n-1),1);
     end
 
-
+    """
+    default_errmeasure(nep::NEP)
+The default way of measuring error (residual norm). 
+"""
     function default_errmeasure(nep::NEP)
         f=function (λ,v);
-            #println(typeof(λ))
             compute_resnorm(nep,λ,v)/norm(v)
         end
         return f
