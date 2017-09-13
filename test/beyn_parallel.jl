139b723265d11735e0c2521597ef17645ee07cce
Elias Jarlebring <eliasj@kth.se>
2017-09-13 14:14:19 +0200

    unit testing of different quadrature methods, cf #21

--- test/beyn_parallel.jl
+++ test/beyn_parallel.jl
@@ -22,17 +22,18 @@ using BenchmarkTools
 
 nep=nep_gallery("dep0",500)
 
+tol=1e-5
 println("Contour integral with quadgk")
-tt1=@timev λ1,v1=contour_beyn(nep,displaylevel=0,radius=0.3,k=3,quad_method=:quadgk,tolerance=1e-5,displaylevel=0)
+tt1=@timev λ1,v1=contour_beyn(nep,displaylevel=0,radius=0.3,k=3,quad_method=:quadgk,tolerance=tol,displaylevel=0)
 display(tt1)
 
 println("Contour integral with quadg")
-tt2=@timev λ2,v2=contour_beyn(nep,displaylevel=0,radius=0.3,k=3,quad_method=:quadg,tolerance=1e-5)
+tt2=@timev λ2,v2=contour_beyn(nep,displaylevel=0,radius=0.3,k=3,quad_method=:quadg,tolerance=tol)
 display(tt2)
 
 println("Contour integral with quadg_parallel")
 
-tt3=@timev λ3,v3=contour_beyn(nep,displaylevel=0,radius=0.3,k=3,quad_method=:quadg_parallel,tolerance=1e-5)
+tt3=@timev λ3,v3=contour_beyn(nep,displaylevel=0,radius=0.3,k=3,quad_method=:quadg_parallel,tolerance=tol)
 display(tt3)
 
 
@@ -40,4 +41,11 @@ println("Contour integral with quadgk (again for verification)")
 tt1b=@timev λ1,v1=contour_beyn(nep,displaylevel=0,radius=0.3,k=3,quad_method=:quadgk,tolerance=1e-5,displaylevel=0)
 display(tt1b)
 
+# Check distance between eigenvalue approximations taking
+# into account that they are not necessarily ordered
+# What we would really want is the Hausdorf distance (but it is
+# is not available in the julia core packages)
 
+@test norm(minimum(abs.(λ1*ones(1,3)-ones(3,1)*λ2'),1))<10*tol
+
+@test norm(minimum(abs.(λ3*ones(1,3)-ones(3,1)*λ2'),1))<10*tol
