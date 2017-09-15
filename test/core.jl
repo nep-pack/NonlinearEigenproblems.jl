8da00d7ed08e6edfe74b514914ed6f6cffdac102
Elias Jarlebring <eliasj@kth.se>
2017-09-15 16:04:38 +0200

    Initial unit tests for NEPCore #21

---
+++ test/core.jl
@@ -0,0 +1,34 @@
+# Tests for core functionality
+
+
+# Intended to be run from nep-pack/ directory or nep-pack/test directory
+workspace()
+push!(LOAD_PATH, pwd()*"/src")	
+
+
+using NEPSolver
+using NEPCore
+using NEPTypes
+using Gallery
+
+using Base.Test
+
+nep=nep_gallery("dep0");
+n=size(nep,1);
+
+## Mlincomb_tests
+V=randn(n,3);
+λ=0.3+1im;
+z1=compute_Mlincomb(nep,λ,V)
+z2=compute_Mlincomb(nep,λ,V,a=[1 1 1])
+@test z1==z2
+z3=compute_Mder(nep,λ,0)*V[:,1]+ compute_Mder(nep,λ,1)*V[:,2]+ compute_Mder(nep,λ,2)*V[:,3]
+@test z1 ≈ z3
+
+## Test Mlincomb starting counting with kth derivative
+z2=compute_Mlincomb(nep,λ,V,a=[0 0 1])
+z3=compute_Mder(nep,λ,2)*V[:,3]
+@test z2≈z3
+z4=compute_Mlincomb(nep,λ,V[:,3],[1], 2);
+@test z3≈z4
+#
