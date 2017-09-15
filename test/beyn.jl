e71a08036042df3990a1f734c290c4c06921b487
Elias Jarlebring <eliasj@kth.se>
2017-09-15 17:22:20 +0200

    minor testset

--- test/beyn.jl
+++ test/beyn.jl
@@ -14,38 +14,44 @@ using LinSolvers
 
 using Base.Test
 
-nep=nep_gallery("dep0")
+@testset "Beyn contour" begin
+    nep=nep_gallery("dep0")
+    @testset "disk at origin" begin
 
-println("Contour integral with σ=0,radius=1,k=2")
-λ,v=contour_beyn(nep,displaylevel=1,radius=1,k=2,quad_method=:quadgk)
+        println("Contour integral with σ=0,radius=1,k=2")
+        λ,v=contour_beyn(nep,displaylevel=1,radius=1,k=2,quad_method=:quadgk)
 
-println(λ[1])
-M=compute_Mder(nep,λ[1])
-minimum(svdvals(M))
-@test minimum(svdvals(M))<eps()*1000
+        println(λ[1])
+        M=compute_Mder(nep,λ[1])
+        minimum(svdvals(M))
+        @test minimum(svdvals(M))<eps()*1000
 
-println(λ[2])
-M=compute_Mder(nep,λ[2])
-@test minimum(svdvals(M))<eps()*1000
+        println(λ[2])
+        M=compute_Mder(nep,λ[2])
+        @test minimum(svdvals(M))<eps()*1000
 
+    end
+    @testset "quadgk" begin
+        println("shifted disk")
 
-println("Contour integral with σ=-0.2,radius=1.5,k=4")
+        λ,v=contour_beyn(nep,displaylevel=1,σ=-0.2,radius=1.5,k=4,quad_method=:quadgk)
 
-λ,v=contour_beyn(nep,displaylevel=1,σ=-0.2,radius=1.5,k=4,quad_method=:quadgk)
+        println(λ[1])
+        M=compute_Mder(nep,λ[1])
+        minimum(svdvals(M))
+        @test minimum(svdvals(M))<sqrt(eps())
 
-println(λ[1])
-M=compute_Mder(nep,λ[1])
-minimum(svdvals(M))
-@test minimum(svdvals(M))<sqrt(eps())
+        println(λ[2])
+        M=compute_Mder(nep,λ[2])
+        @test minimum(svdvals(M))<sqrt(eps())
 
-println(λ[2])
-M=compute_Mder(nep,λ[2])
-@test minimum(svdvals(M))<sqrt(eps())
+        println(λ[3])
+        M=compute_Mder(nep,λ[3])
+        @test minimum(svdvals(M))<sqrt(eps())
 
-println(λ[3])
-M=compute_Mder(nep,λ[3])
-@test minimum(svdvals(M))<sqrt(eps())
-
-println(λ[4])
-M=compute_Mder(nep,λ[4])
-@test minimum(svdvals(M))<sqrt(eps())
+        println(λ[4])
+        M=compute_Mder(nep,λ[4])
+        @test minimum(svdvals(M))<sqrt(eps())
+    end
+    
+end
