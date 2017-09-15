2b42e3e5fd1e5f40075365c8097cc2307b4df72c
Elias Jarlebring <eliasj@kth.se>
2017-09-15 16:57:15 +0200

    testsets in for gun

--- test/gun.jl
+++ test/gun.jl
@@ -15,53 +15,61 @@ using LinSolvers
 using Base.Test
 
 
+guntest=@testset "GUN (NLEVP interface)" begin
 
-nep_org=nlevp_gallery_import("gun","../nlevp3/");
-nep1=nlevp_make_native(nep_org);
+    nep_org=nlevp_gallery_import("gun","../nlevp3/");
+    nep1=nlevp_make_native(nep_org);
+    n=size(nep1,1);
+    tol=1e-11;
+    @testset "Running alg" begin
+        λ1,v1=quasinewton(nep1,λ=150^2+1im,v=ones(n),displaylevel=1,tolerance=tol,maxit=500);
+
+        v1=v1/norm(v1);
+
+        @test norm(compute_Mlincomb(nep1,λ1,v1))<tol*100
+        @test norm(compute_Mder(nep1,λ1)*v1)<tol*100
+
+        @test norm(compute_Mlincomb(nep_org,λ1,v1))<tol*100
+        @test norm(compute_Mder(nep_org,λ1)*v1)<tol*100
+
+    end
+    @testset "Compute derivatives" begin
+        # Test compute_Mlincomb:
+        λ=150^2+2im;
+        V=randn(n,2);    v=V[:,1];
+        z1=compute_Mlincomb(nep1,λ,V[:,1], [1.0], 1)
+        z2=compute_Mlincomb(nep_org,λ,V[:,1], [1.0], 1)
+        # Compare with divided difference
+        ee=1e-4
+        z3=(compute_Mlincomb(nep_org,λ+ee,v)-compute_Mlincomb(nep_org,λ-ee,v))/(2*ee);
+
+        @test norm(z3-z1)<(ee^2)*1000  # Compare native and FD
+        @test norm(z3-z2)<(ee^2)*1000 # Compare MATLAB and FD. This fails on NLEVP 3.0 due to a bug in gun.m in NLEVP 
+        @test norm(z1-z2)<sqrt(eps()) # Compare MATLAB and native.  This fails on NLEVP 3.0 due to a bug in gun.m in NLEVP 
+    end
+    
+
+    @testset "Compare SPMF and Native in alg" begin
+        # Check that two steps of quasinewton always gives the same result
+        λ_org=0
+        try
+            quasinewton(nep_org,maxit=2,λ=150^2+1im,v=ones(n),displaylevel=1)
+        catch e
+            λ_org=e.λ
+        end
 
 
-n=size(nep1,1);
-tol=1e-11;
-λ1,v1=quasinewton(nep1,λ=150^2+1im,v=ones(n),displaylevel=1,tolerance=tol,maxit=500);
+        λ1=0
+        try
+            quasinewton(nep1,maxit=2,λ=150^2+1im,v=ones(n),displaylevel=1)
+        catch e
+            λ1=e.λ
+        end
 
-v1=v1/norm(v1);
+        @test abs(λ1-λ_org)<sqrt(eps())
 
-@test norm(compute_Mlincomb(nep1,λ1,v1))<tol*100
-@test norm(compute_Mder(nep1,λ1)*v1)<tol*100
-
-@test norm(compute_Mlincomb(nep_org,λ1,v1))<tol*100
-@test norm(compute_Mder(nep_org,λ1)*v1)<tol*100
-
-# Test compute_Mlincomb:
-λ=150^2+2im;
-V=randn(n,2);
-z1=compute_Mlincomb(nep1,λ,V[:,1], [1.0], 1)
-z2=compute_Mlincomb(nep_org,λ,V[:,1], [1.0], 1)
-# Compare with divided difference
-ee=1e-4
-z3=(compute_Mlincomb(nep_org,λ+ee,v)-compute_Mlincomb(nep_org,λ-ee,v))/(2*ee);
-
-@test norm(z3-z1)<(ee^2)*100  # Compare native and FD
-@test norm(z3-z2)<(ee^2)*100 # Compare MATLAB and FD. This fails on NLEVP 3.0 due to a bug in gun.m in NLEVP 
-@test norm(z1-z2)<sqrt(eps()) # Compare MATLAB and native.  This fails on NLEVP 3.0 due to a bug in gun.m in NLEVP 
-
-
-# Check that two steps of quasinewton always gives the same result
-λ_org=0
-try
-    quasinewton(nep_org,maxit=2,λ=150^2+1im,v=ones(n),displaylevel=1)
-catch e
-    λ_org=e.λ
+    end
 end
+Base.Test.print_test_results(guntest)
 
-
-λ1=0
-try
-    quasinewton(nep1,maxit=2,λ=150^2+1im,v=ones(n),displaylevel=1)
-catch e
-    λ1=e.λ
-end
-
-@test abs(λ1-λ_org)<sqrt(eps())
-
-
+1;
