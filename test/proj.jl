2bf59bd81a59ec464567cb29d667ab813a39a9a2
Elias Jarlebring <eliasj@kth.se>
2017-09-18 10:14:56 +0200

    TOL adjustment. Passes all tests.

--- test/proj.jl
+++ test/proj.jl
@@ -54,7 +54,7 @@ projtest=@testset "Projected problems" begin
 
         n=size(nep,1);
         println("Running Newton Raphson")
-        tol=1e-8
+        tol=1e-12
         λ,x =newton(nep,maxit=30,λ=1+1im,tolerance=tol,
                     v=ones(n));
         # Check residual is small
