af7d4050d51c40db3a3eece15fc50aa219874aa2
Elias Jarlebring <eliasj@kth.se>
2017-09-20 10:20:55 +0200

    unit test for infbilanczos

--- test/infbilanczos.jl
+++ test/infbilanczos.jl
@@ -6,24 +6,66 @@ using NEPSolver
 using NEPCore
 using NEPTypes
 using Gallery
+using MAT
+using Base.Test
+using LinSolvers
+
 
 # explicit import needed for overloading
 # functions from packages
-import NEPCore.compute_Mlincomb
+# import NEPCore.compute_Mlincomb
 
-println("Load dep0")
+#println("Load dep0")
+#nep=nep_gallery("dep0",100)
+##nep=nep_gallery("pep0");
+#println("Transposing dep0");
+## Construct the transposed NEP
+#nept=DEP([nep.A[1]',nep.A[2]'],nep.tauv)
+#
+#
 
-nep=nep_gallery("dep0",100)
-#nep=nep_gallery("pep0");
-println("Transposing dep0");
-# Construct the transposed NEP
-nept=DEP([nep.A[1]',nep.A[2]'],nep.tauv)
+
+file=matopen("matlab/infbilanczos/qdep_infbilanczos.mat");
+A0=read(file,"A0");
+A1=read(file,"A1");
+close(file)
+
+tau=1;
+quadfun= S -> S^2;
+constfun= S -> eye(S);
+expfun= S -> expm(-tau*S);
 
 
+AA=[-speye(A0),A0,A1]
 
-m=20;
-λ,Q,err = infbilanczos(nep,nept,maxit=m,Neig=m,σ=2.0,γ=2);
-minimum(svdvals(compute_Mder(nep,λ)))
+fi=[quadfun,constfun,expfun]
+nep=SPMF_NEP(AA,fi)
+
+AAt=[-speye(A0),A0',A1']
+
+nept=SPMF_NEP(AAt,[quadfun,constfun,expfun])
+
+
+@testset "Infbilanczos" begin
+    m=30;
+    λ,Q,T = infbilanczos(nep,nept,maxit=m,Neig=m,σ=0);
+
+    # Produced with a different implementation
+    Tstar=[  -1.665117675679600   5.780562035399026                   0                   0
+             5.780562035399026  11.562308485001218 -18.839546184493731                   0
+             0  18.839546184493734 -15.213756300995186   9.788512505128466                                
+             0                   0   9.788512505128464  -0.120825360586847                                ] 
+    n0=size(Tstar,1);
+    @test norm(Tstar-T[1:n0,1:n0])<1e-10
+
+    println("Post-processing: computing eigenvectors")
+    thiserr=ones(m)*NaN
+    for i=1:length(λ)
+        v=compute_eigvec_from_eigval_lu(nep,λ[i],default_linsolvercreator);
+        thiserr[i]=norm(compute_Mlincomb(nep,λ[i],v));
+    end
+    @test length(find(thiserr .< 1e-7)) == 9
+end
 
 #
 ##for i=1:m
