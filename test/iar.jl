efd3e1fddd340a90504cf8e75acfd1a0412994bb
gmele <gmele@kth.se>
2017-09-21 11:34:47 +0200

    iar takes as input the orthogonalization method as originally suggested in #24. TODO: extend unit test.

--- test/iar.jl
+++ test/iar.jl
@@ -24,7 +24,7 @@ n=size(dep,1);
 IAR=@testset "IAR" begin
     @testset "accuracy eigenpairs" begin
 
-        (λ,Q)=iar(dep,σ=3,Neig=5,v=ones(n),
+        (λ,Q)=iar(dep,orthmethod=ModifiedGramSchmidt,σ=3,Neig=5,v=ones(n),
             displaylevel=0,maxit=100,tol=eps()*100)
 
         @testset "IAR eigval[$i]" for i in 1:length(λ)
@@ -32,46 +32,5 @@ IAR=@testset "IAR" begin
         end
     end
 
-    # NOW TEST DIFFERENT ORTHOGONALIZATION METHODS
-
-    # define "by hand" the double GS orthogonalization
-    function doubleGS(VV, vv, h)
-        h[:]=VV'*vv;    vv[:]=vv-VV*h;
-        g=VV'*vv;       vv[:]=vv-VV*g;
-        h[] = h[]+g[];  β=norm(vv);
-        vv[:]=vv/β
-        return β
-    end
-
-    orthmethod1 = (VV, vv, h) -> orthogonalize_and_normalize!(VV, vv, h, DGKS )
-    orthmethod2 = (VV, vv, h) -> doubleGS(VV, vv, h )
-    orthmethod3 = (VV, vv, h) -> orthogonalize_and_normalize!(VV, vv, h, ModifiedGramSchmidt )
-    orthmethod4 = (VV, vv, h) -> orthogonalize_and_normalize!(VV, vv, h, ClassicalGramSchmidt )
-
-
-
-    @testset "orthogonalization" begin
-        # testing different orthogonalization methods
-        @testset "DGKS" begin
-            (λ,Q,err,V)=iar(dep,σ=3,Neig=5,v=ones(n),displaylevel=0,maxit=100,tol=eps()*100,orthmethod=orthmethod1)
-            @test norm(V'*V-eye(size(V,2)))<1e-6
-         end
-
-         @testset "hand_written_doubleGS" begin
-             (λ,Q,err,V)=iar(dep,σ=3,Neig=5,v=ones(n),displaylevel=0,maxit=100,tol=eps()*100,orthmethod=orthmethod2)
-             @test norm(V'*V-eye(size(V,2)))<1e-6
-          end
-
-          @testset "ModifiedGramSchmidt" begin
-              (λ,Q,err,V)=iar(dep,σ=3,Neig=5,v=ones(n),displaylevel=0,maxit=100,tol=eps()*100,orthmethod=orthmethod3)
-              @test norm(V'*V-eye(size(V,2)))<1e-6
-           end
-
-           @testset "ClassicalGramSchmidt" begin
-               (λ,Q,err,V)=iar(dep,σ=3,Neig=5,v=ones(n),displaylevel=0,maxit=100,tol=eps()*100,orthmethod=orthmethod4)
-               @test norm(V'*V-eye(size(V,2)))<1e-6
-            end
-
-    end
 
 end
