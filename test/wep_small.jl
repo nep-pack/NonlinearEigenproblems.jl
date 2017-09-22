4cf78df038f0bed2fad8680819319cb0e0b16418
Elias Jarlebring <eliasj@kth.se>
2017-09-22 15:12:58 +0200

    workaround for compute_Mder not available

--- test/wep_small.jl
+++ test/wep_small.jl
@@ -11,29 +11,39 @@ using NEPCore
 using NEPTypes
 using Gallery
 using LinSolvers
+using Gallery.Waveguide
 
 using Base.Test
+import NEPCore.compute_Mder;
 
 
+nep=nep_gallery("waveguide", 3*5*7, 3*5*7,"JARLEBRING","FD","WEP")
 
-nep=nep_gallery("waveguide")
-n=size(nep,1);
-v0=vec(eye(n,1));
-λ0=-3-3im
-v0=compute_Mder(nep,λ0)\v0; v0=v0/norm(v0);
-v0=compute_Mder(nep,λ0)\v0; v0=v0/norm(v0);
-v0=compute_Mder(nep,λ0)\v0; v0=v0/norm(v0);
-v0=compute_Mder(nep,λ0)\v0; v0=v0/norm(v0);
-v0=compute_Mder(nep,λ0)\v0; v0=v0/norm(v0);
-
-n0=norm(compute_Mlincomb(nep,λ0,v0))
-myerrmeasure=(λ,v) -> (norm(compute_Mlincomb(nep,λ,v))/(n0*norm(v)))
-try 
-    λ,v= quasinewton(Complex128,nep,displaylevel=1,λ=λ0,v=v0,
-                     errmeasure=myerrmeasure,maxit=100,tol=1e-2
-                     )
-catch e
-    println(e.λ)
+# Waveguide compute_Mder(WEP_FD) (or direct solve) not implemented.
+# Workaround: use the spmf-version, but only for the linear solves.
+nep_spmf=nep_gallery("waveguide", 3*5*7, 3*5*7,"JARLEBRING","FD","SPMF")
+function compute_Mder(nep::WEP_FD,λ::Number,i::Integer)
+    return compute_Mder(nep_spmf,λ,i);
 end
+λstar=-2.690050173308845 - 3.1436003386330347im  # An exact eigenvalue
+n=size(nep,1);
 
-    
+@testset "WEP" begin
+    λ0=-3-3.5im
+    v0=ones(n); v0=v0/norm(v0);
+    #
+    n0=norm(compute_Mlincomb(nep,λ0,v0))
+    #myerrmeasure=(λ,v) -> (norm(compute_Mlincomb(nep,λ,v))/(n0*norm(v)))
+    myerrmeasure=(λ,v) -> abs(λ-λstar) # Use eigenvalue error as errmeasure
+
+    λ,v=resinv(Complex128,nep,displaylevel=1,λ=λ0,v=v0,
+               errmeasure=myerrmeasure,tol=1e-12)
+
+
+    @test  norm(compute_Mlincomb(nep,λ,v))/norm(v)  < 1e-10
+    λ,v=quasinewton(Complex128,nep,displaylevel=1,λ=λ0,v=v0,
+                    errmeasure=myerrmeasure,tol=1e-12)
+
+    @test  norm(compute_Mlincomb(nep,λ,v))/norm(v)  < 1e-10
+
+end
