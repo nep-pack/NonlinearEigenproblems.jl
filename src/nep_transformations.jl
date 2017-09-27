3e9b2216c3614808e30180398359784c303a6502
Elias Jarlebring <eliasj@kth.se>
2017-09-27 21:06:35 +0200

    renaming of types and make them hidden for user

--- src/nep_transformations.jl
+++ src/nep_transformations.jl
@@ -1,23 +1,21 @@
 # Transformations between types
-export ShiftScaleNEP
-export MobiusTransformNEP
 export transform_to_pep
 export shift_and_scale
 export mobius_transform
 
-   """
-    type ShiftScaleNEP <: NEP
+"""
+    type ShiftScaledNEP <: NEP
     ShiftScaleNEP(orgnep::NEP[,shift=0][,scale=1])
 
 Transforms a nep (orgnep) M(λ)v to a new nep T(λ)=M(scale*λ+shift). This can be used if the method does not have an easy implementation of shift and scaling. Usage of this transformation can slow down the algorithm.
 
 """
 
-type ShiftScaleNEP <: NEP
+type ShiftScaledNEP <: NEP
     shift::Number
     scale::Number
     orgnep::NEP
-    function ShiftScaleNEP(orgnep;shift=0,scale=1)
+    function ShiftScaledNEP(orgnep;shift=0,scale=1)
         return new(shift,scale,orgnep)
     end
 end
@@ -36,7 +34,7 @@ julia> norm(compute_Mder(nep0,α*(4+4im)+σ)-compute_Mder(nep1,4+4im))
 ```
 """
 function shift_and_scale(orgnep::NEP;shift=0,scale=1)
-    return ShiftScaleNEP(orgnep,shift=shift,scale=scale);
+    return ShiftScaledNEP(orgnep,shift=shift,scale=scale);
 end
 
 # Native implementation for PEPs by transformation of the coefficients. This is not necessarily fast.
@@ -73,26 +71,26 @@ end
 
 
 # Size and issparse implemented by delegation
-function size(nep::ShiftScaleNEP,dim=-1)
+function size(nep::ShiftScaledNEP,dim=-1)
     return size(nep.orgnep,dim)
 end
-function issparse(nep::ShiftScaleNEP)
+function issparse(nep::ShiftScaledNEP)
     return issparse(nep.orgnep)
 end
 
 
-function compute_Mder(nep::ShiftScaleNEP,λ::Number,i::Integer=0)
+function compute_Mder(nep::ShiftScaledNEP,λ::Number,i::Integer=0)
     # Just scale the orgnep call. Chain rule for differentiation.
     return (nep.scale^i)*compute_Mder(nep.orgnep,nep.scale*λ+nep.shift,i);
 end
 
-function compute_MM(nep::ShiftScaleNEP,S,V)
+function compute_MM(nep::ShiftScaledNEP,S,V)
     # Just call orgnep with a different S
     return compute_MM(nep.orgnep,S*nep.scale+eye(S)*nep.shift,V)
 end
 
 
-function compute_Mlincomb(nep::ShiftScaleNEP,λ::Number,V;a=ones(size(V,2)))
+function compute_Mlincomb(nep::ShiftScaledNEP,λ::Number,V;a=ones(size(V,2)))
     # Multiply the coefficient matrix V with a diagonal matrix
     # corresponding to the chain rule
     p=size(V,2);
@@ -109,7 +107,7 @@ Transforms a nep (orgnep) M(λ)v to a new nep T(λ)=M((a*λ+b)/(c*λ+d)). Usage 
 
 """
 function mobius_transform(orgnep::NEP;a=1,b=0,c=0,d=1)
-    return MobiusTransformNEP(orgnep;a=a,b=b,c=c,d=d)    
+    return MobiusTransformedNEP(orgnep;a=a,b=b,c=c,d=d)    
 end
 
 function mobius_transform(orgnep::SPMF_NEP;a=1,b=0,c=0,d=1)
@@ -131,36 +129,36 @@ end
 Hidden type representing a transformed NEP
 """
 
-type MobiusTransformNEP <: NEP
+type MobiusTransformedNEP <: NEP
     a::Number
     b::Number
     c::Number
     d::Number
     orgnep::NEP
-    function MobiusTransformNEP(orgnep;a=1,b=0,c=0,d=1)
+    function MobiusTransformedNEP(orgnep;a=1,b=0,c=0,d=1)
         return new(a,b,c,d,orgnep)
     end
 end
 
 # Size and issparse implemented by delegation
-function size(nep::MobiusTransformNEP,dim=-1)
+function size(nep::MobiusTransformedNEP,dim=-1)
     return size(nep.orgnep,dim)
 end
-function issparse(nep::MobiusTransformNEP)
+function issparse(nep::MobiusTransformedNEP)
     return issparse(nep.orgnep)
 end
 
-function compute_Mder(nep::MobiusTransformNEP,λ::Number,i::Integer=0)
+function compute_Mder(nep::MobiusTransformedNEP,λ::Number,i::Integer=0)
     # I did not found a better way
     return compute_Mder_from_MM(nep,λ,i);
 end
 
-function compute_MM(nep::MobiusTransformNEP,S,V)
+function compute_MM(nep::MobiusTransformedNEP,S,V)
     # Just call orgnep with a different S
     return compute_MM(nep.orgnep,(nep.a*S+nep.b*eye(S))/(nep.c*S+nep.d*eye(S)),V)
 end
 
-function compute_Mlincomb(nep::MobiusTransformNEP,λ::Number,V;a=ones(size(V,2)))
+function compute_Mlincomb(nep::MobiusTransformedNEP,λ::Number,V;a=ones(size(V,2)))
     # I did not found a better way
     return compute_Mlincomb_from_MM(nep,λ,V,a)
 end
