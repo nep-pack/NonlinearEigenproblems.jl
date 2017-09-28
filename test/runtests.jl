f8d9fc32fc42c48c8f2a7478f5547c32af4de92b
Emil Ringh <eringh@kth.se>
2017-09-28 15:25:44 +0200

    UnitTesting: runtests.jl now defines a complete test-set and crawls and runs the folder test for all .jl-files that are not in the ban-list. #21

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,28 @@
+##################
+# Run's all test #
+##################
+using Base.Test
 
-# Run's all test
+# Add tests below if you wish that they are not run together with all tests
+tests_not_to_run = ["runtests.jl", "Beyn_parallel.jl"]::Array{String,1}
 
-include("newton_test.jl")
+
+base_path = string(@__DIR__)
+file_list = readdir(base_path)
+
+@testset "All tests" begin
+    for i = 1:length(file_list)
+        file = file_list[i]
+        upp_file = uppercase(file) #OBS: Make no case difference
+        if endswith(upp_file, ".JL")
+            is_in_norun_list = false
+            for k = 1:length(tests_not_to_run)
+                is_in_norun_list = is_in_norun_list || (upp_file == uppercase(tests_not_to_run[k])) #OBS: Make no case difference
+            end
+            if(!is_in_norun_list)
+              include(base_path *"/" * file)
+            end
+        end
+    end
+end
+
