949aa3ec2deb7ada20713c803fdc1645bbbd8829
eringh <eringh@kth.se>
2017-10-30 20:35:25 +0100

    Added basic functionality test for Resinv and made sub-tests in Newton-test. #21 #26

--- test/newton_test.jl
+++ test/newton_test.jl
@@ -12,7 +12,9 @@ using LinSolvers
 using Base.Test
 
 nep=nep_gallery("dep0")
+@testset "Newton iterations" begin
 
+@testset "Newton and AugNewton" begin
 # newton and augnewton are equivalent, therefore I expect them
 # to generate identical results
 λ1,x1 =newton(nep,displaylevel=0,v=ones(size(nep,1)),λ=0,tol=eps()*10);
@@ -26,11 +28,14 @@ r2=compute_resnorm(nep,λ2,x2)
 
 @test r1 < eps()*100
 @test r2 < eps()*100
+end
+
+@testset "Newton QR" begin
 
 #Run derivative test for left and right eigenvectors returned by newtonqr
 λ3,x3,y3 =  newtonqr(nep, λ=0, v=ones(size(nep,1)), displaylevel=0,tol=eps()*10);
 
-println("\nTesting formula for derivative (with left and right eigvecs\n")
+#println("\nTesting formula for derivative (with left and right eigvecs)\n")
 tau=1;
 n=size(nep,1);
 Mλ=-eye(n)-tau*nep.A[2]*exp(-tau*λ3)
@@ -48,3 +53,14 @@ nep.tauv[2]=nep.tauv[2]+δ
 λp_approx=(λ3δ-λ3)/δ;
 
 @test abs(λp-λp_approx)< (δ*10)
+end
+
+@testset "Resinv" begin
+# basic functionality of resinv
+λ4,x4 =  resinv(nep, λ=0, v=ones(size(nep,1)), displaylevel=0,tol=eps()*10);
+r4=compute_resnorm(nep,λ4,x4)
+
+@test r4 < eps()*100
+end
+
+end
