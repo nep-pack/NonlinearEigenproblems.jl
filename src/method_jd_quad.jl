13f0d4a06544ffc2e3d8fcb960ab98475cb3a167
eringh <eringh@kth.se>
2017-11-06 23:13:03 +0100

    JD: Updated to projectable_NEP. Still needs to implement the eigesolver for the general SPMF-case. #26

--- src/method_jd_quad.jl
+++ src/method_jd_quad.jl
@@ -27,51 +27,43 @@ function jd_quad{T}(::Type{T},
     V::Array{T,2} = zeros(T,size(nep,1),1)
     V[:,1] = v
     u::Array{T,1} = v
-    theta::T = λ
 
    #loop...
 
    for kk=1:maxit
 
-    	err=errmeasure(theta,u)
+    	err=errmeasure(λ,u)
 
     if (displaylevel>0)
       println("Iteration:",k," errmeasure:",err)
    	end
   	if (err< tolerance)
-      	return (theta,u)
+      	return (λ,u)
     end
 
-	# Projected matrices
-	Ap0 = 	V'*(nep.A[1]*V)
-	Ap1 = 	V'*(nep.A[2]*V)
-	Ap2 = 	V'*(nep.A[3]*V)
+    # Projected matrices
+    set_projectmatrices!(proj_nep,V,V)
 
+    # Create the projected NEP problem and
+    # find the eigenvalue with smallest absolute value
+    Dc,Vc = jd_inner_eig_solver(typeof(nep), T, proj_nep, eigsolvertype)
+    c = sortperm(abs.(Dc))
 
-	# Create the projected polynomial problem and
-	# find the eigenvalue with smallest absolute value
-
-	Ap = [Ap0,Ap1,Ap2]
-	pep_temp = PEP(Ap)
-	Dc,Vc = polyeig(T,pep_temp,DefaultEigSolver)
-	c = sortperm(abs.(Dc))
-
-
-	theta = Dc[c[1]]
+	λ = Dc[c[1]]
 	s = Vc[:,c[1]]
 	s = s/norm(s)
 
 	u = V*s
 
-    Mdu::Array{T,1} = compute_Mlincomb(nep,theta,u,[1],1)
+    Mdu::Array{T,1} = compute_Mlincomb(nep,λ,u,[1],1)
     P1 = eye(T,n) - Mdu*u'/(u'*Mdu)
     P2 = eye(T,n) - u*u'
 
-    r = compute_Mlincomb(nep,theta,u)
+    r = compute_Mlincomb(nep,λ,u)
 
     MP2 = zeros(T,size(nep,1),size(P2,2))
     for ii = 1:size(P2,2)
-        MP2[:,ii] = compute_Mlincomb(nep,theta,P2[:,ii],[1],0)
+        MP2[:,ii] = compute_Mlincomb(nep,λ,P2[:,ii],[1],0)
     end
     X = P1*MP2
 
@@ -91,16 +83,25 @@ function jd_quad{T}(::Type{T},
 	end
 	v = t/norm(t);
 
-
 	# Update the search space V
     V = [V v]
 
-
-	println("Iteration: ",kk," norm of residual:", compute_resnorm(nep,theta,u))
+	println("Iteration: ",kk," norm of residual:", compute_resnorm(nep,λ,u))
 
   end
 
-    err=errmeasure(theta,u)
+    err=errmeasure(λ,u)
     msg="Number of iterations exceeded. maxit=$(maxit)."
     throw(NoConvergenceException(λ,v,err,msg))
+end
+
+
+function jd_inner_eig_solver(::Type{T_orig_nep}, T, proj_nep, eigsolvertype) where {T_orig_nep <: ProjectableNEP}
+    error("NOT IMPLEMTED YET") #TODO: Implement this!
+end
+
+function jd_inner_eig_solver(::Type{T_orig_nep}, T, proj_nep, eigsolvertype) where {T_orig_nep <: PEP}
+    pep_temp = PEP(get_Av(proj_nep))
+    Dc,Vc = polyeig(T,pep_temp,eigsolvertype)
+    return Dc,Vc
 end
--- src/run_test_jd_quad.jl
+++ src/run_test_jd_quad.jl
@@ -14,21 +14,13 @@ using Gallery
 
 
 
-n=100; # mat size
-	   
-A0 = randn(n,n);
-A1 = randn(n,n);
-A2 = randn(n,n);
-A = [A0,A1,A2];
-nep=PEP(A);
+nep = nep_gallery("pep0",100)
 
 
-
-λ,u =jd_quad(nep,tolerance=1e-5,maxit=80);
+λ,u =jd_quad(nep,tolerance=1e-10,maxit=80);
 
 println("\n Resnorm of computed solution: ",compute_resnorm(nep,λ,u))
 println("\n Smallest eigevalue found: \n λ: ",λ)
-
 
 
 Dc,Vc = polyeig(nep,DefaultEigSolver);
@@ -37,7 +29,7 @@ println("\n 5 smallest eigenvalues according to the absolute values: \n", Dc[c[1
 
 
 println("\n Testing in only Complex64 precision (32 bit in real and 32 bit in imaginary)")
-λ,u =jd_quad(Complex64,nep,tolerance=1e-5,maxit=80);
+λ,u =jd_quad(Complex64,nep,tolerance=5e-4,maxit=80);
 
 println("\n Resnorm of computed solution: ",compute_resnorm(nep,λ,u))
 println("\n Smallest eigevalue found: \n λ: ",λ)
@@ -45,7 +37,5 @@ println("\n Smallest eigevalue found: \n λ: ",λ)
 
 
 Dc,Vc = polyeig(Complex64,nep,DefaultEigSolver);
-c = sortperm(abs(Dc));
+c = sortperm(abs.(Dc));
 println("\n 5 smallest eigenvalues according to the absolute values: \n", Dc[c[1:5]]);
-
-
