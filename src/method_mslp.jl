19bee3a217a8eb1e478f71f5b1ee2c902f4d0d5e
Elias Jarlebring <eliasj@kth.se>
2017-11-09 10:35:46 +0100

    mslp clean up

--- src/method_mslp.jl
+++ src/method_mslp.jl
@@ -5,8 +5,31 @@ export mslp
 
 
 """
-     mslp(nep,..)
-   Method of successive linear problems
+     mslp([eltype],nep::NEP;[errmeasure,][tol,][maxit,][λ,][v,][displaylevel,][eigsolvertype::DataType][armijo_factor=1,][armijo_max])
+
+Runs the method of successive linear problems. The  method requires the solution of a
+generalized eigenvalue problem in every iteration. The method used for the eigenvalue
+computation is specified in eigsolvertype. See `newton` for other parameters.
+
+# Example
+Create a rational NEP with SPMFs.
+```julia-repl
+julia> Av=[ones(3,3),eye(3,3),triu(ones(3,3))];
+julia> fv=[S-> S, S -> S^2, S::AbstractArray -> inv(full(S)-eye(S)*10)]
+julia> nep=SPMF_NEP(Av,fv)
+julia> (λ,v)=mslp(nep)
+julia> compute_Mlincomb(nep,λ,v)
+3-element Array{Complex{Float64},1}:
+ -1.38778e-17+1.65715e-18im
+ -5.55112e-17+1.30633e-17im
+ -4.16334e-17-1.54436e-17im
+```
+
+# References
+* A. Ruhe, Algorithms for the nonlinear eigenvalue problem, SIAM J. Numer. Anal. 10 (1973) 674-689
+
+
+
 """
 mslp(nep::NEP;params...)=mslp(Complex128,nep;params...)
 function mslp{T}(::Type{T},
@@ -16,40 +39,39 @@ function mslp{T}(::Type{T},
                  tol=eps(real(T))*100,
                  maxit=100,
                  λ=zero(T),
-                 v=randn(real(T),size(nep,1)),
                  displaylevel=0,
                  eigsolvertype::DataType=DefaultEigSolver)
 
-    # Ensure types λ and v are of type T
+    # Ensure types λ is of type T
     λ = T(λ)
-    v = Array{T,1}(v)
 
-    σ=λ;     
+    # Allocate memory for the eigenvector approximation
+    v = zeros(T,size(nep,1));
+
     err=Inf;
 
-
-    #levsolver = LinEigSolver();
-        
     # Main loop
     for k=1:maxit
+
+        # solve generalized eigenvalue problem
+        solver=eigsolvertype(compute_Mder(nep,λ,0),compute_Mder(nep,λ,1));
+
+        d,v = eig_solve(solver,target=0,nev=1);
+
+        # update eigenvalue
+        λ += -d
+
         # Normalize
         v=v/norm(v);
 
+        # Checck for convergence
         err=errmeasure(λ,v)
-
-        @ifd(println("Iteration:",k," errmeasure:",err))
+        @ifd(println("Iteration:",k," errmeasure:",err, " λ=",λ))
         
         if (err< tol)
             return (λ,v)
         end
-
-        # solve generalized eigenvalue problem
-        #d,v = levsolver.solve(compute_Mder(nep,λ,0),B=compute_Mder(nep,λ,1),λ_t=λ,nev=1);
-        solver=eigsolvertype(compute_Mder(nep,λ,0),compute_Mder(nep,λ,1));
-
-        d,v = eig_solve(solver,target=0,nev=1);
-        # update eigenvalue
-        λ += -d
+        
     end
 
     msg="Number of iterations exceeded. maxit=$(maxit)."
