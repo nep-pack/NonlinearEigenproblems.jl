1dc34c926813a524ff4d57b96ccd0c2252ecc7bb
eringh <eringh@kth.se>
2017-11-11 22:51:04 +0100

    JD: Introduced linsolvercreator and removed explicit need for matrix-access (which was there before). Also implemented SG as default inner NEP-solver for projected problem. And minor cleanup. #26

--- src/method_jd_quad.jl
+++ src/method_jd_quad.jl
@@ -8,6 +8,7 @@ function jd_quad{T, T_orth<:IterativeSolvers.OrthogonalizationMethod}(::Type{T},
                     nep::ProjectableNEP;
                     orthmethod::Type{T_orth} = DGKS,
                     errmeasure::Function = default_errmeasure(nep::NEP),
+                    linsolvercreator::Function=default_linsolvercreator,
                     tol = eps(real(T))*100,
                     maxit = 100,
                     λ = zero(T),
@@ -21,15 +22,13 @@ function jd_quad{T, T_orth<:IterativeSolvers.OrthogonalizationMethod}(::Type{T},
 
     proj_nep = create_proj_NEP(nep)
 
-    V::Array{T,2} = zeros(T, size(nep,1), maxit)
+    V::Array{T,2} = zeros(T, size(nep,1), maxit+1)
     V[:,1] = u
-    dummy_vector = zeros(T,maxit)
+    dummy_vector = zeros(T,maxit+1)
 
     for k=1:maxit
         err=errmeasure(λ,u)
-        if (displaylevel>0)
-          println("Iteration:",k," errmeasure:",err)
-        end
+        @ifd(print("Iteration:", k, " errmeasure:", err, "\n"))
         if (err< tol)
             return (λ,u)
         end
@@ -37,45 +36,36 @@ function jd_quad{T, T_orth<:IterativeSolvers.OrthogonalizationMethod}(::Type{T},
         # Projected matrices
         W = view(V, :, 1:k) # extact subarrays, memory-CPU efficient
         w = view(V, :, k+1); # next vector position
-
         set_projectmatrices!(proj_nep, W, W)
 
         # find the eigenvalue with smallest absolute value of projected NEP
         λ,s = jd_inner_eig_solver(typeof(nep), T, proj_nep, eigsolvertype)
         s = s/norm(s)
 
+        # the approximate eigenvector
         u[:] = W*s
 
-        Mdu::Array{T,1} = compute_Mlincomb(nep,λ,u,[1],1)
-        P1 = eye(T,n) - Mdu*u'/(u'*Mdu)
-        P2 = eye(T,n) - u*u'
-
-        r = compute_Mlincomb(nep,λ,u)
-
-        MP2 = zeros(T,size(nep,1),size(P2,2))
-        for ii = 1:size(P2,2)
-            MP2[:,ii] = compute_Mlincomb(nep,λ,P2[:,ii],[1],0)
-        end
-        X = P1*MP2
-
-        # Least squares, -pseudo_inv(X)*r
-        Q,R = qr(X)
-        w[:] = -R\(Q'*r)
+        # solve for the basis extension using equation (9) to avoid matrix access.
+        # the vector should be orthogonal to u
+        pk::Array{T,1} = compute_Mlincomb(nep,λ,u,[1],1)
+        linsolver = linsolvercreator(nep,λ)
+        tt::Array{T,1} = lin_solve(linsolver, pk, tol=tol) # M(λ)\pk
+        α = norm(u)^2/dot(tt,u)
+        w[:] = -u + α*tt
 
         # orthogonalization
         orthogonalize_and_normalize!(W, w, dummy_vector[1:k], orthmethod)
-
-        println("Iteration: ",k," norm of residual:", compute_resnorm(nep,λ,u))
     end
 
     err=errmeasure(λ,u)
     msg="Number of iterations exceeded. maxit=$(maxit)."
-    throw(NoConvergenceException(λ,v,err,msg))
+    throw(NoConvergenceException(λ,u,err,msg))
 end
 
 
 function jd_inner_eig_solver(::Type{T_orig_nep}, T, proj_nep, eigsolvertype) where {T_orig_nep <: ProjectableNEP}
-    error("NOT IMPLEMTED YET") #TODO: Implement this!
+    λ,s = sgiter(T, proj_nep, 1)
+    return λ, s
 end
 
 function jd_inner_eig_solver(::Type{T_orig_nep}, T, proj_nep, eigsolvertype) where {T_orig_nep <: PEP}
--- src/run_test_jd_quad.jl
+++ src/run_test_jd_quad.jl
@@ -27,9 +27,17 @@ println("\n 5 smallest eigenvalues according to the absolute values: \n", Dc[c[1
 
 
 println("\n Testing in only Complex64 precision (32 bit in real and 32 bit in imaginary)")
-λ,u =jd_quad(Complex64, nep, tol=5e-4, maxit=80);
+λ,u =jd_quad(Complex64, nep, tol=1e-4, maxit=80);
 println("\n Resnorm of computed solution: ",compute_resnorm(nep,λ,u))
 println("\n Smallest eigevalue found: \n λ: ",λ)
 Dc,Vc = polyeig(Complex64,nep,DefaultEigSolver);
 c = sortperm(abs.(Dc));
 println("\n 5 smallest eigenvalues according to the absolute values: \n", Dc[c[1:5]]);
+
+
+
+println("\n Testing a non-PEP")
+nep2 = nep_gallery("real_quadratic")
+λ,u =jd_quad(Complex128, nep2, tol=1e-10, maxit=80);
+println("\n Resnorm of computed solution: ",compute_resnorm(nep2,λ,u))
+println("\n Smallest eigevalue found: \n λ: ",λ)
