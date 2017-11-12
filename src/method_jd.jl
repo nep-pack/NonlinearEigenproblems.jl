d1458ef2276cef1a5082f2b3f1009c6330b89d09
eringh <eringh@kth.se>
2017-11-12 19:36:53 +0100

    JD: Removed inner eigsolvetype (Linear) since it was only used for PEP and hence the structure gets a bit wierd. Also, it is unlikely that the projected problem is sparse and highly structured, and hence we rely on the default.

--- src/method_jd.jl
+++ src/method_jd.jl
@@ -13,8 +13,7 @@ function jd{T, T_orth<:IterativeSolvers.OrthogonalizationMethod}(::Type{T},
                     maxit = 100,
                     λ = zero(T),
                     v0 = randn(size(nep,1)),
-                    displaylevel = 0,
-                    eigsolvertype::DataType = DefaultEigSolver)
+                    displaylevel = 0)
 
     λ::T = T(λ)
     u::Array{T,1} = Array{T,1}(v0)/norm(v0)
@@ -39,7 +38,7 @@ function jd{T, T_orth<:IterativeSolvers.OrthogonalizationMethod}(::Type{T},
         set_projectmatrices!(proj_nep, W, W)
 
         # find the eigenvalue with smallest absolute value of projected NEP
-        λ,s = jd_inner_eig_solver(typeof(nep), T, proj_nep, eigsolvertype)
+        λ,s = jd_inner_eig_solver(typeof(nep), T, proj_nep)
         s = s/norm(s)
 
         # the approximate eigenvector
@@ -63,14 +62,14 @@ function jd{T, T_orth<:IterativeSolvers.OrthogonalizationMethod}(::Type{T},
 end
 
 
-function jd_inner_eig_solver(::Type{T_orig_nep}, T, proj_nep, eigsolvertype) where {T_orig_nep <: ProjectableNEP}
+function jd_inner_eig_solver(::Type{T_orig_nep}, T, proj_nep) where {T_orig_nep <: ProjectableNEP}
     λ,s = sgiter(T, proj_nep, 1)
     return λ, s
 end
 
-function jd_inner_eig_solver(::Type{T_orig_nep}, T, proj_nep, eigsolvertype) where {T_orig_nep <: PEP}
+function jd_inner_eig_solver(::Type{T_orig_nep}, T, proj_nep) where {T_orig_nep <: PEP}
     pep_temp = PEP(get_Av(proj_nep))
-    Dc,Vc = polyeig(T,pep_temp,eigsolvertype)
+    Dc,Vc = polyeig(T,pep_temp)
     c = sortperm(abs.(Dc))
     λ = Dc[c[1]]
     s = Vc[:,c[1]]
