7e6182864833c8d9c39b17ed78f9979cca733db8
eringh <eringh@kth.se>
2017-11-12 21:17:24 +0100

    JD: Can ask for computing multiple eigenvalues. #26

--- src/method_jd.jl
+++ src/method_jd.jl
@@ -9,36 +9,49 @@ function jd{T, T_orth<:IterativeSolvers.OrthogonalizationMethod}(::Type{T},
                     orthmethod::Type{T_orth} = DGKS,
                     errmeasure::Function = default_errmeasure(nep::NEP),
                     linsolvercreator::Function=default_linsolvercreator,
+                    Neig = 1,
                     tol = eps(real(T))*100,
                     maxit = 100,
                     λ = zero(T),
                     v0 = randn(size(nep,1)),
                     displaylevel = 0)
 
-    λ::T = T(λ)
-    u::Array{T,1} = Array{T,1}(v0)/norm(v0)
     n = size(nep,1)
+    if (maxit > n)
+        warn("maxit = ", maxit, " is larger than size of NEP = ", n,". Setting maxit = size(nep,1)")
+        maxit = n
+    end
+
+    λ_vec::Array{T,1} = Array{T,1}(Neig)
+    u_vec::Array{T,2} = Array{T,2}(n,Neig)
+    u::Array{T,1} = Array{T,1}(v0)/norm(v0)
 
     proj_nep = create_proj_NEP(nep)
 
     V::Array{T,2} = zeros(T, size(nep,1), maxit+1)
     V[:,1] = u
     dummy_vector = zeros(T,maxit+1)
+    conveig = 0
 
     for k=1:maxit
         err=errmeasure(λ,u)
-        @ifd(print("Iteration:", k, " errmeasure:", err, "\n"))
-        if (err< tol)
-            return (λ,u)
+        @ifd(print("Iteration: ", k, " converged eigenvalues: ", conveig, " errmeasure: ", err, "\n"))
+        if convergence_criterion(err, tol, λ, λ_vec, conveig, T)
+            conveig += 1
+            λ_vec[conveig] = λ
+            u_vec[:,conveig] = u
+            if (conveig == Neig)
+                return (λ_vec,u_vec)
+            end
         end
 
         # Projected matrices
         W = view(V, :, 1:k) # extact subarrays, memory-CPU efficient
-        w = view(V, :, k+1); # next vector position
+        w = view(V, :, k+1) # next vector position
         set_projectmatrices!(proj_nep, W, W)
 
         # find the eigenvalue with smallest absolute value of projected NEP
-        λ,s = jd_inner_eig_solver(typeof(nep), T, proj_nep)
+        λ,s = jd_inner_eig_solver(typeof(nep), T, proj_nep, conveig+1)
         s = s/norm(s)
 
         # the approximate eigenvector
@@ -57,21 +70,36 @@ function jd{T, T_orth<:IterativeSolvers.OrthogonalizationMethod}(::Type{T},
     end
 
     err=errmeasure(λ,u)
-    msg="Number of iterations exceeded. maxit=$(maxit)."
-    throw(NoConvergenceException(λ,u,err,msg))
+    # Check one last time since space was expanded since prevous
+    if convergence_criterion(err, tol, λ, λ_vec, conveig, T)
+        conveig += 1
+        λ_vec[conveig] = λ
+        u_vec[:,conveig] = u
+        if (conveig == Neig)
+            return (λ_vec,u_vec)
+        end
+    end
+
+    msg="Number of iterations exceeded. maxit=$(maxit) and only $(conveig) eigenvalues converged out of $(Neig)."
+    throw(NoConvergenceException(cat(1,λ_vec[1:conveig],λ),cat(2,u_vec[:,1:conveig],u),err,msg))
 end
 
 
-function jd_inner_eig_solver(::Type{T_orig_nep}, T, proj_nep) where {T_orig_nep <: ProjectableNEP}
-    λ,s = sgiter(T, proj_nep, 1)
+function convergence_criterion(err, tol, λ, λ_vec, conveig, T)
+    #Small error and not already found (expception if it is the first)
+    return (err < tol) && (conveig == 0 || all( abs.(λ - λ_vec[1:conveig])./abs.(λ_vec[1:conveig]) .> eps(real(T))*1e5 ) )
+end
+
+function jd_inner_eig_solver(::Type{T_orig_nep}, T, proj_nep, N) where {T_orig_nep <: ProjectableNEP}
+    λ,s = sgiter(T, proj_nep, N)
     return λ, s
 end
 
-function jd_inner_eig_solver(::Type{T_orig_nep}, T, proj_nep) where {T_orig_nep <: PEP}
+function jd_inner_eig_solver(::Type{T_orig_nep}, T, proj_nep, N) where {T_orig_nep <: PEP}
     pep_temp = PEP(get_Av(proj_nep))
     Dc,Vc = polyeig(T,pep_temp)
     c = sortperm(abs.(Dc))
-    λ = Dc[c[1]]
-    s = Vc[:,c[1]]
+    λ = Dc[c[N]]
+    s = Vc[:,c[N]]
     return λ, s
 end
--- test/jd.jl
+++ test/jd.jl
@@ -14,34 +14,43 @@ nep = nep_gallery("pep0",100)
 
 jdtest=@testset "Jacobi–Davidson" begin
 
-λ,u =jd(nep, tol=1e-10, maxit=80);
-println("\n Resnorm of computed solution: ",compute_resnorm(nep,λ,u))
-println("\n Smallest eigevalue found: \n λ: ",λ)
-Dc,Vc = polyeig(nep,DefaultEigSolver);
-c = sortperm(abs.(Dc));
-println("\n 5 smallest eigenvalues according to the absolute values: \n", Dc[c[1:5]]);
+λ,u =jd(nep, tol=1e-10, maxit=80, Neig = 5, displaylevel=1)
+println("\n Resnorm of computed solution: ",compute_resnorm(nep,λ[1],u[:,1]))
+println("\n Smallest eigevalues found: \n λ: ",λ)
+Dc,Vc = polyeig(nep,DefaultEigSolver)
+c = sortperm(abs.(Dc))
+println("\n 5 smallest eigenvalues according to the absolute values: \n", Dc[c[1:5]])
 
-@test norm(compute_Mlincomb(nep,λ,u)) < 1e-10
-@test (abs(Dc[c[1]]-λ) < 1e-10 || abs(Dc[c[2]]-λ) < 1e-10) #Complex conjugate eigenvalues
+@test norm(compute_Mlincomb(nep,λ[1],u[:,1])) < 1e-10
+@test norm(compute_Mlincomb(nep,λ[2],u[:,2])) < 1e-10
+@test norm(compute_Mlincomb(nep,λ[3],u[:,3])) < 1e-10
+@test norm(compute_Mlincomb(nep,λ[4],u[:,4])) < 1e-10
+
+@test (abs(Dc[c[1]]-λ[1]) < 1e-10 && abs(Dc[c[2]]-λ[2]) < 1e-10) || (abs(Dc[c[2]]-λ[1]) < 1e-10 && abs(Dc[c[1]]-λ[2]) < 1e-10) #Complex conjugate eigenvalues
+@test  abs(Dc[c[3]]-λ[3]) < 1e-10
+@test (abs(Dc[c[4]]-λ[4]) < 1e-10 && abs(Dc[c[5]]-λ[5]) < 1e-10) || (abs(Dc[c[5]]-λ[4]) < 1e-10 && abs(Dc[c[4]]-λ[5]) < 1e-10) #Complex conjugate eigenvalues
 
 
 
 println("\n Testing in only Complex64 precision (32 bit in real and 32 bit in imaginary)")
-λ,u =jd(Complex64, nep, tol=1e-4, maxit=80);
-println("\n Resnorm of computed solution: ",compute_resnorm(nep,λ,u))
+λ,u =jd(Complex64, nep, tol=1e-4, maxit=80, Neig = 2, displaylevel = 1)
+println("\n Resnorm of computed solution: ",compute_resnorm(nep,λ[1],u[:,1]))
 println("\n Smallest eigevalue found: \n λ: ",λ)
-Dc,Vc = polyeig(Complex64,nep,DefaultEigSolver);
-c = sortperm(abs.(Dc));
-println("\n 5 smallest eigenvalues according to the absolute values: \n", Dc[c[1:5]]);
+Dc,Vc = polyeig(Complex64,nep,DefaultEigSolver)
+c = sortperm(abs.(Dc))
+println("\n 5 smallest eigenvalues according to the absolute values: \n", Dc[c[1:5]])
 
-@test norm(compute_Mlincomb(nep,λ,u)) < 1e-4
-@test (abs(Dc[c[1]]-λ) < 1e-4 || abs(Dc[c[2]]-λ) < 1e-4) #Complex conjugate eigenvalues
+@test norm(compute_Mlincomb(nep,λ[1],u[:,1])) < 1e-4
+@test norm(compute_Mlincomb(nep,λ[2],u[:,2])) < 1e-4
+@test (abs(Dc[c[1]]-λ[1]) < 1e-4 && abs(Dc[c[2]]-λ[2]) < 1e-4) || (abs(Dc[c[2]]-λ[1]) < 1e-4 && abs(Dc[c[1]]-λ[2]) < 1e-4) #Complex conjugate eigenvalues
 
 
 
 println("\n Testing a non-PEP")
 nep2 = nep_gallery("real_quadratic")
-λ,u =jd(Complex128, nep2, tol=1e-10, maxit=80);
+λ,u =jd(Complex128, nep2, tol=1e-10, maxit=80, displaylevel = 1)
+λ = λ[1]
+u = vec(u)
 println("\n Resnorm of computed solution: ",compute_resnorm(nep2,λ,u))
 println("\n Smallest eigevalue found: \n λ: ",λ)
 
