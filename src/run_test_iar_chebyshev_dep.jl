137e4ae20f9e9ef09bc0c0e990cb8859a649569b
gmele <gmele@kth.se>
2018-07-05 14:25:37 +0200

    minor

--- src/run_test_iar_chebyshev_dep.jl
+++ src/run_test_iar_chebyshev_dep.jl
@@ -22,12 +22,14 @@ nep=nep_gallery("dep0");
 
 #compute_Mlincomb(nep::DEP,λ::Number,V;a=ones(size(V,2)))=compute_Mlincomb_from_MM!(nep,λ,V,a)
 #(λ,Q,err)=iar(nep,σ=0,γ=1,Neig=6,v=ones(4),displaylevel=1,maxit=100,tol=eps()*100,check_error_every=1)
+σ=1; γ=0.4; n=size(nep,1)
+nep=DEP([broadcast(*,nep.A,exp.(-nep.tauv*σ)/γ); [-σ/γ*eye(n)] ],[nep.tauv*γ; 0])
 (λ,Q,err)=iar_chebyshev(nep,σ=0,γ=1,Neig=6,v=ones(4),displaylevel=1,maxit=100,tol=eps()*100,check_error_every=1)
-#
-# errormeasure=default_errmeasure(nep);
-# for i=1:length(λ)
-#     println("Eigenvalue=",λ[i]," residual = ",errormeasure(λ[i],Q[:,i]))
-# end
+
+errormeasure=default_errmeasure(nep);
+for i=1:length(λ)
+    println("Eigenvalue=",λ[i]," residual = ",errormeasure(λ[i],Q[:,i]))
+end
 #
 # m=size(err,1);
 # for i=1:m
