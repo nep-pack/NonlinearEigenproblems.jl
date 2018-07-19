4bb3b432703125f393b860ac877e764b636ed97e
Max Bennedich <bennedich@gmail.com>
2018-07-19 06:51:40 +0200

    Refactor runtests, and print test number.

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,33 +3,28 @@
 ##################
 using Base.Test
 
+function to_uppercase_set(array)
+    return Set{String}(map(uppercase, array))
+end
+
 # Add tests below if you wish that they are not run together with all tests
-tests_not_to_run = [
+tests_not_to_run = to_uppercase_set([
     "runtests.jl", # this file
     "Beyn_parallel.jl", # currently disabled
     "fiber.jl", # needs MATLAB
     "gun.jl", # needs MATLAB
     "matlablinsolvers.jl", # needs MATLAB
     "iar_chebyshev.jl", # currently contains a bug
-    ]::Array{String,1}
-
-
-base_path = string(@__DIR__)
-file_list = readdir(base_path)
+    ])
 
 @testset "All tests" begin
-    for i = 1:length(file_list)
-        file = file_list[i]
-        upp_file = uppercase(file) #OBS: Make no case difference
-        if endswith(upp_file, ".JL")
-            is_in_norun_list = false
-            for k = 1:length(tests_not_to_run)
-                is_in_norun_list = is_in_norun_list || (upp_file == uppercase(tests_not_to_run[k])) #OBS: Make no case difference
-            end
-            if(!is_in_norun_list)
-                @printf("Running test %s\n", file)
-                @time include(base_path *"/" * file)
-            end
-        end
+    base_path = string(@__DIR__)
+    file_list = readdir(base_path)
+    tests_to_run = filter!(f -> ismatch(r"(?i)\.jl$", f) && !in(uppercase(f), tests_not_to_run), file_list)
+
+    for i = 1:length(tests_to_run)
+        file = tests_to_run[i]
+        @printf("Running test %s (%d / %d)\n", file, i, length(tests_to_run))
+        @time include(base_path *"/" * file)
     end
 end
