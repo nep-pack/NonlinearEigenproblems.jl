ef59e09344b144775be3941dc301885b797e4d92
Max Bennedich <bennedich@gmail.com>
2018-07-20 11:56:37 +0200

    Add simple serialization module and test #42

---
+++ src/utils/Serialization.jl
@@ -0,0 +1,29 @@
+module Serialization
+    export write_sparse_matrix
+    export read_sparse_matrix
+
+    # Writes the sparse matrix M in plain text format to a file with the given name
+    function write_sparse_matrix(filename, M)
+        open(filename, "w") do f
+            write(f, "$(size(M, 1))\n$(size(M, 2))\n")
+            nz = findnz(M)
+            for n = 1:3
+                foreach(x -> write(f, "$x\n"), nz[n])
+            end
+        end
+    end
+
+    # Reads and returns a sparse matrix written by the write_sparse_matrix function
+    function read_sparse_matrix(filename)
+        data = open(filename) do f
+            readlines(f)
+        end
+        m = parse(Int, data[1])
+        n = parse(Int, data[2])
+        c = Integer((length(data) - 2) / 3)
+        I = map(x -> parse(Int, x), data[3:3+c-1])
+        J = map(x -> parse(Int, x), data[3+c:3+2*c-1])
+        V = map(x -> parse(Float64, x), data[3+2*c:3+3*c-1])
+        return sparse(I, J, V, m, n)
+    end
+end
