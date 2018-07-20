fd31878f65df024b9fd17ec286f4c0079612f411
Emil Ringh <eringh@kth.se>
2018-07-20 11:16:26 +0200

    Disabled JD-tests again as it still fails (computations not the same as on my machine).

--- test/jd.jl
+++ test/jd.jl
@@ -29,10 +29,10 @@ println("\n 4 smallest eigenvalues according to the absolute values: \n", Dc[c[1
 @test norm(compute_Mlincomb(nep,λ[4],u[:,4])) < TOL
 
 # Test realtive errors in eig-values and take hight for complex conjuagte values
-@test  abs(Dc[c[1]]-λ[1])abs(Dc[c[1]]) < TOL*1e2
+@test  abs(Dc[c[1]]-λ[1])/abs(Dc[c[1]]) < TOL*1e2
 @test (abs(Dc[c[2]]-λ[2])/abs(Dc[c[2]]) < TOL && abs(Dc[c[3]]-λ[3])/abs(Dc[c[3]]) < TOL*1e2) ||
       (abs(Dc[c[3]]-λ[2])/abs(Dc[c[3]]) < TOL && abs(Dc[c[2]]-λ[3])/abs(Dc[c[2]]) < TOL*1e2)
-@test  abs(Dc[c[4]]-λ[4])abs(Dc[c[4]]) < TOL*1e2
+@test  abs(Dc[c[4]]-λ[4])/abs(Dc[c[4]]) < TOL*1e2
 
 
 
@@ -49,7 +49,7 @@ println("\n 4 smallest eigenvalues according to the absolute values: \n", Dc[c[1
 
 @test norm(compute_Mlincomb(nep,λ[1],u[:,1])) < TOL
 @test norm(compute_Mlincomb(nep,λ[2],u[:,2])) < TOL
-@test  abs(Dc[c[1]]-λ[1])abs(Dc[c[1]]) < TOL*50
+@test  abs(Dc[c[1]]-λ[1])/abs(Dc[c[1]]) < TOL*50
 @test (abs(Dc[c[2]]-λ[2])/abs(Dc[c[2]]) < TOL*50) || (abs(Dc[c[3]]-λ[2])/abs(Dc[c[3]]) < TOL*50) #Complex conjugate eigenvalues
 
 
