ff1aed4deb298db9c3a0b72b2a4761ac984d8cfe
Max Bennedich <bennedich@gmail.com>
2018-07-25 11:43:29 +0200

    Add load_modules_for_test script which can be used to run tests without warnings and new workspace

--- test/runtests.jl
+++ test/runtests.jl
@@ -19,22 +19,7 @@ tests_not_to_run = to_uppercase_set([
     "matlablinsolvers.jl", # needs MATLAB
     ])
 
-push!(LOAD_PATH, string(@__DIR__, "/../src"))
-push!(LOAD_PATH, string(@__DIR__, "/../src/gallery_extra"))
-push!(LOAD_PATH, string(@__DIR__, "/../src/gallery_extra/waveguide"))
-
-using NEPCore
-using NEPTypes
-using LinSolvers
-using NEPSolver
-using Gallery
-using IterativeSolvers
-using LinearMaps
-using GalleryPeriodicDDE
-using GalleryWaveguide
-using Serialization
-
-global global_modules_loaded = true
+include("load_modules_for_tests.jl")
 
 @testset "All tests" begin
     base_path = string(@__DIR__)
