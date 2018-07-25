7b0a337e8ca4f3731401ca3a88f6cc153d185f4b
Max Bennedich <bennedich@gmail.com>
2018-07-25 13:02:09 +0200

    Automatically detect test scripts

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,11 +8,14 @@ function to_uppercase_set(array)
     return Set{String}(map(uppercase, array))
 end
 
+function is_test_script(file)
+    # only include .jl files containing the string '@test'
+    ismatch(r"(?i)\.jl$", file) && contains(open(readstring, file), "@test")
+end
+
 # Add tests below if you wish that they are not run together with all tests
 tests_not_to_run = to_uppercase_set([
     "runtests.jl", # this file
-    "submit_test_coverage.jl", # run by CI tool
-    "utils.jl", # test utilities
     "Beyn_parallel.jl", # currently disabled
     "fiber.jl", # needs MATLAB
     "gun.jl", # needs MATLAB
@@ -24,17 +27,15 @@ include("load_modules_for_tests.jl")
 @testset "All tests" begin
     base_path = string(@__DIR__)
     file_list = readdir(base_path)
-    tests_to_run = filter(f -> ismatch(r"(?i)\.jl$", f) && !in(uppercase(f), tests_not_to_run), file_list)
+    tests_to_run = filter(f -> is_test_script(f) && !in(uppercase(f), tests_not_to_run), file_list)
 
     to = TimerOutput()
 
     for i = 1:length(tests_to_run)
         file = tests_to_run[i]
         test_name = replace(file, r"(?i)\.jl$", "")
-        #if test_name == "tiar" || test_name == "spmf" || test_name == "transf"
-            @printf("Running test %s (%d / %d)\n", test_name, i, length(tests_to_run))
-            @timeit to test_name include(base_path * "/" * file)
-        #end
+        @printf("Running test %s (%d / %d)\n", test_name, i, length(tests_to_run))
+        @timeit to test_name include(base_path * "/" * file)
     end
 
     show(to; title = "Test Performance", compact = true)
