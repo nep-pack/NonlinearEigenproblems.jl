3e226d81a6911142bcdd002543a2cee5c78ae888
Max Bennedich <bennedich@gmail.com>
2018-08-03 18:21:51 +0200

    Use expression parsing instead of string matching to locate tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,27 +1,40 @@
-##################
-# Runs all tests #
-##################
+################################################################################
+# Runs all .jl files in the current directory containing a @test or @testset
+# macro, except those specified to be excluded
+################################################################################
 using Base.Test
 using TimerOutputs
 
-function to_uppercase_set(array)
-    return Set{String}(map(uppercase, array))
-end
-
-function is_test_script(file)
-    # only include .jl files containing the string '@test'
-    ismatch(r"(?i)\.jl$", file) && contains(open(readstring, file), "@test")
-end
-
 # Add tests below if you wish that they are not run together with all tests
-tests_not_to_run = to_uppercase_set([
+tests_not_to_run = Set{String}(map(uppercase, [
     "runtests.jl", # this file
     "Beyn_parallel.jl", # currently disabled
     "fiber.jl", # needs MATLAB
     "gun.jl", # needs MATLAB
     "matlablinsolvers.jl", # needs MATLAB
     "wep_large.jl", #  Extensive test for used during development. Needs MATLAB
-    ])
+    ]))
+
+function is_test_script(file)
+    if ismatch(r"(?i)\.jl$", file)
+        src = open(readstring, file)
+        pos = 1
+        while !done(src, pos)
+            expr, pos = parse(src, pos)
+            if contains_test_macro(expr)
+                return true
+            end
+        end
+    end
+    return false
+end
+
+function contains_test_macro(expr::Expr)
+    if expr.head == :macrocall && in(expr.args[1], [Symbol("@test") Symbol("@testset")])
+        return true
+    end
+    return any(e -> contains_test_macro(e), filter(a -> isa(a, Expr), expr.args))
+end
 
 include("load_modules_for_tests.jl")
 
