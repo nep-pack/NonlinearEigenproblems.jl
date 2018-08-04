0b21c2c0262ce275dab50655aae4821b36553ecb
Max Bennedich <bennedich@gmail.com>
2018-08-04 13:05:17 +0200

    Support running tests in subdirectories

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,6 @@
 ################################################################################
-# Runs all .jl files in the current directory containing a @test or @testset
-# macro, except those specified to be excluded
+# Runs all .jl files in the directory of this script containing a @test or
+# @testset macro, except those specified to be excluded
 ################################################################################
 using Base.Test
 using TimerOutputs
@@ -15,7 +15,9 @@ tests_not_to_run = Set{String}(map(uppercase, [
     "wep_large.jl", #  Extensive test for used during development. Needs MATLAB
     ]))
 
-function is_test_script(file)
+include("load_modules_for_tests.jl")
+
+function is_test_script(file::AbstractString)
     if ismatch(r"(?i)\.jl$", file)
         src = open(readstring, file)
         pos = 1
@@ -36,20 +38,18 @@ function contains_test_macro(expr::Expr)
     return any(e -> contains_test_macro(e), filter(a -> isa(a, Expr), expr.args))
 end
 
-include("load_modules_for_tests.jl")
-
 @testset "All tests" begin
-    base_path = string(@__DIR__)
-    file_list = readdir(base_path)
-    tests_to_run = filter(f -> is_test_script(joinpath(base_path, f)) && !in(uppercase(f), tests_not_to_run), file_list)
+    root = string(@__DIR__)
+    tests_to_run = [joinpath(dir, file) for (dir, _, files) in walkdir(root) for file in files
+        if is_test_script(joinpath(dir, file)) && !in(uppercase(file), tests_not_to_run)]
 
     to = TimerOutput()
 
     for i = 1:length(tests_to_run)
         file = tests_to_run[i]
-        test_name = replace(file, r"(?i)\.jl$", "")
+        test_name = replace(file, Regex("$root/?(.+).jl\$", "i"), s"\1")
         @printf("Running test %s (%d / %d)\n", test_name, i, length(tests_to_run))
-        @timeit to test_name include(base_path * "/" * file)
+        @timeit to test_name include(file)
     end
 
     show(to; title = "Test Performance", compact = true)
