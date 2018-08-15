b94f19adfda86155734a8d5c8541676b231721f2
Elias Jarlebring <jarlebring@gmail.com>
2018-08-15 13:25:58 +0200

    SUMNEP type #51

--- src/NEPTypes.jl
+++ src/NEPTypes.jl
@@ -7,6 +7,7 @@ module NEPTypes
     export REP
     export SPMF_NEP
     export AbstractSPMF
+    export SUMNEP
 
     export MatrixAndFunction
     export LowRankMatrixAndFunction
@@ -788,7 +789,53 @@ julia> compute_Mder(nep,λ)[1:2,1:2]
         return false; # A projected NEP is (essentially) never sparse
     end
 
+    """
+        type SUMNEP{nep1::NEP,nep2::NEP} <: NEP
 
+SUMNEP corresponds to a sum of two NEPs, i.e., if nep is a SUMNEP it
+is defined by
+```math
+M(λ)=M_1(λ)+M_2(λ)
+```
+where M_1 and M_2 are defined by `nep1` and `nep2`.
+
+# Example:
+```julia-repl
+julia> nep1=DEP([ones(3,3),randn(3,3)])
+julia> nep2=PEP([ones(3,3),randn(3,3),randn(3,3)])
+julia> sumnep=SUMNEP(nep1,nep2);
+julia> s=3.0;
+julia> M=compute_Mder(sumnep,s);
+3×3 Array{Float64,2}:
+  8.54014     6.71897   7.12007 
+ -0.943908  -13.0795   -0.621659
+  6.03155    -7.26726  -6.42828 
+julia> M1=compute_Mder(nep1,s);
+julia> M2=compute_Mder(nep2,s);
+julia> M1+M2  # Same as M
+3×3 Array{Float64,2}:
+  8.54014     6.71897   7.12007 
+ -0.943908  -13.0795   -0.621659
+  6.03155    -7.26726  -6.42828 
+```
+"""
+    struct SUMNEP{NEP1<:NEP,NEP2<:NEP}  <: NEP 
+        nep1::NEP1
+        nep2::NEP2
+    end
+
+    # Delegate all interface functions
+    size(snep::SUMNEP)=size(snep.nep1)
+    size(snep::SUMNEP,d)=size(snep.nep1,d)
+    compute_Mlincomb(nep::SUMNEP, λ::Number, V;a=ones(eltype(λ),size(V,2))) =
+        (compute_Mlincomb(nep.nep1, λ, V,a=a)+compute_Mlincomb(nep.nep2,λ,V,a=a))
+    compute_Mder(nep::SUMNEP, λ::Number,i::Int = 0) =
+        (compute_Mder(nep.nep1,λ,i)+compute_Mder(nep.nep2,λ,i))
+    compute_MM(nep::SUMNEP, S::Matrix,V::Matrix) =
+        (compute_MM(nep.nep1,S,V)+compute_M(nep.nep2,S,V))
+
+
+    
     ############################################################################
     # PNEP (Polynomial plus Nonlinear Eigenvalue Problem)
     ############################################################################
