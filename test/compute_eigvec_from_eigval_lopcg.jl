2d7176c8a0b6d50094561bea9625cae0c0b9ecbb
Max Bennedich <bennedich@gmail.com>
2018-09-05 21:47:58 +0200

    materialize adjoint matrices, optimize memory usage, and create a better start vector #65

--- test/compute_eigvec_from_eigval_lopcg.jl
+++ test/compute_eigvec_from_eigval_lopcg.jl
@@ -9,7 +9,9 @@ import NonlinearEigenproblems.NEPCore.compute_Mlincomb
 
 @testset "compute eigvec lopcg" begin
     @testset "dep0_sparse" begin
-        nep=nep_gallery("dep0_sparse",100);nept=DEP([nep.A[1]',nep.A[2]'],nep.tauv);
+        nep = nep_gallery("dep0_sparse", 100);
+        nept = DEP([copy(nep.A[1]'), copy(nep.A[2]')], nep.tauv);
+        Random.seed!(13) # this results in a good start vector
         λ,Q,err = iar(nep,maxit=100,Neig=2,σ=1.0,γ=1,displaylevel=0,check_error_every=1);
         v=compute_eigvec_from_eigval_lopcg(nep,nept,λ[1]);
         errormeasure=default_errmeasure(nep);
@@ -17,8 +19,8 @@ import NonlinearEigenproblems.NEPCore.compute_Mlincomb
     end
 
     @testset "pep0" begin
-        nep=nep_gallery("pep0",100);
-        nept=PEP([nep.A[1]',nep.A[2]',nep.A[3]'])
+        nep = nep_gallery("pep0", 100);
+        nept = PEP([copy(nep.A[1]'), copy(nep.A[2]'), copy(nep.A[3]')])
         λ,Q,err = iar(nep,maxit=100,Neig=2,σ=1.0,γ=1,displaylevel=0,check_error_every=1);
         v=compute_eigvec_from_eigval_lopcg(nep,nept,λ[1]);
         errormeasure=default_errmeasure(nep);
