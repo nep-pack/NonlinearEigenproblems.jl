5b3b823f5c50a8aa7405a9934283607f363fa30a
gmele <gmele@kth.se>
2018-09-07 13:49:34 +0200

    compute_Mlincomb for SPMF initiated (faster). It works now.

--- profiling/compute_Mlincomb_SPMF.jl
+++ profiling/compute_Mlincomb_SPMF.jl
@@ -13,7 +13,7 @@ f2 = S -> Matrix{eltype(S)}(I, size(S));
 f3 = S -> exp(-Matrix(S));
 f4 = S -> sqrt(10*S+100);
 
-n=100;
+n=100000;
 Random.seed!(1) # reset the random seed
 K=[1:n;2:n;1:n-1]; J=[1:n;1:n-1;2:n]; # sparsity pattern of tridiag matrix
 A1=sparse(K, J, rand(3*n-2))
@@ -25,7 +25,7 @@ AA = [A1,A2,A3,A4]
 fi = [f1,f2,f3,f4]
 nep=SPMF_NEP(AA, fi)
 
-n=size(nep,1);	k=50;
+n=size(nep,1);	k=10;
 V=rand(n,k);	λ=rand()*im+rand();	#TODO: if λ complex doesn't work. WHY?
 a=rand(k)
 
@@ -41,10 +41,10 @@ function compute_Mlincomb2(nep::SPMF_NEP, λ, V, a)
 
 	Z=zeros(eltype(V),n)
 	for i=1:size(nep.A,1)
-		Fi=nep.fi[i](S)[:,2]
+		Fi=nep.fi[i](S)[:,1]
 		Z=Z .+ nep.A[i]*(V*Fi);
 	end
-	return Z
+	return a[1]*Z
 end
 
 
