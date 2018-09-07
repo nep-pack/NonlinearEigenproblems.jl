e65c85d77d7a5815ba033a334debd77b70dbf76a
gmele <gmele@kth.se>
2018-09-07 09:21:40 +0200

    minor

--- profiling/compute_Mlincomb_DEP.jl
+++ profiling/compute_Mlincomb_DEP.jl
@@ -1,18 +1,11 @@
 #Intended to be run from nep-pack/ directory or nep-pack/profiling directory
 
-push!(LOAD_PATH, string(@__DIR__, "/../src"))
-push!(LOAD_PATH, string(@__DIR__, "/../src/gallery_extra"))
-push!(LOAD_PATH, string(@__DIR__, "/../src/gallery_extra/waveguide"))
-
-using NEPCore
-using NEPTypes
-using LinSolvers
-using NEPSolver
-using Gallery
-using IterativeSolvers
-using LinearAlgebra
-using Random
+using NonlinearEigenproblems.NEPSolver
+using NonlinearEigenproblems.Gallery
+using NonlinearEigenproblems.NEPCore
 using Test
+using LinearAlgebra
+using IterativeSolvers
 
 nep=nep_gallery("dep0_tridiag",10000)
 
@@ -21,14 +14,14 @@ n=size(nep,1);	k=1;
 V=rand(n,k);	λ=rand()*im+rand();	#TODO: if λ complex doesn't work. WHY?
 a=rand(k)
 
-z1=compute_Mlincomb(nep,λ,copy(V),a=a)
+z1=compute_Mlincomb(nep,λ,copy(V),a)
 
-compute_Mlincomb(nep,λ,V,a=a)
-@time z1=compute_Mlincomb(nep,λ,V,a=a)
+compute_Mlincomb(nep,λ,V,a)
+@time z1=compute_Mlincomb(nep,λ,V,a)
 
 # old way of compute_Mlincomb used for DEP
-import NEPCore.compute_Mlincomb_from_MM
+import NonlinearEigenproblems.NEPCore.compute_Mlincomb_from_MM
 z2=compute_Mlincomb_from_MM(nep,λ,V,a)
 @time z2=compute_Mlincomb_from_MM(nep,λ,V,a)
 
-println("Error=",opnorm(z1-z2))
+println("Error=",norm(z1-z2))
