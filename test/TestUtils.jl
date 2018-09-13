12b1991749c5f3f03a0ca5cc64e4e150fae92f7e
Max Bennedich <bennedich@gmail.com>
2018-09-13 22:53:10 +0200

    Include comparisons in report

--- test/TestUtils.jl
+++ test/TestUtils.jl
@@ -1,12 +1,19 @@
 module TestUtils
 
+using InteractiveUtils
 using BenchmarkTools
 using Statistics
 using Printf
 using HTTP
+using Dates
 
 export @bench
 export report_benchmarks
+
+const time_tolerance_percent = 10
+const memory_tolerance_percent = 1
+const ci_github_repo = "maxbennedich/julia-ci"
+const source_github_repo = "nep-pack/NonlinearEigenproblems.jl"
 
 # the TEST_SUITE environment variable can be set by the CI tool
 run_benchmark = get(ENV, "TEST_SUITE", "") == "benchmark"
@@ -68,41 +75,75 @@ function report_benchmarks(test_results)
 
     aggregate_trials(test_results, [])
 
+    new_run_context = get_run_context()
     new_benchmark = BenchmarkGroup()
     foreach(t -> new_benchmark[t.key] = t.trial, values(aggregated_trials))
+
+    write_file("run_context.txt", new_run_context)
     BenchmarkTools.save("benchmark.json", new_benchmark)
 
-    json_request = HTTP.request("GET", "https://raw.githubusercontent.com/maxbennedich/julia-ci/master/benchmark.json")
-    json = String(json_request.body)
+    old_run_context = read_github_file("run_context.txt")
+    json = read_github_file("benchmark.json")
     old_benchmark = BenchmarkTools.load(IOBuffer(json))[1]
 
-    foreach(j -> println("$(j[1]): $(j[2])"), judge(new_benchmark, old_benchmark))
-    println("-------------")
-    for key in intersect(keys(old_benchmark), keys(new_benchmark))
-        j = judge(new_benchmark[key], old_benchmark[key]; time_tolerance = 0.1, memory_tolerance = 0.05)
-        if j.time != :invariant || j.memory != :invariant
-            println("$key -- $(prettytime(old_benchmark[key].time)) -> $(prettytime(new_benchmark[key].time)) -- $j")
+    open("benchmark.md", "w") do io
+        commit_msg = get(ENV, "TRAVIS_COMMIT_MESSAGE", "Unknown commit")
+        commit_id = get(ENV, "TRAVIS_COMMIT", "")
+        print(io, "### $commit_msg\n")
+        isempty(commit_id) || print(io, "commit [$commit_id](https://github.com/$source_github_repo/commit/$commit_id)\n")
+
+        print(io, "```diff\n")
+        column_width = maximum([length(test.name) for test in values(aggregated_trials)])
+        column_title = "Test name"
+        divider = " " * "─"^(column_width + 39) * "\n"
+        @printf(io, "%s %s%s         Time               Memory\n%s", divider, column_title, " "^(column_width - length(column_title)), divider)
+
+        # hierarchically sort and print benchmark trials
+        function print_trials(test)
+            trial = aggregated_trials[test]
+
+            if haskey(old_benchmark, trial.key) && haskey(new_benchmark, trial.key)
+                j = judge(new_benchmark[trial.key], old_benchmark[trial.key])
+                time_diff = 100 * (j.ratio.time - 1)
+                memory_diff = 100 * (j.ratio.memory - 1)
+            else
+                time_diff = NaN
+                memory_diff = NaN
+            end
+
+            diff_prefix =
+                time_diff >=  time_tolerance_percent || memory_diff >=  memory_tolerance_percent ? '-' :
+                time_diff <= -time_tolerance_percent || memory_diff <= -memory_tolerance_percent ? '+' : ' ';
+
+            @printf(io, "%c%s%s    %s (%s)    %s (%s)\n",
+                diff_prefix,
+                trial.name,
+                " "^(column_width - length(trial.name)),
+                prettytime(trial.trial.time),
+                prettypercent(time_diff),
+                prettymemory(trial.trial.memory),
+                prettypercent(memory_diff))
+
+            sorted_children = sort(
+                [x for x in test.results if haskey(aggregated_trials, x)];
+                lt = (a,b) -> isless(aggregated_trials[b].trial.time, aggregated_trials[a].trial.time))
+            foreach(print_trials, sorted_children)
         end
+
+        print_trials(test_results)
+        print(io, divider)
+        print(io, "```\n")
+
+        print(io, "Previous context:\n")
+        print(io, "```\n")
+        print(io, old_run_context)
+        print(io, "```\n")
+
+        print(io, "Current context:\n")
+        print(io, "```\n")
+        print(io, new_run_context)
+        print(io, "```\n")
     end
-
-    column_width = maximum([length(test.name) for test in values(aggregated_trials)])
-    column_title = "Test name"
-    divider = "─"^(column_width + 23) * "\n"
-    @printf("%s%s%s     Time       Memory\n%s", divider, column_title, " "^(column_width - length(column_title)), divider)
-
-    # hierarchically sort and print benchmark trials
-    function print_trials(test)
-        trial = aggregated_trials[test]
-        @printf("%s%s    %s    %s\n", trial.name, " "^(column_width - length(trial.name)), prettytime(trial.trial.time), prettymemory(trial.trial.memory))
-
-        sorted_children = sort(
-            [x for x in test.results if haskey(aggregated_trials, x)];
-            lt = (a,b) -> isless(aggregated_trials[b].trial.time, aggregated_trials[a].trial.time))
-        foreach(print_trials, sorted_children)
-    end
-
-    print_trials(test_results)
-    print(divider)
 end
 
 import Base.+
@@ -117,6 +158,32 @@ import Base.zero
         t1.allocs + t2.allocs)
 
 zero(t::BenchmarkTools.TrialEstimate) = BenchmarkTools.TrialEstimate(t.params, 0, 0, 0, 0)
+
+function read_github_file(file)
+    req = HTTP.request("GET", "https://raw.githubusercontent.com/$ci_github_repo/master/$file")
+    String(req.body)
+end
+
+function write_file(file, str)
+    open(file, "w") do f
+        write(f, str)
+    end
+end
+
+function get_run_context()
+    io = IOBuffer()
+    write(io, "UTC time: $(Dates.format(Dates.now(Dates.UTC), "yyyy-mm-dd HH:MM:SS"))\n")
+    write(io, "Commit: $(get(ENV, "TRAVIS_COMMIT", "N/A"))\n")
+    write(io, "Message: $(get(ENV, "TRAVIS_COMMIT_MESSAGE", "N/A"))\n")
+    write(io, "Travis build: $(get(ENV, "TRAVIS_BUILD_NUMBER", "N/A"))\n")
+    write(io, "\n")
+    versioninfo(io, verbose = true)
+    return String(io.data)
+end
+
+function prettypercent(p)
+    lpad(isnan(p) ? "--- " : @sprintf("%+.0f%%", p), 5, " ")
+end
 
 # Copied from TimerOutputs.jl
 function prettytime(t)
