70bdc3a88d5ee3e6d49d25a4ae68997f7751ecc9
Max Bennedich <bennedich@gmail.com>
2018-09-18 14:34:14 +0200

    Use LinSolvers in NLEIGS #79

---
+++ src/nleigs/linsolvercache.jl
@@ -0,0 +1,23 @@
+using LinearAlgebra
+using SuiteSparse
+
+struct LinSolverCache{T<:Number}
+    solver::Dict{T,LinSolver}
+    nep::NEP
+    linsolvercreator::Function
+end
+
+LinSolverCache(::Type{T}, nep, linsolvercreator) where T<:Number =
+    LinSolverCache(Dict{T,LinSolver}(), nep, linsolvercreator)
+
+function solve(cache, σ, y, add_to_cache)
+    @debug "Lin solve for $σ, cache=$add_to_cache, size=$(length(cache.solver))"
+    if add_to_cache
+        solver = get!(() -> (
+            @debug "Cache miss, creating lin solver for $σ";
+            cache.linsolvercreator(cache.nep, σ)), cache.solver, σ)
+    else
+        solver = cache.linsolvercreator(cache.nep, σ)
+    end
+    lin_solve(solver, y)
+end
