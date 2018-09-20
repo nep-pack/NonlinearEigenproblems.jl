f4de01c0553005e113a4fd30e1d6a04a88ca02f7
gmele <gmele@kth.se>
2018-09-20 12:59:58 +0200

    minor

--- profiling/compute_Mlincomb_PEP.jl
+++ profiling/compute_Mlincomb_PEP.jl
@@ -11,10 +11,10 @@ import Base.exp
 n=10000;
 Random.seed!(1) # reset the random seed
 K=[1:n;2:n;1:n-1]; J=[1:n;1:n-1;2:n]; # sparsity pattern of tridiag matrix
-A1=sparse(K, J, rand(3*n-2))
-A2=sparse(K, J, rand(3*n-2))
-A3=sparse(K, J, rand(3*n-2))
-A4=sparse(K, J, rand(3*n-2))
+A1=sparse(K, J, rand(Float32,3*n-2))
+A2=sparse(K, J, rand(Float32,3*n-2))
+A3=sparse(K, J, rand(Float32,3*n-2))
+A4=sparse(K, J, rand(Float32,3*n-2))
 
 AA = [A1,A2,A3,A4]
 nep=PEP(AA)
@@ -30,3 +30,7 @@ import NonlinearEigenproblems.NEPCore.compute_Mlincomb_from_MM
 z2=compute_Mlincomb_from_MM(nep,λ,V,a)
 @time z2=compute_Mlincomb_from_MM(nep,λ,V,a)
 println("Error=",norm(z1-z2))
+
+V=rand(Float32,n,k);	λ=rand(Float32)*im+rand(Float32); a=rand(Float32,k)
+z2=compute_Mlincomb_from_MM(nep,λ,V,a)
+z3=compute_Mlincomb_from_MM(nep,λ,V,a)
