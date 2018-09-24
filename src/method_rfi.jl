23e65c9371095c23085e6b0d75a9c2079be72b98
parik <pup@kth.se>
2018-09-24 16:05:37 +0200

    Some function comments and minor modifications in rfi

--- src/method_rfi.jl
+++ src/method_rfi.jl
@@ -5,21 +5,24 @@ export rfi
 export rfi_b
 
 """
-    rfi(nep,nept,[λ=0,][errmeasure=default_errmeasure,][tol=eps()*100,][maxit=100,][v=randn,][u=randn,][displaylevel=0,][linsolvecreator=default_linsolvecreator,])
+ rfi(nep,nept,[λ=0,][errmeasure=default_errmeasure,][tol=eps()*100,][maxit=100,][v=randn,][u=randn,][displaylevel=0,][linsolvecreator=default_linsolvecreator,])
 
-This is an implementation of the two-sided Rayleigh functional Iteration. This method requires the transpose of the NEP, which needs to be provided in nept.
+This is an implementation of the two-sided Rayleigh functional Iteration(RFI) to compute an eigentriplet of the problem specified by 'nep'.
+This method requires the transpose of the NEP, specified in 'nept'. 
+'λ', 'u' and 'v' are initial guesses for the eigenvalue, the right eigenvector and the left eigenvector respectively.
+A 'NoConvergenceException' is thrown if an eigentriplet is not found in 'maxit' iterations.
 
 # Example:
 julia> nep=nep_gallery("dep0");
 julia> nept=DEP([nep.A[1]',nep.A[2]'])
-julia> λ,v,u=rfi(nep,nept,v=ones(size(nep,1)))
-julia> opnorm(compute_Mder(nep,λ)*v) % v is a right eigenvector
-5.4672143489065705e-16
-julia> opnorm(u'*compute_Mder(nep,λ)) % u is a left eigenvector
-4.1447913221215544e-16
+julia> λ,v,u=rfi_b(nep,nept)
+julia> compute_resnorm(nep,λ,v) % v is a right eigenvector
+4.347204570675246e-16
+julia> compute_resnorm(nept,λ,u) % u is a left eigenvector
+7.173081573164097e-16
 
 # Reference
-*  Algorithm 4 in  Schreiber, Nonlinear Eigenvalue Problems: Newton-type Methods and Nonlinear Rayleigh Functionals, PhD thesis, TU Berlin, 2008
+*  Algorithm 4 in  Schreiber, Nonlinear Eigenvalue Problems: Newton-type Methods and Nonlinear Rayleigh Functionals, PhD thesis, TU Berlin, 2008.
 
 """
 rfi(nep::NEP, nept::NEP; kwargs...) = rfi(ComplexF64,nep, nept,;kwargs...)
@@ -29,18 +32,14 @@ function rfi(::Type{T},
             errmeasure::Function=default_errmeasure(nep::NEP),
             tol = eps(real(T))*1000,
             maxit=100,
-            λ = zero(T),
-            v = randn(size(nep,1)),
-            u = randn(size(nep,1)),
+            λ::Number = zero(T),
+            v::Vector = randn(T,size(nep,1)),
+            u::Vector = randn(T,size(nep,1)),
             linsolvercreator::Function=default_linsolvercreator,
             displaylevel=0) where {T <: Number}
 
         err = Inf
 
-        #Ensure type coherence
-        λ = T(λ)
-        v = Vector{T}(v)
-        u = Vector{T}(u)
         #Normalize v and u
         normalize!(v)
         normalize!(u)
@@ -59,11 +58,9 @@ function rfi(::Type{T},
                 local linsolver::LinSolver = linsolvercreator(nep,λ)
                 local linsolver_t::LinSolver = linsolvercreator(nept,λ)
 
-                #S1: T(λ_k)x_(k+1) = T'(λ_k)u_(k)
                 x = lin_solve(linsolver,compute_Mlincomb(nep,λ,u,[T(1)],1),tol = tol)
                 u[:] = normalize(x)
 
-                #S2: (T(λ_k)^H)y_(k+1) = (T'(λ_k)^H)v_(k)
                 y = lin_solve(linsolver_t,compute_Mlincomb(nept,λ,v,[T(1)],1),tol = tol)
                 v[:] = normalize(y)
 
@@ -88,8 +85,25 @@ function rfi(::Type{T},
 end
 
 """
-Two-sided Rayleigh functional Iteration(Bordered version), as given as Algorithm 5 in  "Nonlinear Eigenvalue Problems: Newton-type Methods and
-Nonlinear Rayleigh Functionals", by Kathrin Schreiber.
+ rfi_b(nep,nept,[λ=0,][errmeasure=default_errmeasure,][tol=eps()*100,][maxit=100,][v=randn,][u=randn,][displaylevel=1,][linsolvecreator=default_linsolvecreator,])
+
+This is an implementation of the two-sided Rayleigh functional Iteration(RFI)-Bordered version to compute an eigentriplet of the problem specified by 'nep'.
+This method requires the transpose of the NEP, specified in 'nept'. 
+'λ', 'u' and 'v' are initial guesses for the eigenvalue, the right eigenvector and the left eigenvector respectively.
+A 'NoConvergenceException' is thrown if an eigentriplet is not found in 'maxit' iterations.
+
+# Example:
+julia> nep=nep_gallery("dep0");
+julia> nept=DEP([nep.A[1]',nep.A[2]'])
+julia> λ,v,u=rfi_b(nep,nept,v=ones(size(nep,1)))
+julia> compute_resnorm(nep,λ,v) % v is a right eigenvector
+5.343670589284583e-15
+julia> compute_resnorm(nept,λ,u) % u is a left eigenvector
+5.271390516634306e-16
+
+# Reference
+*  Algorithm 5 in  Schreiber, Nonlinear Eigenvalue Problems: Newton-type Methods and Nonlinear Rayleigh Functionals, PhD thesis, TU Berlin, 2008.
+
 """
 rfi_b(nep::NEP, nept::NEP; kwargs...) = rfi_b(ComplexF64,nep, nept,;kwargs...)
 function rfi_b(::Type{T},
@@ -98,18 +112,13 @@ function rfi_b(::Type{T},
             errmeasure::Function=default_errmeasure(nep::NEP),
             tol = eps(real(T))*1000,
             maxit=100,
-            λ = zero(T),
-            v = randn(size(nep,1)),
-            u = randn(size(nep,1)),
+            λ::Number = zero(T),
+            v::Vector = randn(T,size(nep,1)),
+            u::Vector = randn(T,size(nep,1)),
             linsolvercreator::Function=default_linsolvercreator,
             displaylevel=1) where {T <: Number}
 
         err = Inf
-
-        #Ensure type coherence
-        λ = T(λ)
-        v = Vector{T}(v)
-        u = Vector{T}(u)
         #Normalize v and u
         normalize!(v)
         normalize!(u)
@@ -126,16 +135,11 @@ function rfi_b(::Type{T},
 
                 #Construct C_k
                 C = [compute_Mder(nep,λ,0) compute_Mlincomb(nep,λ,u,[T(1)],1);v'*compute_Mder(nep,λ,1) T(0.0)]
-                #local linsolver::LinSolver = BackslashLinSolver(C);
 
-                #C[s;μ] = -[T(λ)u;0]
-                #l1 = lin_solve(linsolver,-[compute_Mlincomb(nep,λ,u,[1],0);0],tol = tol);
                 l1 = C\-[compute_Mlincomb(nep,λ,u,[T(1)],0);0]
                 s = l1[1:end-1]
                 u[:] = normalize(u+s)
 
-                #C[t;ν] = -[T(λ)'v;0]
-                #l2 = lin_solve(linsolver,-[compute_Mlincomb(nept,λ,v,[1],0);0],tol = tol);
                 l2 = C\-[compute_Mlincomb(nept,λ,v,[T(1)],0);0]
                 t = l2[1:end-1]
                 v[:] = normalize(v+t)
