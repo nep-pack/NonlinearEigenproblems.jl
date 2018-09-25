8ac97cd341b80c92c6ac40323029b3e13aeec442
parik <pup@kth.se>
2018-09-25 14:21:13 +0200

    Proper documentation for rfi

--- src/method_rfi.jl
+++ src/method_rfi.jl
@@ -5,14 +5,15 @@ export rfi
 export rfi_b
 
 """
- rfi(nep,nept,[λ=0,][errmeasure=default_errmeasure,][tol=eps()*100,][maxit=100,][v=randn,][u=randn,][displaylevel=0,][linsolvecreator=default_linsolvecreator,])
+    rfi(nep,nept,[λ=0,][errmeasure=default_errmeasure,][tol=eps()*100,][maxit=100,][v=randn,][u=randn,][displaylevel=0,][linsolvecreator=default_linsolvecreator,])
 
 This is an implementation of the two-sided Rayleigh functional Iteration(RFI) to compute an eigentriplet of the problem specified by 'nep'.
 This method requires the transpose of the NEP, specified in 'nept'. 
 'λ', 'u' and 'v' are initial guesses for the eigenvalue, the right eigenvector and the left eigenvector respectively.
 A 'NoConvergenceException' is thrown if an eigentriplet is not found in 'maxit' iterations.
 
-# Example:
+# Example
+```julia-repl
 julia> nep=nep_gallery("dep0");
 julia> nept=DEP([nep.A[1]',nep.A[2]'])
 julia> λ,v,u=rfi_b(nep,nept)
@@ -20,6 +21,7 @@ julia> compute_resnorm(nep,λ,v) % v is a right eigenvector
 4.347204570675246e-16
 julia> compute_resnorm(nept,λ,u) % u is a left eigenvector
 7.173081573164097e-16
+```
 
 # Reference
 *  Algorithm 4 in  Schreiber, Nonlinear Eigenvalue Problems: Newton-type Methods and Nonlinear Rayleigh Functionals, PhD thesis, TU Berlin, 2008.
@@ -85,14 +87,15 @@ function rfi(::Type{T},
 end
 
 """
- rfi_b(nep,nept,[λ=0,][errmeasure=default_errmeasure,][tol=eps()*100,][maxit=100,][v=randn,][u=randn,][displaylevel=1,][linsolvecreator=default_linsolvecreator,])
+    rfi_b(nep,nept,[λ=0,][errmeasure=default_errmeasure,][tol=eps()*100,][maxit=100,][v=randn,][u=randn,][displaylevel=1,][linsolvecreator=default_linsolvecreator,])
 
 This is an implementation of the two-sided Rayleigh functional Iteration(RFI)-Bordered version to compute an eigentriplet of the problem specified by 'nep'.
 This method requires the transpose of the NEP, specified in 'nept'. 
 'λ', 'u' and 'v' are initial guesses for the eigenvalue, the right eigenvector and the left eigenvector respectively.
 A 'NoConvergenceException' is thrown if an eigentriplet is not found in 'maxit' iterations.
 
-# Example:
+# Example
+```julia-repl
 julia> nep=nep_gallery("dep0");
 julia> nept=DEP([nep.A[1]',nep.A[2]'])
 julia> λ,v,u=rfi_b(nep,nept,v=ones(size(nep,1)))
@@ -100,6 +103,7 @@ julia> compute_resnorm(nep,λ,v) % v is a right eigenvector
 5.343670589284583e-15
 julia> compute_resnorm(nept,λ,u) % u is a left eigenvector
 5.271390516634306e-16
+```
 
 # Reference
 *  Algorithm 5 in  Schreiber, Nonlinear Eigenvalue Problems: Newton-type Methods and Nonlinear Rayleigh Functionals, PhD thesis, TU Berlin, 2008.
