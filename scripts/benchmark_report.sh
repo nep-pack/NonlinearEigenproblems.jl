6334e0d3ef1bd5be5e4dc12bd9a08f4782605728
Max Bennedich <bennedich@gmail.com>
2018-09-25 23:46:20 +0200

    Allow command line options in scripts

--- scripts/benchmark_report.sh
+++ scripts/benchmark_report.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-julia --color=yes -e 'using Pkg; Pkg.activate("."); include("test/benchmark/benchmark_report.jl")' "$@"
+julia --color=yes -e 'using Pkg; Pkg.activate("."); include("test/benchmark/benchmark_report.jl")' -- "$@"
