08e0b9f5a910efd0f7fdb717196118b73a7d98d1
Max Bennedich <bennedich@gmail.com>
2018-10-09 14:38:52 +0200

    NLEIGS: add test for naive gun (no low rank) #137

---
+++ test/nleigs/nleigs_gun_naive.jl
@@ -0,0 +1,10 @@
+using NonlinearEigenproblemsTest
+using NonlinearEigenproblems
+using Test
+
+@bench @testset "NLEIGS: Naive Gun" begin
+    nep = nep_gallery("nlevp_native_gun")
+    square = [-1-1im; -1+1im; 1+1im; 1-1im]
+    λ, X, _ = nleigs(nep, 150^2 .+ 200.0*square, displaylevel=displaylevel)
+    verify_lambdas(1, nep, λ, X)
+end
