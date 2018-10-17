dedbc38322b2a562085bfc7de6f465eb437ec513
Elias Jarlebring <jarlebring@gmail.com>
2018-10-17 13:25:15 +0200

    Create dir for profiling info #141

--- profiling/kthpdc_profile_init.jl
+++ profiling/kthpdc_profile_init.jl
@@ -1,18 +1,26 @@
-using Pkg;
+using Pkg, Dates;
 Pkg.activate("."); # Assumes . is the NonlinearEigenproblems directory
 Pkg.instantiate();
 
 
-# Profile some code
+hpc_script="hpc2.jl"; # Script to be run
+
+# Create a directory for the profiling data:
+datetime=mkdir(Dates.format(now(),"yyyy-mm-dd-HH:MM:SS"));
+profile_data_dir="prf_"*datetime*"_";
+profile_data_dir=profile_data_dir*replace(hpc_script,".jl" => "")
+mkdir(profile_data_dir)
+
+# Profile the code
 using Profile
-hpc_script="hpc3.jl"; # Script to be run
 include(hpc_script) # always run without profiling first to compile, etc
-@profile include(hpc_script)
+@profile include(hpc_script);
 
 # Extract profiling data to variables
 li, lidict = Profile.retrieve()
 
 # Store it to disk (non-compatible version)
 using Serialization
-open("profile_li.data", "w") do f; serialize(f, li); end
-open("profile_lidict.data", "w") do f; serialize(f, lidict); end
+
+open(joinpath(profile_data_dir,"profile_li.data"), "w") do f; serialize(f, li); end
+open(joinpath(profile_data_dir,"profile_lidict.data"), "w") do f; serialize(f, lidict); end
