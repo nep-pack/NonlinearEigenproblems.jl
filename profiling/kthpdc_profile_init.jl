1ffb339c5fc3f0eee098a3f8d30eab8f32c5ff2b
Max Bennedich <bennedich@gmail.com>
2018-10-17 15:49:11 +0200

    Profiling metadata #141

--- profiling/kthpdc_profile_init.jl
+++ profiling/kthpdc_profile_init.jl
@@ -13,8 +13,11 @@ mkdir(profile_data_dir)
 
 # Profile the code
 using Profile
+t1 = time_ns()
 include(hpc_script) # always run without profiling first to compile, etc
+t2 = time_ns()
 @profile include(hpc_script);
+t3 = time_ns()
 
 # Extract profiling data to variables
 li, lidict = Profile.retrieve()
@@ -24,3 +27,11 @@ using Serialization
 
 open(joinpath(profile_data_dir,"profile_li.data"), "w") do f; serialize(f, li); end
 open(joinpath(profile_data_dir,"profile_lidict.data"), "w") do f; serialize(f, lidict); end
+
+include("metadata_serialization.jl")
+file_name = joinpath(profile_data_dir, "metadata.json")
+total_time = (t3 - t1) / 1e9
+profile_time = (t3 - t2) / 1e9
+# TODO: pass any other relevant custom fields to the method below, such as the
+# hpc_script, or a manually entered comment
+save_metadata(file_name, total_time, profile_time)
