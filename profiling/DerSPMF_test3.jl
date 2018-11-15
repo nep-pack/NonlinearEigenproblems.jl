d63466d2583fe2770a917e5d13bd01c679574d33
gmele <gmele@kth.se>
2018-11-15 12:59:17 +0100

    DerSPMF: test initiated

--- profiling/DerSPMF_test3.jl
+++ profiling/DerSPMF_test3.jl
@@ -1,13 +1,13 @@
-using NonlinearEigenproblems, Random, SparseArrays, Revise, LinearAlgebra, BenchmarkTools
-
-
-nep=nep_gallery("dep0")
+A0=[1 3; 4 5]; A1=[3 4; 5 6];
+id_op=S -> one(S) # Note: We use one(S) to be valid both for matrices and scalars
+exp_op=S -> exp(S)
+nep=SPMF_NEP([A0,A1],[id_op,exp_op]);
 m=5
 σ1=3.5;
 Dnep=DerSPMF(nep,σ1,m)
 σ2=9;
 DDnep=DerSPMF(Dnep,σ2,m)
-V=randn(5);
+V=randn(2,5);
 n1=compute_Mlincomb(nep,0,V)-compute_Mlincomb(Dnep,0,V)
 n2=compute_Mlincomb(nep,σ1,V)-compute_Mlincomb(Dnep,σ1,V)
 n3=compute_Mlincomb(nep,σ2,V)-compute_Mlincomb(Dnep,σ2,V)
--- test/core.jl
+++ test/core.jl
@@ -92,3 +92,21 @@ end
     z2=compute_Mlincomb_from_MM(nep,λ,V,a)
     @test norm(z-z2)<sqrt(eps())*100
 end
+
+# compute_Mlincomb for DerSPMF
+@bench @testset "compute_Mlincomb DerSPMF" begin
+    A0=[1 3; 4 5]; A1=[3 4; 5 6];
+    id_op=S -> one(S) # Note: We use one(S) to be valid both for matrices and scalars
+    exp_op=S -> exp(S)
+    nep=SPMF_NEP([A0,A1],[id_op,exp_op]);
+    m=5; σ1=3.5; σ2=9;
+    Dnep=DerSPMF(nep,σ1,m)
+    DDnep=DerSPMF(Dnep,σ2,m)
+    V=randn(2,5);
+    @test norm( compute_Mlincomb(nep,0,V)-compute_Mlincomb(Dnep,0,V) )<sqrt(eps())*100
+    @test norm( compute_Mlincomb(nep,σ1,V)-compute_Mlincomb(Dnep,σ1,V) )<sqrt(eps())*100
+    @test norm( compute_Mlincomb(nep,σ2,V)-compute_Mlincomb(Dnep,σ2,V) )<sqrt(eps())*100
+    @test norm( compute_Mlincomb(nep,0,V)-compute_Mlincomb(DDnep,0,V) )<sqrt(eps())*100
+    @test norm( compute_Mlincomb(nep,σ1,V)-compute_Mlincomb(DDnep,σ1,V) )<sqrt(eps())*100
+    @test norm( compute_Mlincomb(nep,σ2,V)-compute_Mlincomb(DDnep,σ2,V) )<sqrt(eps())*100
+end
