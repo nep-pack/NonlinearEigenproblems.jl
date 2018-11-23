2d30c71e66f9293c55f0cfedfcb0ecb75c32e4e1
meleg <gmele@kth.se>
2018-11-23 06:46:54 +0100

    InfLan: framework for precomputation in SPMF fixed.

--- infLan_testing/test_dep_debug.jl
+++ infLan_testing/test_dep_debug.jl
@@ -2,8 +2,6 @@ using NonlinearEigenproblems, Random, SparseArrays, Revise
 import ..NEPSolver.ilan;
 include("../src/method_ilan.jl");
 
-
-
 n=10000
 Random.seed!(1) # reset the random seed
 K = [1:n;2:n;1:n-1]; J=[1:n;1:n-1;2:n]
@@ -16,6 +14,15 @@ nep=DEP([A1,A2,A3],[0,1,0.8])
 
 v0=rand(n)
 V,H,ω,HH=ilan_benchmark(nep,σ=0,γ=1;Neig=10,displaylevel=1,maxit=10,tol=eps()*100,check_error_every=1,v=v0)
+
+
+f1= S -> -S
+f2= S -> one(S)
+f3= S -> exp(-S)
+f4= S -> exp(-0.8*S)
+nep=SPMF_NEP([one(A1),A1,A2,A3],[f1,f2,f3,f4])
+
+
 V2,H2,ω2,HH2=ilan(nep,σ=0,γ=1;Neig=10,displaylevel=1,maxit=10,tol=eps()*100,check_error_every=1,v=v0)
 
 display(norm(V-V2))
--- test/ilan_test.jl
+++ test/ilan_test.jl
@@ -11,22 +11,48 @@ include("../src/method_ilan.jl");
 
 @testset "ILAN" begin
 
-    n=1000
-    Random.seed!(1) # reset the random seed
-    K = [1:n;2:n;1:n-1]; J=[1:n;1:n-1;2:n]
-    A1 = sparse(K, J, rand(3*n-2)); A1 = A1+A1';
-    A2 = sparse(K, J, rand(3*n-2)); A2 = A2+A2';
-    A3 = sparse(K, J, rand(3*n-2)); A3 = A3+A3';
+    @testset "DEP" begin
+        n=1000
+        Random.seed!(1) # reset the random seed
+        K = [1:n;2:n;1:n-1]; J=[1:n;1:n-1;2:n]
+        A1 = sparse(K, J, rand(3*n-2)); A1 = A1+A1';
+        A2 = sparse(K, J, rand(3*n-2)); A2 = A2+A2';
+        A3 = sparse(K, J, rand(3*n-2)); A3 = A3+A3';
 
-    nep=DEP([A1,A2,A3],[0,1,0.8])
-    v0=rand(n)
-    V,H,ω,HH=ilan_benchmark(nep,σ=0,γ=1;Neig=10,displaylevel=1,maxit=10,tol=eps()*100,check_error_every=1,v=v0)
-    V2,H2,ω2,HH2=ilan(nep,σ=0,γ=1;Neig=10,displaylevel=1,maxit=10,tol=eps()*100,check_error_every=1,v=v0)
+        nep=DEP([A1,A2,A3],[0,1,0.8])
+        v0=rand(n)
+        V,H,ω,HH=ilan_benchmark(nep,σ=0,γ=1;Neig=10,displaylevel=1,maxit=10,tol=eps()*100,check_error_every=1,v=v0)
+        V2,H2,ω2,HH2=ilan(nep,σ=0,γ=1;Neig=10,displaylevel=1,maxit=10,tol=eps()*100,check_error_every=1,v=v0)
 
-    @test norm(V-V2)<sqrt(eps())
-    @test norm(H-H2)<sqrt(eps())
-    @test norm(ω-ω2)<sqrt(eps())
-    @test norm(HH-HH2)<sqrt(eps())
+        @test norm(V-V2)<sqrt(eps())
+        @test norm(H-H2)<sqrt(eps())
+        @test norm(ω-ω2)<sqrt(eps())
+        @test norm(HH-HH2)<sqrt(eps())
+    end
+
+    @testset "SPMF and DEP" begin
+
+        n=1000
+        Random.seed!(1) # reset the random seed
+        K = [1:n;2:n;1:n-1]; J=[1:n;1:n-1;2:n]
+        A1 = sparse(K, J, rand(3*n-2)); A1 = A1+A1';
+        A2 = sparse(K, J, rand(3*n-2)); A2 = A2+A2';
+        A3 = sparse(K, J, rand(3*n-2)); A3 = A3+A3';
+
+        nep=DEP([A1,A2,A3],[0,1,0.8])
+        v0=rand(n)
+        V,H,ω,HH=ilan_benchmark(nep,σ=0,γ=1;Neig=10,displaylevel=1,maxit=10,tol=eps()*100,check_error_every=1,v=v0)
+
+        f1= S -> -S; f2= S -> one(S); f3= S -> exp(-S); f4= S -> exp(-0.8*S)
+        nep=SPMF_NEP([one(A1),A1,A2,A3],[f1,f2,f3,f4])
+        V2,H2,ω2,HH2=ilan(nep,σ=0,γ=1;Neig=10,displaylevel=1,maxit=10,tol=eps()*100,check_error_every=1,v=v0)
+
+        @test norm(V-V2)<sqrt(eps())
+        @test norm(H-H2)<sqrt(eps())
+        @test norm(ω-ω2)<sqrt(eps())
+        @test norm(HH-HH2)<sqrt(eps())
+
+    end
 
 
 end
