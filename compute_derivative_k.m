cbf053ab1d64ee398ca0473c609048bffc894bf4
Elias Jarlebring <jarlebring@gmail.com>
2019-01-21 20:20:39 +0100

    refresh docs

---
+++ compute_derivative_k.m
@@ -0,0 +1,14 @@
+function Z=compute_derivative_k(s,k)
+     randn('seed',0);
+     n=10;
+     A0=randn(n,n); A1=randn(n,n);
+     Z=zeros(n,n);
+     if (k==0)
+         Z=A0+s*A1;
+     end
+     if (k==1)
+         Z=A1;
+     end
+     Z=Z+(A1^k)*expm(s*A1);
+
+end
