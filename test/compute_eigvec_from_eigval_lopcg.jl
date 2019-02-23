ac936fdeec571551e78fb9137abd93a1fce70b30
Elias Jarlebring <jarlebring@gmail.com>
2019-02-23 10:35:52 +0100

    Fixes #65

--- test/compute_eigvec_from_eigval_lopcg.jl
+++ test/compute_eigvec_from_eigval_lopcg.jl
@@ -7,12 +7,14 @@ using Test
 import NonlinearEigenproblems.NEPCore.compute_Mlincomb
 
 @testset "compute eigvec lopcg" begin
+    # Arbitrary vectors. We avoid random in unit tests
+    v0=Vector{Float64}(cos.(3.2 .+ (1:100))); normalize!(v0);
+    v1=Vector{Float64}(sin.(1:100)); normalize!(v0);
     @bench @testset "dep0_sparse" begin
         nep = nep_gallery("dep0_sparse", 100);
         nept = DEP([copy(nep.A[1]'), copy(nep.A[2]')], nep.tauv);
-        Random.seed!(13) # this results in a good start vector
-        λ,Q,err = iar(nep,maxit=100,Neig=2,σ=1.0,γ=1,displaylevel=0,check_error_every=1);
-        v=compute_eigvec_from_eigval_lopcg(nep,nept,λ[1]);
+        λ,Q,err = iar(nep,maxit=100,Neig=2,σ=1.0,γ=1,displaylevel=0,check_error_every=1,v=v0);
+        v=compute_eigvec_from_eigval_lopcg(nep,nept,λ[1],x=v1);
         errormeasure=default_errmeasure(nep);
         @test errormeasure(λ[1],v)<1e-5;
     end
@@ -20,8 +22,8 @@ import NonlinearEigenproblems.NEPCore.compute_Mlincomb
     @bench @testset "pep0" begin
         nep = nep_gallery("pep0", 100);
         nept = PEP([copy(nep.A[1]'), copy(nep.A[2]'), copy(nep.A[3]')])
-        λ,Q,err = iar(nep,maxit=100,Neig=2,σ=1.0,γ=1,displaylevel=0,check_error_every=1);
-        v=compute_eigvec_from_eigval_lopcg(nep,nept,λ[1]);
+        λ,Q,err = iar(nep,maxit=100,Neig=2,σ=1.0,γ=1,displaylevel=0,check_error_every=1,v=v0);
+        v=compute_eigvec_from_eigval_lopcg(nep,nept,λ[1],x=v1);
         errormeasure=default_errmeasure(nep);
         @test errormeasure(λ[1],v)<1e-5;
     end
