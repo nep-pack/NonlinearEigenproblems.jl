a3c9e661f1be0298a6a5b69c85720fbfdc6e0ab6
Elias Jarlebring <jarlebring@gmail.com>
2019-03-10 19:41:46 +0100

    documentation

--- src/errmeasure.jl
+++ src/errmeasure.jl
@@ -6,18 +6,92 @@ export ResidualErrmeasure
 export DefaultErrmeasure
 
 
+"""
+    abstract type Errmeasure; end
 
+Concrete subtypes of `Errmeasure` represent specific ways of
+measuring the error of an eigenpair. NEP-solvers take a type as
+input and then instantiate a `Errmeasure`. As a NEP-solver user,
+you use the type as follows
+```julia
+julia> quasinewton(nep,errmeasure=ResidualErrmeasure)
+```
+User-specified ways of measuring error can be given by
+creating a new subtype of `Errmeasure` and using it as a
+`errmeasure` keyword. You need to specify the way to
+measure the error in the method `estimate_error`.
+
+# Example
+This shows how to compute a reference solution and
+then use this as a reference solution. The error
+in the second run will be effectively the
+eigenvalue error.
+```julia
+julia> nep=nep_gallery("qdep0");
+julia> (λref,vref)=quasinewton(nep,λ=-1);
+julia> struct EigvalError <: Errmeasure; nep::NEP; end
+julia> function NonlinearEigenproblems.estimate_error(E::EigvalError,λ,v)
+return abs(λref-λ);
+end
+julia> (λ,v)=quasinewton(nep,errmeasure=EigvalError,λ=-1.0 ,displaylevel=1,tol=5e-13)
+Precomputing linsolver
+Iteration:  1 errmeasure:2.466988587467300320e-03, λ=-1.0 + 0.0im
+Iteration:  2 errmeasure:4.625160667012763183e-01, λ=-0.539950921886191 + 0.0im
+Iteration:  3 errmeasure:2.799848726755167494e-01, λ=-1.282451861262984 + 0.0im
+Iteration:  4 errmeasure:3.422925625951256379e-02, λ=-1.0366962448469799 + 0.0im
+Iteration:  5 errmeasure:5.530128437585268841e-04, λ=-1.0019139757437088 + 0.0im
+Iteration:  6 errmeasure:1.159388768512403800e-04, λ=-1.002351049710616 + 0.0im
+Iteration:  7 errmeasure:2.658434455016234210e-06, λ=-1.0024643301530123 + 0.0im
+Iteration:  8 errmeasure:1.726871190488310503e-07, λ=-1.0024668159003483 + 0.0im
+Iteration:  9 errmeasure:4.819693533164581822e-09, λ=-1.0024669934071608 + 0.0im
+Iteration: 10 errmeasure:5.234268574128009277e-10, λ=-1.0024669880640404 + 0.0im
+Iteration: 11 errmeasure:3.762568034915148019e-11, λ=-1.002466988625093 + 0.0im
+Iteration: 12 errmeasure:3.205657961302676995e-12, λ=-1.0024669885842616 + 0.0im
+Iteration: 13 errmeasure:3.352873534367972752e-14, λ=-1.0024669885874338 + 0.0im
+```
+
+See also: [`DefaultErrmeasure`](@ref), [`ResidualErrmeasure`](@ref), [`BackwardErrmeasure`](@ref), [`estimate_error`](@ref), [`init_errmeasure`](@ref).
+
+"""
 abstract type Errmeasure; end
 
+"""
+    function init_errmeasure(E::Errmeasure,nep)
 
+This function is called in a precomputation phase for an error measure.
+For user defined `Errmeasure`, you do not need to overload this
+if your `Errmeasure` contains only one field which is a `NEP`.
+
+See also: [`Errmeasure`](@ref)
+"""
 function init_errmeasure(E::Type{<:Errmeasure},nep::NEP)
     return E(nep);
 end
 
+"""
+    function estimate_error(E::Errmeasure,λ,v)
+
+Returns the error estimate for the eigenpair `(λ,v)`. The way to measure
+the error is specified in `Errmeasure`.
+
+See also: [`Errmeasure`](@ref)
+
+
+"""
 function estimate_error(errdata::Errmeasure,λ,v)
     error("Not implemented:",errdata);
 end
 
+
+"""
+    struct ResidualErrmeasure <: Errmeasure
+
+This `Errmeasure` species that the residual norm should be
+used to measure the error.
+
+See also: [`Errmeasure`](@ref)
+
+"""
 struct ResidualErrmeasure <: Errmeasure;
     nep::NEP
 end
@@ -27,6 +101,21 @@ function estimate_error(errdata::ResidualErrmeasure, λ,v)
 end
 
 
+"""
+    struct BackwardErrmeasure <: Errmeasure
+
+This `Errmeasure` gives a way to compute the backward error.
+The backward error estimate are only given for NEPs which are
+subtypes of  `AbstractSPMF`.
+
+# Example
+```julia
+julia> nep=nep_gallery("qdep0");
+julia> (λ,v)=quasinewton(nep,λ=-1,errmeasure=BackwardErrmeasure,tol=1e-10);
+```
+See also: [`Errmeasure`](@ref)
+
+"""
 struct BackwardErrmeasure{X<:Real} <: Errmeasure
     nep::NEP
     coeffs::Vector{X};
