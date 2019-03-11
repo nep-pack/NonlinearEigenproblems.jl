2eeacdb90aa4d4ce14b8ba42fcf1636eb49b8cee
Elias Jarlebring <jarlebring@gmail.com>
2019-03-11 14:50:48 +0100

    refresh docs

--- search_index.js
+++ search_index.js
@@ -701,7 +701,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Error measure",
     "title": "Measuring the error",
     "category": "section",
-    "text": "** This only works on NEP-PACK version 0.2.6 and onwards **All iterative algorithms need some form of termination criteria. In NEP-PACK, all NEP-solvers provide the possibility to specify the desired tolerance, as well as how the error is measured or estimated. The tolerance is specified in the kwarg  tol (which is a real number) and the way to measure the error is given in errmeasure."
+    "text": "This only works on NEP-PACK version 0.2.6 and onwardsAll iterative algorithms need some form of termination criteria. In NEP-PACK, all NEP-solvers provide the possibility to specify the desired tolerance, as well as how the error is measured or estimated. The tolerance is specified in the kwarg  tol (which is a real number) and the way to measure the error is given in errmeasure."
 },
 
 {
@@ -709,7 +709,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Error measure",
     "title": "Standard usage",
     "category": "section",
-    "text": "NEP-PACK comes with several ways to measure errors for many NEP-types.errmeasure=ResidualErrmeasure: The error is estimated by the useof the residual norm:mathrm err=fracM(λ)vverrmeasure=BackwardErrmeasure: The error is estimated by using the backward error bounds. This error measure will not work for all NEPs. Default implementation is provided for any AbstractSPMF. If your NEP is an AbstractSPMF with terms:M(λ)=A_1f_1(λ)+cdots+A_mf_m(λ)the error will be estimated bymathrm err=fracM(λ)vv\nfrac1A_1_Ff_1(λ)+cdots+A_m_Ff_m(λ)In other words, the BackwardErrmeasure is a weighting of the ResidualErrmeasure.errmeasure=DefaultErrmeasure: When this errmeasure is specified, NEP-PACK tries to determine a error measure for you. In general, BackwardErrmeasure will be preferred if possible.Example:Most NEP-solvers take the errmeasure as an kwarg.julia> nep=nep_gallery(\"qdep0\");\njulia> # Solve the problem to residual norm 1e-8\njulia> (λ,v)=mslp(nep,errmeasure=ResidualErrmeasure,tol=1e-8)\njulia> norm(compute_Mlincomb(nep,λ,v))/norm(v) # It\'s smaller than tol?\n3.503700819937386e-9\njulia> nep isa AbstractSPMF # Is it an AbstractSPMF so we can use BackwardErrmeasure?\ntrue\njulia> (λ,v)=mslp(nep,errmeasure=BackwardErrmeasure,tol=1e-10)\njulia> factor=abs(fv[1](λ))*norm(Av[1])+\n     abs(fv[2](λ))*norm(Av[2])+abs(fv[3](λ))*norm(Av[3]);\njulia> norm(compute_Mlincomb(nep,λ,v))/(norm(v)*factor)\n1.659169482386331e-11"
+    "text": "NEP-PACK comes with several ways to measure errors for many NEP-types.errmeasure=ResidualErrmeasure: The error is estimated by the use of the residual norm:mathrmerr=fracM(λ)vverrmeasure=BackwardErrmeasure: The error is estimated by using the backward error bounds. This error measure will not work for all NEPs. An implementation is provided for any AbstractSPMF. If your NEP is an AbstractSPMF with terms:\nM(λ)=A_1f_1(λ)+cdots+A_mf_m(λ)\nthe error will be estimated by\nmathrmerr=fracM(λ)vvfrac1A_1_Ff_1(λ)+cdots+A_m_Ff_m(λ)\nIn other words, the BackwardErrmeasure is a weighting of the ResidualErrmeasure.\nerrmeasure=DefaultErrmeasure: When this errmeasure is specified, NEP-PACK tries to determine a error measure for you. In general, BackwardErrmeasure will be preferred if possible. This behavior may change in future versions of NEP-PACK.Example: Most NEP-solvers take the errmeasure as an kwarg.julia> nep=nep_gallery(\"qdep0\");\njulia> # Solve the problem to residual norm 1e-8\njulia> (λ,v)=mslp(nep,errmeasure=ResidualErrmeasure,tol=1e-8)\njulia> norm(compute_Mlincomb(nep,λ,v))/norm(v) # It\'s smaller than tol?\n3.503700819937386e-9\njulia> nep isa AbstractSPMF # Is it an AbstractSPMF so we can use BackwardErrmeasure?\ntrue\njulia> (λ,v)=mslp(nep,errmeasure=BackwardErrmeasure,tol=1e-10)\njulia> factor=abs(fv[1](λ))*norm(Av[1])+\n     abs(fv[2](λ))*norm(Av[2])+abs(fv[3](λ))*norm(Av[3]);\njulia> norm(compute_Mlincomb(nep,λ,v))/(norm(v)*factor)\n1.659169482386331e-11"
 },
 
 {
@@ -721,19 +721,19 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "errmeasure/#Function-handle-1",
+    "location": "errmeasure/#User-defined-error-1:-Function-handle-1",
     "page": "Error measure",
-    "title": "Function handle",
+    "title": "User defined error 1: Function handle",
     "category": "section",
-    "text": "The easiest is that the user gives a function handle which is called to obtain the error. The errmeasure can be a function, which takes two parameters as input (λ,v) and returns the error The most common situation is that you want to report the error (as a function of iteration) with a reference solutions.  If we want to get a very accurate approximation of the true error, we can run the  algorithm twice, and the second time we run the algorithm we use the result of the first run as a reference. julia> nep=nep_gallery(\"qdep0\");\njulia> v0=ones(size(nep,1));\njulia> (λref,v)=resinv(nep,v=v0,λ=230^2+1im,displaylevel=1);\njulia> myerrmeasure = (λ,v) -> abs(λ-λref)/abs(λ);\njulia> (λ,v)=resinv(nep,v=v0,λ=250^2+1im,displaylevel=1,tol=1e-10,errmeasure=myerrmeasure);\nIteration:  1 errmeasure:1.274091618457501296e-01 \nIteration:  2 errmeasure:9.535794095609478882e-01 \n...\nIteration: 49 errmeasure:1.269396691930517923e-10 \nIteration: 50 errmeasure:7.608430406801784718e-11 "
+    "text": "The user can provide a function handle which is called to obtain the error. The errmeasure can be a function, which takes two parameters as input (λ,v) and returns the error (or estimate thereof).The most common situation is that you want to report the error (as a function of iteration) with a reference solutions.  If we want to get a very accurate approximation of the true error, we can run the  algorithm twice, and the second time we run the algorithm we use the result of the first run as a reference. julia> nep=nep_gallery(\"qdep0\");\njulia> v0=ones(size(nep,1));\njulia> (λref,v)=resinv(nep,v=v0,λ=230^2+1im,displaylevel=1);\njulia> myerrmeasure = (λ,v) -> abs(λ-λref)/abs(λ);\njulia> (λ,v)=resinv(nep,v=v0,λ=250^2+1im,displaylevel=1,tol=1e-10,errmeasure=myerrmeasure);\nIteration:  1 errmeasure:1.274091618457501296e-01 \nIteration:  2 errmeasure:9.535794095609478882e-01 \n...\nIteration: 49 errmeasure:1.269396691930517923e-10 \nIteration: 50 errmeasure:7.608430406801784718e-11 "
 },
 
 {
-    "location": "errmeasure/#A-user-defined-type-1",
+    "location": "errmeasure/#User-defined-error-2:-A-user-defined-type-1",
     "page": "Error measure",
-    "title": "A user defined type",
+    "title": "User defined error 2: A user defined type",
     "category": "section",
-    "text": "Julia can be faster when we use types instead of function handles. It is  possible to provide carry out the same simulation as above with a user defined type. You first need to define a new typejulia> struct RefErrmeasure <: Errmeasure; nep::NEP; endThe error measure should then provided in the function  estimate_error:julia> v0=ones(size(nep,1));\njulia> (λref,v)=resinv(nep,v=v0,λ=230^2+1im,displaylevel=1);\njulia> function NonlinearEigenproblems.estimate_error(e::RefErrmeasure,λ,v)\n         return abs(λ-λref)/abs(λ);\n       end\njulia> (λ,v)=resinv(nep,v=v0,λ=250^2+1im,displaylevel=1,tol=1e-10,errmeasure=RefErrmeasure);\nIteration:  1 errmeasure:1.274091618457501296e-01 \n...\nIteration: 49 errmeasure:1.269396691930517923e-10 \nIteration: 50 errmeasure:7.608430406801784718e-11 "
+    "text": "Due to the multiple dispatch and handling of types in Julia, code can be faster when if use types instead of function handles. It is  possible to provide carry out the same simulation as above with a user defined type. You first need to define a new typejulia> struct RefErrmeasure <: Errmeasure; nep::NEP; endThe error measure should then provided in the function  estimate_error:julia> v0=ones(size(nep,1));\njulia> (λref,v)=resinv(nep,v=v0,λ=230^2+1im,displaylevel=1);\njulia> function NonlinearEigenproblems.estimate_error(e::RefErrmeasure,λ,v)\n         return abs(λ-λref)/abs(λ);\n       end\njulia> (λ,v)=resinv(nep,v=v0,λ=250^2+1im,displaylevel=1,tol=1e-10,errmeasure=RefErrmeasure);\nIteration:  1 errmeasure:1.274091618457501296e-01 \n...\nIteration: 49 errmeasure:1.269396691930517923e-10 \nIteration: 50 errmeasure:7.608430406801784718e-11 "
 },
 
 {
@@ -741,7 +741,15 @@ var documenterSearchIndex = {"docs": [
     "page": "Error measure",
     "title": "As a NEP-solver developer",
     "category": "section",
-    "text": "NEP-solvers should use the Errmeasure as follows. The NEP-solver should take as input a ErrmeasureType (which corresponds to either a function or a type, but this you will not have to worry about).function mysolver(nep::NEP;errmeasure::ErrmeasureType=DefaultErrmeasure)Before the main iteration, you need to initialize the error measure computation. The precomptued data  is stored in a variable typically called ermdata:    ermdata=init_errmeasure(errmeasure,nep);In the main for loop you want to call the estimate_error function:for k=1:maxit\n    err=estimate_error(ermdata,λ,v)\n    if (err < 1e-10)\n       return (λ,v)\n    end \n    ....\n\nend "
+    "text": "NEP-solvers should use the Errmeasure as follows. The NEP-solver should take as input a ErrmeasureType. This corresponds to either a function or a type, but if you follow the procedure below, you will not have to worry about that.function mysolver(nep::NEP;errmeasure::ErrmeasureType=DefaultErrmeasure)Before the main iteration, you need to initialize the error measure computation. The precomptued data  is stored in a variable typically called ermdata:    ermdata=init_errmeasure(errmeasure,nep);In the main for loop you want to call the estimate_error function:for k=1:maxit\n    err=estimate_error(ermdata,λ,v)\n    if (err < 1e-10)\n       return (λ,v)\n    end \n    ....\n\nend "
+},
+
+{
+    "location": "errmeasure/#Methods-and-types-1",
+    "page": "Error measure",
+    "title": "Methods and types",
+    "category": "section",
+    "text": "NonlinearEigenproblems.NEPTypes.ErrmeasureNonlinearEigenproblems.NEPTypes.estimate_errorNonlinearEigenproblems.NEPTypes.init_errmeasure"
 },
 
 {
