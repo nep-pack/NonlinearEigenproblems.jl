fe7e10ddbdf0e50a69971d3350a03c004d86edd6
Emil Ringh <eringh@kth.se>
2019-06-20 10:47:41 +0200

    ILAN_Benchmark: Removed erroneous docs.

--- src/method_ilan_benchmark.jl
+++ src/method_ilan_benchmark.jl
@@ -6,31 +6,7 @@ using Random
 using Statistics
 
 """
-    iar(nep,[maxit=30,][σ=0,][γ=1,][linsolvecreator=default_linsolvecreator,][tolerance=eps()*10000,][Neig=6,][errmeasure,][v=rand(size(nep,1),1),][displaylevel=0,][check_error_every=1,][orthmethod=DGKS])
-
-Run the infinite Arnoldi method on the nonlinear eigenvalue problem stored in `nep`.
-
-The target `σ` is the center around which eiganvalues are computed. The kwarg `errmeasure` is a function handle which can be used to specify how the error is measured to be used in termination (default is absolute residual norm). A Ritz pair `λ` and `v` is flagged a as converged (to an eigenpair) if `errmeasure` is less than `tol`. The vector
-`v` is the starting vector for constructing the Krylov space. The orthogonalization method, used in contructing the orthogonal basis of the Krylov space, is specified by `orthmethod`, see the package `IterativeSolvers.jl`. The iteration
-is continued until `Neig` Ritz pairs converge. This function throws a `NoConvergenceException` if the wanted eigenpairs are not computed after `maxit` iterations. The `linsolvercreator` is a function which specifies how the linear system is created and solved.
-
-
-# Example
-```julia-repl
-julia> using NonlinearEigenproblems, LinearAlgebra
-julia> nep=nep_gallery("dep0",100);
-julia> v0=ones(size(nep,1));
-julia> λ,v=iar(nep;v=v0,tol=1e-5,Neig=3);
-julia> norm(compute_Mlincomb!(nep,λ[1],v[:,1])) # Is it an eigenvalue?
-julia> λ    # print the computed eigenvalues
-3-element Array{Complex{Float64},1}:
- -0.15606211475666945 - 0.12273439802763578im
- -0.15606211475666862 + 0.12273439802763489im
-  0.23169243065648365 - 9.464790582509696e-17im
-```
-
-# References
-* Algorithm 2 in Jarlebring, Michiels Meerbergen, A linear eigenvalue algorithm for the nonlinear eigenvalue problem, Numer. Math, 2012
+    ilan_benchmark()
 """
 ilan_benchmark(nep::NEP;params...)=ilan_benchmark(ComplexF64,nep;params...)
 function ilan_benchmark(
