133cebf55d9d15e54ee0ed7381641e4cda045df0
Emil Ringh <eringh@kth.se>
2019-06-20 10:43:51 +0200

    TIAR: Minor (indentation)

--- src/method_tiar.jl
+++ src/method_tiar.jl
@@ -225,9 +225,9 @@ function tiar(
     k=k-1
     # NoConvergenceException
     if conv_eig<Neig && Neig != Inf
-       err=err[end,1:Neig];
-       idx=sortperm(err); # sort the error
-       λ=λ[idx];  Q=Q[:,idx]; err=err[idx];
+        err=err[end,1:Neig];
+        idx=sortperm(err); # sort the error
+        λ=λ[idx];  Q=Q[:,idx]; err=err[idx];
         msg="Number of iterations exceeded. maxit=$(maxit)."
         if conv_eig<3
             msg=string(msg, " Check that σ is not an eigenvalue.")
