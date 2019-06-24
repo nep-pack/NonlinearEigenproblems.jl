5e2a7f47fffca1fa9f334662778eeb4e133ec420
Emil Ringh <eringh@kth.se>
2019-06-24 16:38:38 +0200

    IAR/TIAR: More comments

--- src/method_tiar.jl
+++ src/method_tiar.jl
@@ -5,9 +5,7 @@ using LinearAlgebra
 using Random
 
 """
-    tiar(nep,[maxit=30,][σ=0,][γ=1,][linsolvecreator=default_linsolvecreator,][tolerance=eps()*10000,][Neig=6,][errmeasure,]
-[v=rand(size(nep,1),1),][logger=0,][check_error_every=1,][orthmethod=DGKS,]
-[proj_solve=false,][inner_solver_method=DefaultInnerSolver,][inner_logger=0])
+    tiar(nep,[maxit=30,][σ=0,][γ=1,][linsolvecreator=default_linsolvecreator,][tolerance=eps()*10000,][Neig=6,][errmeasure,][v=rand(size(nep,1),1),][logger=0,][check_error_every=1,][orthmethod=DGKS,][proj_solve=false,][inner_solver_method=DefaultInnerSolver,][inner_logger=0])
 
 Run the tensor infinite Arnoldi method on the nonlinear eigenvalue problem stored in `nep`. This is equivalent to `iar`, but handles orthogonalization with
 a tensor representation.
