5af75984f4d0d566a58e6a8c55022eed22f5b94e
Elias Jarlebring <jarlebring@gmail.com>
2019-07-19 23:25:16 +0200

    tolerance typo

--- test/contour_block_SS.jl
+++ test/contour_block_SS.jl
@@ -9,5 +9,5 @@ using LinearAlgebra
 @testset "block SS" begin
     nep=nep_gallery("dep0",3)
     λ,V=contour_block_SS(nep,radius=1.0,N=1000,σ=0.1,K=3,L=3);
-    @test norm(compute_Mlincomb(nep,λ[1],V[:,1])) ≈ sqrt(eps())
+    @test norm(compute_Mlincomb(nep,λ[1],V[:,1])) < sqrt(eps())
 end
