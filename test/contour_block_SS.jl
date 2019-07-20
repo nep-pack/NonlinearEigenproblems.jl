f79b369e774fe32f6132c7b0b80a6231ee65c92a
Elias Jarlebring <jarlebring@gmail.com>
2019-07-20 09:09:37 +0200

    contour integrals in documentation

--- test/contour_block_SS.jl
+++ test/contour_block_SS.jl
@@ -8,6 +8,6 @@ using LinearAlgebra
 
 @testset "block SS" begin
     nep=nep_gallery("dep0",3)
-    λ,V=contour_block_SS(nep,radius=1.0,N=1000,σ=0.1,k=3,L=3);
+    λ,V=contour_block_SS(nep,radius=1.0,N=1000,σ=0.1,k=3,K=3);
     @test norm(compute_Mlincomb(nep,λ[1],V[:,1])) < sqrt(eps())
 end
