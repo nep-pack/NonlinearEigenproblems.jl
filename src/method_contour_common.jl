71a9901aaf61aa0896632c229ce570ff87c799e0
Elias Jarlebring <jarlebring@gmail.com>
2019-07-22 20:40:44 +0200

    comments

--- src/method_contour_common.jl
+++ src/method_contour_common.jl
@@ -4,8 +4,53 @@ using Distributed, LinearAlgebra, Random
 
 export MatrixIntegrator, MatrixTrapezoidal;
 
+"""
+    abstract type MatrixIntegrator
+
+This type is used for integration of (matrix valued)
+functions with a particular structure. It
+is used in `contour_beyn` and `contour_block_SS`.
+
+In order to specify your own way to
+do numerical quadrature you should inherit from this
+type
+```
+julia> abstract type MyIntegrator <: MatrixIntegrator; end
+```
+and implement the method `integrate_interval`:
+```
+julia> import NonlinearEigenproblems.NEPSolver.integrate_interval
+julia> function integrate_interval(ST::Type{MyType},::Type{T},f,gv,a,b,N,logger) where {T<:Number}
+```
+The function `integrate_interval` should integrate functions on the interval `[a,b]` with `N` quadrature points. Further parameters:
+
+* The type `T` (typically `ComplexF64`) specifies what the target number type is.
+* The `logger::Logger` specifies if / how things should written to the log.
+* `f::Function`, takes a scalar input (in the interval `[a,b]`) and returns a matrix or a vector.
+* `gv::Vector{Function}` which takes scalar input (in the interval `[a,b]`)  and returns scalars.
+
+The function `integrate_interval` should return a tensor `I`
+where the last dimension is `length(gv)` and should contain the
+integrals. More precisely,
+ `I[:,:,1]`, ... `I[:,:,m]` should contain approximations
+of the product of `f(x)gv[1](x)`,...`f(x)gv[m](x)` over
+`[a,b]`, e.g., `I[:,:,1]` should contain an approximation of
+```math
+\\int_a^b f(x)g_1(x)\\,dx
+```
+See also: `contour_beyn`, `contour_block_SS`, `MatrixTrapezoidal`
+"""
 abstract type MatrixIntegrator ; end
 
+
+"""
+    abstract type MatrixTrapezoidal <: MatrixIntegrator; end
+
+Implements the trapezoidal rule to be used
+with quadrature methods.
+
+See also: `contour_beyn`, `contour_block_SS`, `MatrixIntegrator`
+"""
 abstract type MatrixTrapezoidal <: MatrixIntegrator; end
 
 
