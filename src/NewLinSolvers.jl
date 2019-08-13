6cfc402617283465cb7aa6d39434e9a5fafa1327
Elias Jarlebring <jarlebring@gmail.com>
2019-08-13 13:13:06 +0200

    Factorization recycling

--- src/NewLinSolvers.jl
+++ src/NewLinSolvers.jl
@@ -19,9 +19,9 @@ end
 struct FactorizeLinSolverCreator{T_values,T_factor} <: LinSolverCreator
     umfpack_refinements::Int;
     recycled_factorizations::Dict{T_values,T_factor};
-    recycle_factorizations::Bool;
+    max_factorizations::Int
     function FactorizeLinSolverCreator(;umfpack_refinements::Int=1,
-                                       recycle_factorizations=false,
+                                       max_factorizations=0,
                                        nep=nothing,
                                        precomp_values=[]
                                        )
@@ -31,18 +31,27 @@ struct FactorizeLinSolverCreator{T_values,T_factor} <: LinSolverCreator
         end
 
         # Compute all the factorizations
+        local precomp_factorizations
+
         precomp_factorizations=map(s-> factorize(compute_Mder(nep,s)), precomp_values);
 
 
         # Put them in a dict
         T_from=eltype(precomp_values);
-        T_to=eltype(precomp_factorizations);
+        local T_to,T_from
+        if (size(precomp_values,1)>0)
+            T_to=eltype(precomp_factorizations);
+        else
+            T_to=Any;
+            T_from=Number;
+        end
+
         dict=Dict{T_from,T_to}();
         for i=1:size(precomp_values,1)
             dict[precomp_values[i]]=precomp_factorizations[i];
         end
 
-        return new{T_from,T_to}(umfpack_refinements,dict,recycle_factorizations)
+        return new{T_from,T_to}(umfpack_refinements,dict,max_factorizations)
 
     end
 end
@@ -51,11 +60,18 @@ DefaultLinSolverCreator = FactorizeLinSolverCreator
 
 
 function create_linsolver(creator::FactorizeLinSolverCreator,nep,λ)
+    # Let's see if we find it in recycled_factorizations
     if (λ in keys(creator.recycled_factorizations))
         Afact=creator.recycled_factorizations[λ];
         return FactorizeLinSolver(Afact,creator.umfpack_refinements);
     else
-        return FactorizeLinSolver(nep,λ,creator.umfpack_refinements);
+        solver=FactorizeLinSolver(nep,λ,creator.umfpack_refinements);
+        if  (length(keys(creator.recycled_factorizations))
+             < creator.max_factorizations )
+            # Let's save the factorization
+            creator.recycled_factorizations[λ]=solver.Afact;
+        end
+        return solver;
     end
 
 end
