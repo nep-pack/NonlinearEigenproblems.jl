2d2d59bea850b8fcb3e96fc3fbc8063a42e12882
gmele <gmele@kth.se>
2019-09-04 12:09:05 +0200

    minor testing

--- CORK_working_dir/test.jl
+++ CORK_working_dir/test.jl
@@ -1,7 +1,7 @@
-using NonlinearEigenproblems, Random, LinearAlgebra
+using NonlinearEigenproblems, Random, LinearAlgebra, PyPlot
 
 
-nep=nep_gallery("dep1")
+nep=nep_gallery("dep0")
 # iar linearization
 d=5
 M=diagm( 0 =>  ones(d) )[2:end,:]
@@ -9,13 +9,19 @@ N=diagm( -1 =>  1 ./ (1:d-1) )[2:end,:]
 
 Av=Array{AbstractMatrix,1}(undef, d)
 Bv=Array{AbstractMatrix,1}(undef, d)
-for j=1:d
-    Av[j]=compute_Mder(nep,0,j)/j
-end
-Bv[1]=-compute_Mder(nep,0,0)
+
+Av[1]=-compute_Mder(nep,0,0)
 for j=2:d
-    Bv[j]=zero(Av[1])
+    Av[j]=zero(Av[1])
 end
+
+for j=1:d
+    Bv[j]=compute_Mder(nep,0,j)/j
+end
+
 
 cp=CORK_pencil(M,N,Av,Bv)
 AA,BB=build_CORK_pencil(cp)
+
+λ=eigvals(AA,BB)
+plot(real(λ),imag(λ),marker="*",markerfacecolor=:none,c=:black,linestyle=:none)
