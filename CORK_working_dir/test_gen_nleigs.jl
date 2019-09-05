bde4a8b356b64d76c07ee4a5cd221e9ec982f7ce
gmele <gmele@kth.se>
2019-09-05 10:23:22 +0200

    CORK with NLEIGS linearizarion. Better working example.

--- CORK_working_dir/test_gen_nleigs.jl
+++ CORK_working_dir/test_gen_nleigs.jl
@@ -4,9 +4,13 @@ include("extract_nleigs_structure.jl")
 
 nep=nep_gallery("dep0");
 unit_square = float([1+1im, 1-1im, -1-1im,-1+1im])
+
+r=5
+θ=range(0,stop=2π,length=1000)
+Σ=r*cos.(θ) + 1im*r*sin.(θ)
 D, β, ξ, σ=nleigs_structure(nep,unit_square);
 
-d=6
+d=19
 σ=σ[1:d+1]; β=β[1:d+1]; ξ=ξ[1:d+1]
 
 M=diagm( -1 => σ[1:d], 0 =>  β[1:d] )[2:end-1,1:end-1]
@@ -24,6 +28,6 @@ AA,BB=build_CORK_pencil(ck)
 λ2=eigvals(Matrix(AA),Matrix(BB))
 
 λ,_=iar(nep,maxit=200,tol=1e-8,neigs=Inf)
-
+pygui(true)
 plot(real(λ),imag(λ),marker="*",markerfacecolor=:none,c=:red,linestyle=:none)
 plot(real(λ2),imag(λ2),marker="o",markerfacecolor=:none,c=:red,linestyle=:none)
