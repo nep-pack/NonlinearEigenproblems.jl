e4db15e6a7a86f135f7f39f448f49698442569da
gmele <gmele@kth.se>
2019-09-05 10:17:59 +0200

    CORK with NLEIGS linearizarion. First working example.

--- CORK_working_dir/test_gen_nleigs.jl
+++ CORK_working_dir/test_gen_nleigs.jl
@@ -4,4 +4,26 @@ include("extract_nleigs_structure.jl")
 
 nep=nep_gallery("dep0");
 unit_square = float([1+1im, 1-1im, -1-1im,-1+1im])
-D, β, ξ, σ=nleigs_structure(nep,unit_square,);
+D, β, ξ, σ=nleigs_structure(nep,unit_square);
+
+d=6
+σ=σ[1:d+1]; β=β[1:d+1]; ξ=ξ[1:d+1]
+
+M=diagm( -1 => σ[1:d], 0 =>  β[1:d] )[2:end-1,1:end-1]
+Av=Array{AbstractMatrix,1}(undef, d)
+Av[1:d-1]=D[1:d-1]; Av[d]=D[d]-σ[d]/β[d+1]*D[d+1]
+
+
+N=diagm( -1 => ones(d), 0 =>  β[1:d]./ξ[1:d] )[2:end-1,1:end-1]
+Bv=Array{AbstractMatrix,1}(undef, d)
+Bv[1:d-1]=D[1:d-1]/ξ[d+1]; Bv[d]=D[d]/ξ[d+1]-D[d+1]/β[d+1]
+ck=CORK_pencil(M,N,Av,Bv)
+
+AA,BB=build_CORK_pencil(ck)
+
+λ2=eigvals(Matrix(AA),Matrix(BB))
+
+λ,_=iar(nep,maxit=200,tol=1e-8,neigs=Inf)
+
+plot(real(λ),imag(λ),marker="*",markerfacecolor=:none,c=:red,linestyle=:none)
+plot(real(λ2),imag(λ2),marker="o",markerfacecolor=:none,c=:red,linestyle=:none)
