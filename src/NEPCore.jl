51a204c00d0f79458d13922dc13c3e2f076d817f
Emil Ringh <emil.ringh@gmail.com>
2019-09-07 10:13:24 +0200

    doc: #205 as discussed in d52d7806f77f967edbcac8a8c0b1600c9474489e

--- src/NEPCore.jl
+++ src/NEPCore.jl
@@ -47,7 +47,7 @@ size(nep::NEP,d)
 and at least one of the following
 
 * M = [`compute_Mder(nep::NEP,λ::Number,i::Integer=0)`](@ref)
-* V = [`compute_Mlincomb!(nep::NEP,λ::Number,V::AbstractVecOrMat,a::Vector)`](@ref)
+* V = [`compute_Mlincomb(nep::NEP,λ::Number,V::AbstractVecOrMat,a::Vector)`](@ref) (or `compute_Mlincomb!`)
 * MM = [`compute_MM(nep::NEP,S,V)`](@ref)
 
 
