9d09cb5bac9f73170b02b88e60f1cea82e1faad1
Elias Jarlebring <jarlebring@gmail.com>
2019-09-07 15:57:35 +0200

    doc typo

--- src/inner_solver.jl
+++ src/inner_solver.jl
@@ -185,7 +185,7 @@ struct SGIterInnerSolver <: InnerSolver end;
 
 Uses [`contour_beyn`](@ref) to solve the inner problem, with radius and number
 of quadrature nodes, given by `radius` and `n`. If the variable `radius` is set
-to `:auto`, the integration radius will be automatically by
+to `:auto`, the integration radius will be automatically selected by
 using the eigenvalue approximations specified by the outer solver.
 
 See also: [`InnerSolver`](@ref), [`inner_solve`](@ref)
