0d3383581d7bfb56c4a79457e2f33d2c1231b55d
Emil Ringh <eringh@kth.se>
2019-09-10 11:39:37 +0200

    rand: integers can be drawn on an interval #188

--- src/gallery_extra/GalleryRand.jl
+++ src/gallery_extra/GalleryRand.jl
@@ -35,14 +35,14 @@ export reset_rng!
         x::UInt128
         w::UInt128
         s::UInt128
-        function MSWS_RNG(seed::UInt128 = 0x00000000000000000000000000000000)
+        function MSWS_RNG(seed::UInt128 = zero(UInt128))
             this = new();
             reset_rng!(this, seed);
             return this
         end
     end
 
-    reset_rng!() = reset_rng!(GLOBAL_MSWS_RNG, 0x00000000000000000000000000000000);
+    reset_rng!() = reset_rng!(GLOBAL_MSWS_RNG, zero(UInt128));
     reset_rng!(rng::MSWS_RNG, seed::Integer) = reset_rng!(rng, unsigned(Int128(seed)));
     reset_rng!(seed::Integer) = reset_rng!(GLOBAL_MSWS_RNG, seed);
     reset_rng!(seed::UInt128) = reset_rng!(GLOBAL_MSWS_RNG, seed);
@@ -64,20 +64,32 @@ export reset_rng!
         rng.x *= rng.x;
         rng.x += (rng.w += rng.s);
         rng.x = (rng.x>>64) | (rng.x<<64);
-        return UInt64(rng.x & 0xFFFFFFFFFFFFFFFF)
+        return UInt64(rng.x & typemax(UInt64))
     end
 
 
-    gen_rng_int(rng::MSWS_RNG, upper::Integer) = gen_rng_int(rng, unsigned(Int64(upper)));
-    gen_rng_int(upper::Integer) = gen_rng_int(GLOBAL_MSWS_RNG, upper);
-    gen_rng_int(upper::UInt64) = gen_rng_int(GLOBAL_MSWS_RNG, upper)
+    gen_rng_int(lower::Integer, upper::Integer) = gen_rng_int(GLOBAL_MSWS_RNG, lower, upper)
+    function gen_rng_int(rng::MSWS_RNG, lower::Integer, upper::Integer)
+        if lower >= upper
+            error("Lower limit cannot be larger than or equal to upper limit.")
+        elseif lower < 0
+            return signed(gen_rng_int(rng, UInt64(upper-lower))) + lower
+        else
+            return gen_rng_int(rng, UInt64(upper-lower)) + lower
+        end
+    end
+
+
+    gen_rng_int(upper::Unsigned) = gen_rng_int(GLOBAL_MSWS_RNG, upper);
+    gen_rng_int(rng::MSWS_RNG, upper::Unsigned) = gen_rng_int(rng, UInt64(upper));
+    gen_rng_int(upper::UInt64) = gen_rng_int(GLOBAL_MSWS_RNG, lower, upper)
     function gen_rng_int(rng::MSWS_RNG, upper::UInt64)
-        return mod(gen_rng_int(rng), upper)
+        return mod(gen_rng_int(rng), upper+1) #Include the upper limit, but wrong if upper=typemax(UInt64)
     end
 
 
     function gen_rng_float(rng::MSWS_RNG = GLOBAL_MSWS_RNG)
-        return Float64(gen_rng_int(rng)/0xFFFFFFFFFFFFFFFF);
+        return Float64(gen_rng_int(rng)/typemax(UInt64));
     end
 
 
--- test/galleryrand.jl
+++ test/galleryrand.jl
@@ -7,46 +7,78 @@ using LinearAlgebra
 @testset "GalleryRand" begin
 
     @testset "Integers" begin
-    @info "Testing uniformity of integers"
-        reset_rng!()
-        n = 1000000;
-        upp = 97;
-        prob = zeros(Int64,upp); # Avoid zero-indexing
-        for i = 1:n
-            prob[gen_rng_int(upp)+1] += 1;
+        @testset "Interval" begin
+        @info "Testing integers drawn in correct interval"
+            reset_rng!()
+            n = 10000;
+            low = -7; upp = 22;
+            isininterval = true;
+            for i = 1:n
+                a = gen_rng_int(low, upp)
+                isininterval = isininterval && (a>=low) && (a<=upp)
+            end
+            @test isininterval
         end
-        @test iszero(sum(iszero.(prob)))
-        true_prob = n/upp;
-        @test abs(minimum(prob)-true_prob)/abs(true_prob) < 100*1/sqrt(n)
-        @test abs(maximum(prob)-true_prob)/abs(true_prob) < 100*1/sqrt(n)
+
+        @testset "Uniformity" begin
+        @info "Testing uniformity of integers"
+            reset_rng!()
+            n = 1000000;
+            low = 3; upp = 99;
+            prob = zeros(Int64, upp); # Avoid zero-indexing
+            for i = 1:n
+                prob[gen_rng_int(low, upp)] += 1;
+            end
+            @test sum(iszero.(prob)) == low-1
+            prob_int = prob[low:upp];
+            true_prob = n/(upp-low+1);
+            @test abs(minimum(prob_int)-true_prob)/abs(true_prob) < 100*1/sqrt(n)
+            @test abs(maximum(prob_int)-true_prob)/abs(true_prob) < 100*1/sqrt(n)
+        end
     end
 
-    @testset "Float interval" begin
-    @info "Testing uniformity of floats in an interval"
-        reset_rng!()
-        n = 1000000;
-        A = zeros(Float64,n);
-        low = 3.0; upp = 12.0;
-        for i = 1:n
-            A[i] = gen_rng_float(low, upp);
-        end
-        sort!(A);
 
-        x = A;
-        expr_cdf = cumsum(ones(n))./n;
-
-        function uniform_cdf(x)
-            if x < low; return 0.0;
-            elseif (x >= low) && (x <= upp); return (x-low)/(upp-low);
-            else return 1.0; end;
-        end
-        true_cdf = zeros(Float64,n)
-        for i = 1:n
-            true_cdf[i] = uniform_cdf(x[i]);
+    @testset "Floats" begin
+        @testset "Interval" begin
+        @info "Testing floats drawn in correct interval"
+            reset_rng!()
+            n = 10000;
+            low = -7.1; upp = 22;
+            isininterval = true;
+            for i = 1:n
+                a = gen_rng_float(low, upp);
+                isininterval = isininterval && (a>=low) && (a<=upp)
+            end
+            @test isininterval
         end
 
-        @test maximum(abs.(expr_cdf-true_cdf)) < sqrt(1/n)
-        @test A[1]-low < 10/n
-        @test upp-A[n] < 10/n
+        @testset "Uniformity" begin
+        @info "Testing uniformity of floats"
+            reset_rng!()
+            n = 1000000;
+            A = zeros(Float64,n);
+            low = 3.0; upp = 12.0;
+            for i = 1:n
+                A[i] = gen_rng_float(low, upp);
+            end
+            sort!(A);
+
+            x = A;
+            expr_cdf = cumsum(ones(n))./n;
+
+            function uniform_cdf(x)
+                if x < low; return 0.0;
+                elseif (x >= low) && (x <= upp); return (x-low)/(upp-low);
+                else return 1.0; end;
+            end
+            true_cdf = zeros(Float64,n)
+            for i = 1:n
+                true_cdf[i] = uniform_cdf(x[i]);
+            end
+
+            @test maximum(abs.(expr_cdf-true_cdf)) < sqrt(1/n)
+            @test A[1]-low < 10/n
+            @test upp-A[n] < 10/n
+        end
     end
 end
