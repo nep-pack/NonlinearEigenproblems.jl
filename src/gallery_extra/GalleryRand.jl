30d43482ccb54e13eadfd0401d18efe6289e3aa5
Emil Ringh <eringh@kth.se>
2019-09-11 15:10:34 +0200

    rand: more purging

--- src/gallery_extra/GalleryRand.jl
+++ src/gallery_extra/GalleryRand.jl
@@ -34,7 +34,6 @@ export gen_rng_int
 export gen_rng_float
 export gen_rng_mat
 export gen_rng_spmat
-export reset_rng!
 
 
     mutable struct MSWS_RNG
@@ -42,89 +41,54 @@ export reset_rng!
         w::UInt128
         s::UInt128
         function MSWS_RNG(seed::UInt128 = zero(UInt128))
-            this = new()
-            reset_rng!(this, seed)
-            return this
+            base = 0x9ef09a97ac0f9ecaef01c4f2db0958c9
+            s = (seed << 1) + base
+            x = 0x1de568e1a1ca1b593cbf13f7407cf43e
+            w = 0xd4ac5c288559e14a5fafc1b7df9f9e0e
+            return new(x, w, s)
         end
     end
 
-
-    reset_rng!() = reset_rng!(GLOBAL_MSWS_RNG, zero(UInt128))
-    reset_rng!(seed::Integer) = reset_rng!(GLOBAL_MSWS_RNG, seed)
-    reset_rng!(rng::MSWS_RNG, seed::Integer) = reset_rng!(rng, unsigned(Int128(seed)))
-    function reset_rng!(rng::MSWS_RNG, seed::UInt128)
-        base = 0x9ef09a97ac0f9ecaef01c4f2db0958c9
-        rng.s = (seed << 1) + base # Left-shift to make sure seed i even, then add to base which is odd (https://github.com/tidwall/weyl/issues/1)
-        rng.x = 0x1de568e1a1ca1b593cbf13f7407cf43e
-        rng.w = 0xd4ac5c288559e14a5fafc1b7df9f9e0e
-        return
-    end
-
-
-
-    const GLOBAL_MSWS_RNG = MSWS_RNG()
-
-
-
-    function gen_rng_int(rng::MSWS_RNG = GLOBAL_MSWS_RNG)
+    function gen_rng_int(rng::MSWS_RNG)
         rng.x *= rng.x
         rng.x += (rng.w += rng.s)
         rng.x = (rng.x>>64) | (rng.x<<64)
         return UInt64(rng.x & typemax(UInt64))
     end
 
-
-    gen_rng_int(upper::Integer) = gen_rng_int(GLOBAL_MSWS_RNG, upper)
-    gen_rng_int(rng::MSWS_RNG, upper::Integer) = gen_rng_int(rng, UInt64(unsigned(upper)))
-    function gen_rng_int(rng::MSWS_RNG, upper::UInt64)
-        return mod(gen_rng_int(rng), upper+1) #Include the upper limit, but wrong if upper=typemax(UInt64)
-    end
-
-
-    function gen_rng_float(rng::MSWS_RNG = GLOBAL_MSWS_RNG)
+    function gen_rng_float(rng::MSWS_RNG)
         return Float64(gen_rng_int(rng)/typemax(UInt64))
     end
 
-    gen_rng_float(lower::Real, upper::Real) = gen_rng_float(GLOBAL_MSWS_RNG, lower, upper)
-    gen_rng_float(rng::MSWS_RNG, lower::Real, upper::Real) = gen_rng_float(rng, Float64(lower), Float64(upper))
-    function gen_rng_float(rng::MSWS_RNG, lower::Float64, upper::Float64)
-        return upper - (upper-lower)*gen_rng_float(rng)
-    end
-
-
-    gen_rng_mat(n::Int64, m::Int64, lower::Real, upper::Real) = gen_rng_mat(GLOBAL_MSWS_RNG, n, m, lower, upper)
-    function gen_rng_mat(rng::MSWS_RNG, n::Int64, m::Int64, lower::Real, upper::Real)
+    function gen_rng_mat(rng::MSWS_RNG, n::Int64, m::Int64)
         A = zeros(Float64,n,m)
         for c = 1:m
             for r = 1:n
-                A[r,c] = gen_rng_float(rng, lower, upper)
+                A[r,c] = 1-2*gen_rng_float(rng)
             end
         end
         return A
     end
 
-
-    gen_rng_spmat(n::Int64, m::Int64, p::Real, lower::Real, upper::Real) = gen_rng_spmat(GLOBAL_MSWS_RNG, n, m, p, lower, upper)
-    function gen_rng_spmat(rng::MSWS_RNG, n::Int64, m::Int64, p::Real, lower::Real, upper::Real)
+    function gen_rng_spmat(rng::MSWS_RNG, n::Int64, m::Int64, p::Real)
         nonzeros = round(p*m*n)
         dict = Dict{Tuple{Int64,Int64},Float64}()
         for i = 1:nonzeros
-            r = gen_rng_int(rng,n-1)+1
-            c = gen_rng_int(rng,m-1)+1
-            dict[r,c] = gen_rng_float(rng, lower, upper)
+            r = mod(gen_rng_int(rng),n) + 1
+            c = mod(gen_rng_int(rng),m) + 1
+            dict[r,c] = 1-2*gen_rng_float(rng)
         end
         idxes = collect(keys(dict))
-        nonzeros = length(idxes)
-        r = zeros(Int64,nonzeros)
-        c = zeros(Int64,nonzeros)
-        vals = zeros(Float64,nonzeros)
-        for i = 1:nonzeros
+        nnzs = length(idxes)
+        r = zeros(Int64,nnzs)
+        c = zeros(Int64,nnzs)
+        vals = zeros(Float64,nnzs)
+        for i = 1:nnzs
             r[i] = idxes[i][1]
             c[i] = idxes[i][2]
             vals[i] = dict[idxes[i]]
         end
         return sparse(r,c,vals,n,m)
     end
-
 
 end
--- test/galleryrand.jl
+++ test/galleryrand.jl
@@ -6,95 +6,51 @@ using LinearAlgebra
 
 @testset "GalleryRand" begin
 
-    @testset "Integers" begin
-        @testset "Interval" begin
-        @info "Testing integers drawn in correct interval"
-            reset_rng!()
-            n = 100000;
-            upp = 22;
-            isininterval_a = true;
-            for i = 1:n
-                a = gen_rng_int(upp)
-                isininterval_a = isininterval_a && (a<=upp)
-            end
-            @test isininterval_a
-        end
-
-        @testset "Uniformity" begin
-        @info "Testing uniformity of integers"
-            reset_rng!()
-            n = 100000;
-            upp = 96;
-            prob = zeros(Int64, upp+1); # Avoid zero-indexing
-            for i = 1:n
-                prob[gen_rng_int(upp)+1] += 1;
-            end
-            true_prob = n/(upp+1);
-            @test abs(minimum(prob)-true_prob)/abs(true_prob) < 100*1/sqrt(n)
-            @test abs(maximum(prob)-true_prob)/abs(true_prob) < 100*1/sqrt(n)
-        end
-    end
-
-
     @testset "Floats" begin
-        @testset "Interval" begin
-        @info "Testing floats drawn in correct interval"
-            reset_rng!()
-            n = 100000;
-            low = -7.1; upp = 22;
-            isininterval_a = true;
-            for i = 1:n
-                a = gen_rng_float(low, upp);
-                isininterval_a = isininterval_a && (a>=low) && (a<=upp)
-            end
-            @test isininterval_a
-        end
-
         @testset "Uniformity" begin
         @info "Testing uniformity of floats"
-            reset_rng!()
-            n = 100000;
-            A = zeros(Float64,n);
-            low = 3.0; upp = 12.0;
+            my_rng = MSWS_RNG()
+            n = 100000
+            A = zeros(Float64,n)
             for i = 1:n
-                A[i] = gen_rng_float(low, upp);
+                A[i] = gen_rng_float(my_rng)
             end
             sort!(A);
 
             x = A;
-            expr_cdf = cumsum(ones(n))./n;
+            expr_cdf = cumsum(ones(n))./n
 
             function uniform_cdf(x)
-                if x < low; return 0.0;
-                elseif (x >= low) && (x <= upp); return (x-low)/(upp-low);
+                if x < 0; return 0.0;
+                elseif (x >= 0) && (x <= 1); return (x-0)/(1-0);
                 else return 1.0; end;
             end
             true_cdf = zeros(Float64,n)
             for i = 1:n
-                true_cdf[i] = uniform_cdf(x[i]);
+                true_cdf[i] = uniform_cdf(x[i])
             end
 
             @test maximum(abs.(expr_cdf-true_cdf)) < sqrt(1/n)
-            @test A[1]-low < 10/n
-            @test upp-A[n] < 10/n
+            @test A[1] < 10/n
+            @test 1-A[n] < 10/n
         end
     end
 
     @testset "Matrices" begin
         @testset "Dense" begin
         @info "Testing interfaces for dense matrices"
-            reset_rng!()
-            A = gen_rng_mat(4, 5, -3.0, 1);
-            @test size(A) == (4,5)
+            my_rng = MSWS_RNG()
+            A = gen_rng_mat(my_rng, 40, 50)
+            @test size(A) == (40,50)
             @test eltype(A) == Float64
             @test !issparse(A)
         end
 
         @testset "Sparse" begin
         @info "Testing interfaces for sparse matrices"
-            reset_rng!()
-            A = gen_rng_spmat(4, 5, 0.4, -3.0, 1);
-            @test size(A) == (4,5)
+            my_rng = MSWS_RNG()
+            A = gen_rng_spmat(my_rng, 40, 50, 0.4)
+            @test size(A) == (40,50)
             @test eltype(A) == Float64
             @test issparse(A)
         end
