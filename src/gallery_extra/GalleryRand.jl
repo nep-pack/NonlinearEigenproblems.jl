0fa7ef46a41832ebf79fe6c4ebd2d5f8bd54cf46
Emil Ringh <eringh@kth.se>
2019-09-11 13:41:35 +0200

    rand: purging of code #188

--- src/gallery_extra/GalleryRand.jl
+++ src/gallery_extra/GalleryRand.jl
@@ -11,31 +11,27 @@ B. Widynski, Middle Square Weyl Sequence RNG, arXiv 1704.00358
         w::UInt64
         s::UInt64
         function MSWS_RNG_32()
-            #this = new(0,0,0xb5ad4eceda1ce2a9);
-            this = new(0,0,0x0000000100000001);
+            #this = new(0,0,0xb5ad4eceda1ce2a9)
+            this = new(0,0,0x0000000100000001)
             return this
         end
     end
 
     function gen_rng_int(rng::MSWS_RNG_32)
-        rng.x *= rng.x;
-        rng.x += (rng.w += rng.s);
-        rng.x = (rng.x>>32) | (rng.x<<32);
+        rng.x *= rng.x
+        rng.x += (rng.w += rng.s)
+        rng.x = (rng.x>>32) | (rng.x<<32)
         return UInt32(rng.x & 0xFFFFFFFF)
     end
 """
 module GalleryRand
 
-
 using LinearAlgebra
 using SparseArrays
 
-
 export MSWS_RNG
-export gen_rng
 export gen_rng_int
 export gen_rng_float
-export gen_rng_complex
 export gen_rng_mat
 export gen_rng_spmat
 export reset_rng!
@@ -46,141 +42,82 @@ export reset_rng!
         w::UInt128
         s::UInt128
         function MSWS_RNG(seed::UInt128 = zero(UInt128))
-            this = new();
-            reset_rng!(this, seed);
+            this = new()
+            reset_rng!(this, seed)
             return this
         end
     end
 
 
-    reset_rng!() = reset_rng!(GLOBAL_MSWS_RNG, zero(UInt128));
-    reset_rng!(rng::MSWS_RNG, seed::Integer) = reset_rng!(rng, unsigned(Int128(seed)));
-    reset_rng!(seed::Integer) = reset_rng!(GLOBAL_MSWS_RNG, seed);
+    reset_rng!() = reset_rng!(GLOBAL_MSWS_RNG, zero(UInt128))
+    reset_rng!(seed::Integer) = reset_rng!(GLOBAL_MSWS_RNG, seed)
+    reset_rng!(rng::MSWS_RNG, seed::Integer) = reset_rng!(rng, unsigned(Int128(seed)))
     function reset_rng!(rng::MSWS_RNG, seed::UInt128)
-        base = 0x9ef09a97ac0f9ecaef01c4f2db0958c9; # Found by a random draw
-        rng.s = (seed << 1) + base; # Left-shift to make sure seed i even, then add to base which is odd (https://github.com/tidwall/weyl/issues/1)
-        rng.x = 0x1de568e1a1ca1b593cbf13f7407cf43e;
-        rng.w = 0xd4ac5c288559e14a5fafc1b7df9f9e0e;
+        base = 0x9ef09a97ac0f9ecaef01c4f2db0958c9
+        rng.s = (seed << 1) + base # Left-shift to make sure seed i even, then add to base which is odd (https://github.com/tidwall/weyl/issues/1)
+        rng.x = 0x1de568e1a1ca1b593cbf13f7407cf43e
+        rng.w = 0xd4ac5c288559e14a5fafc1b7df9f9e0e
         return
     end
 
 
 
-    const GLOBAL_MSWS_RNG = MSWS_RNG();
+    const GLOBAL_MSWS_RNG = MSWS_RNG()
 
 
 
     function gen_rng_int(rng::MSWS_RNG = GLOBAL_MSWS_RNG)
-        rng.x *= rng.x;
-        rng.x += (rng.w += rng.s);
-        rng.x = (rng.x>>64) | (rng.x<<64);
+        rng.x *= rng.x
+        rng.x += (rng.w += rng.s)
+        rng.x = (rng.x>>64) | (rng.x<<64)
         return UInt64(rng.x & typemax(UInt64))
     end
 
 
-    gen_rng_int(lower::Integer, upper::Integer) = gen_rng_int(GLOBAL_MSWS_RNG, lower, upper)
-    function gen_rng_int(rng::MSWS_RNG, lower::Integer, upper::Integer)
-        if lower >= upper
-            error("Lower limit cannot be larger than or equal to upper limit.")
-        elseif lower < 0
-            return signed(gen_rng_int(rng, UInt64(upper-lower))) + lower
-        else
-            return gen_rng_int(rng, UInt64(upper-lower)) + lower
-        end
-    end
-
-
-    gen_rng_int(upper::Integer) = gen_rng_int(GLOBAL_MSWS_RNG, UInt64(unsigned(upper)));
-    gen_rng_int(rng::MSWS_RNG, upper::Integer) = gen_rng_int(rng, UInt64(unsigned(upper)));
-    gen_rng_int(upper::Unsigned) = gen_rng_int(GLOBAL_MSWS_RNG, upper);
-    gen_rng_int(rng::MSWS_RNG, upper::Unsigned) = gen_rng_int(rng, UInt64(upper));
-    gen_rng_int(upper::UInt64) = gen_rng_int(GLOBAL_MSWS_RNG, lower, upper)
+    gen_rng_int(upper::Integer) = gen_rng_int(GLOBAL_MSWS_RNG, upper)
+    gen_rng_int(rng::MSWS_RNG, upper::Integer) = gen_rng_int(rng, UInt64(unsigned(upper)))
     function gen_rng_int(rng::MSWS_RNG, upper::UInt64)
         return mod(gen_rng_int(rng), upper+1) #Include the upper limit, but wrong if upper=typemax(UInt64)
     end
 
 
     function gen_rng_float(rng::MSWS_RNG = GLOBAL_MSWS_RNG)
-        return Float64(gen_rng_int(rng)/typemax(UInt64));
+        return Float64(gen_rng_int(rng)/typemax(UInt64))
     end
 
-
-    gen_rng_float(lower::Real, upper::Real) = gen_rng_float(GLOBAL_MSWS_RNG, Float64(lower), Float64(upper))
-    gen_rng_float(lower::Float64, upper::Float64) = gen_rng_float(GLOBAL_MSWS_RNG, lower, upper)
+    gen_rng_float(lower::Real, upper::Real) = gen_rng_float(GLOBAL_MSWS_RNG, lower, upper)
     gen_rng_float(rng::MSWS_RNG, lower::Real, upper::Real) = gen_rng_float(rng, Float64(lower), Float64(upper))
     function gen_rng_float(rng::MSWS_RNG, lower::Float64, upper::Float64)
-        return upper - (upper-lower)*gen_rng_float(rng);
+        return upper - (upper-lower)*gen_rng_float(rng)
     end
 
 
-    gen_rng_complex(lower::Real, upper::Complex) = gen_rng_complex(GLOBAL_MSWS_RNG, ComplexF64(lower), ComplexF64(upper))
-    gen_rng_complex(lower::Complex, upper::Real) = gen_rng_complex(GLOBAL_MSWS_RNG, ComplexF64(lower), ComplexF64(upper))
-    gen_rng_complex(lower::Complex, upper::Complex) = gen_rng_complex(GLOBAL_MSWS_RNG, ComplexF64(lower), ComplexF64(upper))
-    gen_rng_complex(lower::ComplexF64, upper::ComplexF64) = gen_rng_complex(GLOBAL_MSWS_RNG, lower, upper)
-    gen_rng_complex(rng::MSWS_RNG, lower::Real, upper::Complex) = gen_rng_complex(rng, ComplexF64(lower), ComplexF64(upper))
-    gen_rng_complex(rng::MSWS_RNG, lower::Complex, upper::Real) = gen_rng_complex(rng, ComplexF64(lower), ComplexF64(upper))
-    gen_rng_complex(rng::MSWS_RNG, lower::Complex, upper::Complex) = gen_rng_complex(rng, ComplexF64(lower), ComplexF64(upper))
-    function gen_rng_complex(rng::MSWS_RNG, lower::ComplexF64, upper::ComplexF64)
-        # Defines a rectange in complex plane from which we draw real and imag uniformly
-        return gen_rng_float(rng, real(lower), real(upper)) + one(ComplexF64)im*gen_rng_float(rng, imag(lower), imag(upper))
-    end
-
-
-    gen_rng(lower::Tl,upper::Tu) where {Tl,Tu} = gen_rng(Tl,GLOBAL_MSWS_RNG,lower,upper)
-    gen_rng(::Type{T},lower::Tl,upper::Tu) where {T,Tl,Tu} = gen_rng(T,GLOBAL_MSWS_RNG,lower,upper)
-    gen_rng(::Type{Int64},rng::MSWS_RNG,lower::Int64,upper::Int64) = gen_rng_int(rng,lower,upper)
-    gen_rng(::Type{Float64},rng::MSWS_RNG,lower::Float64,upper::Float64) = gen_rng_float(rng,lower,upper)
-    gen_rng(::Type{ComplexF64},rng::MSWS_RNG,lower::ComplexF64,upper::ComplexF64) = gen_rng_complex(rng,lower,upper)
-    function gen_rng(::Type{T},rng::MSWS_RNG,lower::T,upper::T) where T
-        error("This should not happen. Not implemented for type ", T)
-    end
-    function gen_rng(::Type{T},rng::MSWS_RNG,lower::Tl,upper::Tu) where {T,Tl,Tu}
-        TT = promote_type(Tl,Tu)
-        if (TT<:Complex); TT = ComplexF64; end;
-        gen_rng(TT, rng, TT(lower), TT(upper))
-    end
-
-
-    gen_rng_mat(n::Int64, m::Int64, lower, upper) = gen_rng_mat(GLOBAL_MSWS_RNG, n, m, lower, upper)
-    gen_rng_mat(rng::MSWS_RNG, n::Int64, lower, upper) = gen_rng_mat(rng, n, n, lower, upper)
-    gen_rng_mat(n::Int64, lower, upper) = gen_rng_mat(GLOBAL_MSWS_RNG, n, n, lower, upper)
-    function gen_rng_mat(rng::MSWS_RNG, n::Int64, m::Int64, lower::Tl, upper::Tu) where Tl where Tu
-        T = promote_type(Tl,Tu)
-        if (T<:Complex); T = ComplexF64; end;
-        gen_rng_mat(rng, n, m, T(lower), T(upper))
-    end
-    function gen_rng_mat(rng::MSWS_RNG, n::Int64, m::Int64, lower::T, upper::T) where T <: Union{Int64,Float64,ComplexF64}
-        A = zeros(T,n,m)
+    gen_rng_mat(n::Int64, m::Int64, lower::Real, upper::Real) = gen_rng_mat(GLOBAL_MSWS_RNG, n, m, lower, upper)
+    function gen_rng_mat(rng::MSWS_RNG, n::Int64, m::Int64, lower::Real, upper::Real)
+        A = zeros(Float64,n,m)
         for c = 1:m
             for r = 1:n
-                A[r,c] = gen_rng(T,rng,lower,upper)
+                A[r,c] = gen_rng_float(rng, lower, upper)
             end
         end
         return A
     end
 
 
-    gen_rng_spmat(n::Int64, m::Int64, p::Real, lower, upper) = gen_rng_spmat(GLOBAL_MSWS_RNG, n, m, p, lower, upper)
-    gen_rng_spmat(rng::MSWS_RNG, n::Int64, p::Real, lower, upper) = gen_rng_spmat(rng, n, n, p, lower, upper)
-    gen_rng_spmat(n::Int64, p::Real, lower, upper) = gen_rng_spmat(GLOBAL_MSWS_RNG, n, n, p, lower, upper)
-    function gen_rng_spmat(rng::MSWS_RNG, n::Int64, m::Int64, p::Real, lower::Tl, upper::Tu) where Tl where Tu
-        T = promote_type(Tl,Tu)
-        if (T<:Complex); T = ComplexF64; end;
-        gen_rng_spmat(rng, n, m, p, T(lower), T(upper))
-    end
-    function gen_rng_spmat(rng::MSWS_RNG, n::Int64, m::Int64, p::Real, lower::T, upper::T) where T <: Union{Int64,Float64,ComplexF64}
+    gen_rng_spmat(n::Int64, m::Int64, p::Real, lower::Real, upper::Real) = gen_rng_spmat(GLOBAL_MSWS_RNG, n, m, p, lower, upper)
+    function gen_rng_spmat(rng::MSWS_RNG, n::Int64, m::Int64, p::Real, lower::Real, upper::Real)
         nonzeros = round(p*m*n)
-        dict = Dict{Tuple{Int64,Int64},T}()
+        dict = Dict{Tuple{Int64,Int64},Float64}()
         for i = 1:nonzeros
             r = gen_rng_int(rng,n-1)+1
             c = gen_rng_int(rng,m-1)+1
-            dict[r,c] = gen_rng(T,rng,lower,upper)
+            dict[r,c] = gen_rng_float(rng, lower, upper)
         end
         idxes = collect(keys(dict))
         nonzeros = length(idxes)
         r = zeros(Int64,nonzeros)
         c = zeros(Int64,nonzeros)
-        vals = zeros(T,nonzeros)
+        vals = zeros(Float64,nonzeros)
         for i = 1:nonzeros
             r[i] = idxes[i][1]
             c[i] = idxes[i][2]
--- test/galleryrand.jl
+++ test/galleryrand.jl
@@ -11,33 +11,27 @@ using LinearAlgebra
         @info "Testing integers drawn in correct interval"
             reset_rng!()
             n = 100000;
-            low = -7; upp = 22;
+            upp = 22;
             isininterval_a = true;
-            isininterval_b = true;
             for i = 1:n
-                a = gen_rng_int(low, upp)
-                b = gen_rng(low, upp)
-                isininterval_a = isininterval_a && (a>=low) && (a<=upp)
-                isininterval_b = isininterval_b && (b>=low) && (b<=upp)
+                a = gen_rng_int(upp)
+                isininterval_a = isininterval_a && (a<=upp)
             end
             @test isininterval_a
-            @test isininterval_b
         end
 
         @testset "Uniformity" begin
         @info "Testing uniformity of integers"
             reset_rng!()
-            n = 1000000;
-            low = 3; upp = 99;
-            prob = zeros(Int64, upp); # Avoid zero-indexing
+            n = 100000;
+            upp = 96;
+            prob = zeros(Int64, upp+1); # Avoid zero-indexing
             for i = 1:n
-                prob[gen_rng_int(low, upp)] += 1;
+                prob[gen_rng_int(upp)+1] += 1;
             end
-            @test sum(iszero.(prob)) == low-1
-            prob_int = prob[low:upp];
-            true_prob = n/(upp-low+1);
-            @test abs(minimum(prob_int)-true_prob)/abs(true_prob) < 100*1/sqrt(n)
-            @test abs(maximum(prob_int)-true_prob)/abs(true_prob) < 100*1/sqrt(n)
+            true_prob = n/(upp+1);
+            @test abs(minimum(prob)-true_prob)/abs(true_prob) < 100*1/sqrt(n)
+            @test abs(maximum(prob)-true_prob)/abs(true_prob) < 100*1/sqrt(n)
         end
     end
 
@@ -49,21 +43,17 @@ using LinearAlgebra
             n = 100000;
             low = -7.1; upp = 22;
             isininterval_a = true;
-            isininterval_b = true;
             for i = 1:n
                 a = gen_rng_float(low, upp);
-                b = gen_rng(low, upp)
                 isininterval_a = isininterval_a && (a>=low) && (a<=upp)
-                isininterval_b = isininterval_b && (b>=low) && (b<=upp)
             end
             @test isininterval_a
-            @test isininterval_b
         end
 
         @testset "Uniformity" begin
         @info "Testing uniformity of floats"
             reset_rng!()
-            n = 1000000;
+            n = 100000;
             A = zeros(Float64,n);
             low = 3.0; upp = 12.0;
             for i = 1:n
@@ -90,88 +80,22 @@ using LinearAlgebra
         end
     end
 
-    @testset "Complex" begin
-        @testset "Interval" begin
-        @info "Testing complex drawn in correct interval"
-            reset_rng!()
-            n = 100000;
-            real_low = -7.1;
-            real_upp = 2.9;
-            imag_low = -4.0;
-            imag_upp = 1;
-            a = real_low + imag_upp*1im;
-            b = real_upp + imag_low*1im;
-            isininterval_c = true;
-            isininterval_d = true;
-            for i = 1:n
-                c = gen_rng_complex(a, b);
-                d = gen_rng(a, b);
-                isininterval_c = isininterval_c && (real(c)>=real_low) && (real(c)<=real_upp) &&
-                                                   (imag(c)>=imag_low) && (imag(c)<=imag_upp)
-                isininterval_d = isininterval_d && (real(d)>=real_low) && (real(d)<=real_upp) &&
-                                                   (imag(d)>=imag_low) && (imag(d)<=imag_upp)
-            end
-            @test isininterval_c
-            @test isininterval_d
-        end
-    end
-
     @testset "Matrices" begin
         @testset "Dense" begin
         @info "Testing interfaces for dense matrices"
             reset_rng!()
-
-            A = gen_rng_mat(4, 5, -3, 0);
-            @test size(A) == (4,5)
-            @test eltype(A) == Int64
-            A = gen_rng_mat(4, -1, 3);
-            @test size(A) == (4,4)
-            @test eltype(A) == Int64
-
-            A = gen_rng_mat(4, 5, -3.0, 0);
+            A = gen_rng_mat(4, 5, -3.0, 1);
             @test size(A) == (4,5)
             @test eltype(A) == Float64
-            A = gen_rng_mat(4, -1, 3.1);
-            @test size(A) == (4,4)
-            @test eltype(A) == Float64
-
-            A = gen_rng_mat(3, 7, -3+1im, 1);
-            @test size(A) == (3,7)
-            @test eltype(A) == ComplexF64
-            A = gen_rng_mat(3, -3+1im, 1.0-1im);
-            @test size(A) == (3,3)
-            @test eltype(A) == ComplexF64
+            @test !issparse(A)
         end
 
         @testset "Sparse" begin
         @info "Testing interfaces for sparse matrices"
             reset_rng!()
-
-            A = gen_rng_spmat(4, 5, 0.4, -3, 0);
-            @test size(A) == (4,5)
-            @test eltype(A) == Int64
-            @test issparse(A)
-            A = gen_rng_spmat(4, 0.4, -1, 3);
-            @test size(A) == (4,4)
-            @test eltype(A) == Int64
-            @test issparse(A)
-
-            A = gen_rng_spmat(4, 5, 0.4, -3.0, 0);
+            A = gen_rng_spmat(4, 5, 0.4, -3.0, 1);
             @test size(A) == (4,5)
             @test eltype(A) == Float64
-            @test issparse(A)
-            A = gen_rng_spmat(4, 0.4, -1, 3.1);
-            @test size(A) == (4,4)
-            @test eltype(A) == Float64
-            @test issparse(A)
-
-            A = gen_rng_spmat(3, 7, 0.4, -3+1im, 1);
-            @test size(A) == (3,7)
-            @test eltype(A) == ComplexF64
-            @test issparse(A)
-            A = gen_rng_spmat(3, 0.4, -3+1im, 1.0-1im);
-            @test size(A) == (3,3)
-            @test eltype(A) == ComplexF64
             @test issparse(A)
         end
     end
