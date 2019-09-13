7c33d3f0833f34f56464e232a09a35b2edfcfc9d
Elias Jarlebring <jarlebring@gmail.com>
2019-09-13 15:15:39 +0200

    speed up bem unit test

--- test/gallery.jl
+++ test/gallery.jl
@@ -177,14 +177,14 @@ using Test
 
 
     @info "testing bem_fichera"
-    nep=nep_gallery("bem_fichera",2);
+    nep=nep_gallery("bem_fichera",1);
 
     λ_ref=8.790558462139456 - 0.010815457827738698im
     M=compute_Mder(nep,λ_ref);
     @test rank(M)<size(M,1)
     @onlybench @testset "bem_fichera + IAR" begin
        v=ones(size(nep,1));
-       (λ,vv)=iar(nep,σ=9,v=v,logger=displaylevel,neigs=4,maxit=50,tol=1e-6) # normally takes 30 iterations
+       (λ,vv)=iar(nep,σ=9,v=v,logger=displaylevel,neigs=1,maxit=50,tol=1e-6) # normally takes 30 iterations
        @test norm(compute_Mlincomb(nep,λ[1],vv[:,1]))<sqrt(eps());
     end
     @info "non-existing example"
