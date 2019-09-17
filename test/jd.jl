f6ad8848b842d7219e961ddf3f0f0564d669b5e9
Emil Ringh <eringh@kth.se>
2019-09-17 10:24:20 +0200

    JD test: reset seed #222

--- test/jd.jl
+++ test/jd.jl
@@ -21,7 +21,7 @@ Dc,Vc = polyeig(nep,DefaultEigSolver)
 c = sortperm(abs.(Dc))
 verify_lambdas(2, nep, λ, u, TOL)
 
-
+Random.seed!(0);
 @info "Testing SG as inner solver"
 nep = nep_gallery("real_quadratic")
 nep = SPMF_NEP(get_Av(nep), get_fv(nep))
@@ -29,7 +29,7 @@ TOL = 1e-10;
 λ,u=jd_betcke(Float64, nep, tol=TOL, maxit=4, logger = displaylevel, projtype = :Galerkin, inner_solver_method = SGIterInnerSolver(), v=ones(size(nep,1)), λ=0)
 verify_lambdas(1, nep, λ, u, TOL)
 
-
+Random.seed!(0);
 @info "Testing IAR Cheb as projected solver"
 nep = nep_gallery("dep0_sparse",40)
 TOL = 1e-10;
@@ -38,6 +38,7 @@ verify_lambdas(1, nep, λ, u, TOL)
 λ0 = λ[1] # store these for the next test
 v0 = vec(u)
 
+Random.seed!(0);
 @info "Testing convergence before starting"
 λ,u=jd_betcke(nep, tol=TOL, maxit=25, neigs=1, logger=displaylevel, λ=λ0, v=v0)
 verify_lambdas(1, nep, λ, u, TOL)
@@ -61,12 +62,13 @@ end
 @bench @testset "Effenberger" begin
 @info "Test: Effenberger"
 
-    Random.seed!(0);
-    TOL = 1e-10
+Random.seed!(0);
+TOL = 1e-10
 nep = nep_gallery("pep0",250)
 λ, u = jd_effenberger(nep, neigs=5, logger=displaylevel, tol=TOL, maxit=80, λ=0.82+0.9im, v=ones(ComplexF64,size(nep,1)))
 verify_lambdas(5, nep, λ, u, TOL)
 
+Random.seed!(0);
 TOL = 1e-10
 nep = nep_gallery("dep0",60)
 λ, u = jd_effenberger(nep, neigs=3, logger=displaylevel, tol=TOL, maxit=55, λ=0.6+0im, v=ones(ComplexF64,size(nep,1)))#, inner_solver_method = IARChebInnerSolver())
