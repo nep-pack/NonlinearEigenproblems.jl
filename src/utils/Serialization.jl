ba44630f71f38d11ae00019e9e7eaaf5d08a6551
Emil Ringh <eringh@kth.se>
2019-09-26 09:36:09 +0200

    Reading and writing of sparse matrices Int64 #225

--- src/utils/Serialization.jl
+++ src/utils/Serialization.jl
@@ -5,18 +5,19 @@ module Serialization
     export read_sparse_matrix
 
     # Writes the sparse matrix M in text format to a file with the given name
-    function write_sparse_matrix(filename, M)
+    function write_sparse_matrix(filename, M, T=Int64)
         open(filename, "w") do f
-            write(f, "$(size(M, 1))\n$(size(M, 2))\n")
+            write(f, "$(T(size(M, 1)))\n$(T(size(M, 2)))\n")
             nz = findnz(M)
-            for n = 1:3
-                foreach(x -> write(f, "$x\n"), nz[n])
+            for n = 1:2
+                foreach(x -> write(f, "$(T(x))\n"), nz[n])
             end
+            foreach(x -> write(f, "$x\n"), nz[3])
         end
     end
 
     # Reads and returns a sparse matrix written by write_sparse_matrix
-    function read_sparse_matrix(filename, T=Int)
+    function read_sparse_matrix(filename, T=Int64)
         data = open(filename) do f
             readlines(f)
         end
