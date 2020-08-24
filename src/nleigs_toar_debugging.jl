71dbeedb5599211d6bdd11bd240a8d960fe632bb
Elias Jarlebring <jarlebring@gmail.com>
2020-08-24 10:20:06 +0200

    Major commit adding nleigs toar solver

---
+++ src/nleigs_toar_debugging.jl
@@ -0,0 +1,112 @@
+
+
+
+macro status_rkcont1()
+end
+
+macro status_rkcont2()
+end
+macro status_rkcont3()
+end
+macro status_extend1()
+end
+macro status_extend2()
+end
+macro status_extend3()
+end
+macro status_extend4()
+end
+macro status_run1()
+end
+macro status_run2()
+end
+macro status_run3()
+end
+macro status_run4()
+end
+macro status_run5()
+end
+macro status_run6()
+end
+macro status_run7()
+end
+macro status_nleigs_toar0()
+end
+macro status_nleigs_toar1()
+end
+macro status_nleigs_toar2()
+end
+macro status_nleigs_toar3()
+end
+
+
+macro status_nleigs_toar4()
+end
+
+
+macro status_nleigs_toar5()
+end
+macro status_nleigs_toar6()
+end
+macro status_nleigs_toar7()
+end
+macro status_nleigs_toar8()
+end
+macro status_nleigs_toar9()
+end
+macro status_nleigs_toar10()
+end
+macro status_nleigs_toar11()
+end
+macro status_nleigs_toar12()
+end
+macro status_nleigs_toar13()
+end
+macro status_nleigs_toar14()
+end
+macro status_compress0()
+end
+macro status_compress1()
+end
+
+macro status_compress2()
+end
+macro status_compress3()
+end
+macro status_compress4()
+end
+macro status_compress5()
+end
+
+macro status_compress6()
+end
+macro status_compress7()
+end
+macro status_compress8()
+end
+macro status_compress9()
+end
+macro status_compress10()
+end
+macro status_compress11()
+end
+macro status_compress12()
+end
+macro status_compress13()
+end
+macro status_compress14()
+end
+
+
+macro status_lejabagby0()
+end
+macro status_lejabagby1()
+end
+macro status_lejabagby2()
+end
+macro status_lejabagby3()
+end
+
+
+macro status_lejabagby4()
+end
