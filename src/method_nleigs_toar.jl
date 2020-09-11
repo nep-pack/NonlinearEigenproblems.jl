c2050848fe79018fa534cc47836a352e85614ff6
Elias Jarlebring <jarlebring@gmail.com>
2020-09-11 10:18:11 +0200

    n not defined

--- src/method_nleigs_toar.jl
+++ src/method_nleigs_toar.jl
@@ -366,7 +366,7 @@ function nleigs_toar(nep,rg;
 
     # Pick out the eigenvectors
     kk=size(Vtensor.U.active_mat,2)
-    pp=Int(ceil(size(Vtensor.U.mat,1)*Vtensor.d/n));
+    pp=Int(ceil(size(Vtensor.U.mat,1)*Vtensor.d/size(nep,1)));
     II2=Matrix{Float64}(I,pp,pp);
     VV=(kron(II2,Vtensor.U.active_mat)*Vtensor.S[1:(pp*kk),1:nconv])[1:n,:]
 
