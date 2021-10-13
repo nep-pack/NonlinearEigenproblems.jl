00228ba7db6704e609f47d634af1ebb7cf1e4805
Emil Ringh <eringh@users.noreply.github.com>
2021-10-13 23:34:53 +0200

    Docs

--- src/LinSolverCreators.jl
+++ src/LinSolverCreators.jl
@@ -147,8 +147,20 @@ end
 
 This is the creator for case of a deflated NEP.
 The argument `orglinsolvercreator` is the `LinSolverCreator` for the original NEP.
-This solver will be reused in the solver, and hence pre-computed entities such as, e.g.,
+The extended linear system
+
+```math
+[M, U; X^T, 0][v1; v2] = [b1; b2]
+```
+
+is solved with a Schur complement strategy, recycling
+the linear solver of the original NEP. Hence, pre-computed entities such as, e.g.,
 factorizations and preconditioners can be reused.
+
+
+NB1: The implementation assumes minimality index = 1.
+NB2: The Schur complement is explicitly formed. Hence it is only efficient for a
+few deflated eigenvalues.
 
 See also: [`DeflatedNEPLinSolver`](@ref), [`create_linsolver`](@ref),
 [`deflate_eigpair`](@ref)
--- src/LinSolvers.jl
+++ src/LinSolvers.jl
@@ -200,23 +200,10 @@ See also: [`LinSolver`](@ref), [`GMRESLinSolverCreator`](@ref)
 """
     struct DeflatedNEPLinSolver <: LinSolver
 
-The `DeflatedNEPLinSolver` solves the linear system connected with a deflated NEP.
-The extended linear system is solveed with a Schur complement strategy, recycling
-the linear solver of the original NEP. such that
-
-```math
-[M, U; X^T, 0][v1; v2] = [b1; b2]
-```
-
-NB1: The implementation assumes minimality index = 1.
-NB2: The Schur complement is explicitly formed. Hence it is only efficient for a
-few deflated eigenvalues.
+This represents a solver for a deflated NEP.
 
 See also: [`LinSolver`](@ref), [`DeflatedNEPLinSolverCreator`](@ref),
 [`deflate_eigpair`](@ref)
-
-# References
-* C. Effenberger, Robust successive computation of eigenpairs for nonlinear eigenvalue problems. SIAM J. Matrix Anal. Appl. 34, 3 (2013), pp. 1231-1256.
 """
         struct DeflatedNEPLinSolver{T_NEP, T_num, T_LinSolver} <: LinSolver
             deflated_nep::T_NEP
