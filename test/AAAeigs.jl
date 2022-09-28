f05c87c6707fc28021af2b35cf349e18876fb8cd
Elias Jarlebring <jarlebring@gmail.com>
2022-09-28 16:21:08 +0200

    unit test for AAAeigs

---
+++ test/AAAeigs.jl
@@ -0,0 +1,23 @@
+using NonlinearEigenproblems
+using Test
+using LinearAlgebra
+using IterativeSolvers
+@testset "AAAeigs" begin
+    dep=nep_gallery("dep0");
+    circ=exp.(1im*(0:0.01:2)*pi); circ=circ[1:end-1];
+    Z=2*circ;
+
+    # Test general nonlinear problem
+    (λ,v)=AAAeigs(nep,Z);
+    @test length(λ) == 6 # This problem has 6 eigvals smaller than 2.0
+    @test count(map(i -> norm(compute_Mlincomb(nep,λ[i],normalize(v[:,i]))),1:6) .< sqrt(eps())) == 6
+
+    # Test SumNEP
+    Av=get_Av(nep);
+    nep2=SumNEP(PEP([Av[2],-Av[1]]),SPMF_NEP([Av[3]],[s->exp(-s)]));
+    (λ,v)=AAAeigs(nep2,Z);
+    @test length(λ) == 6
+    @test count(map(i -> norm(compute_Mlincomb(nep,λ[i],normalize(v[:,i]))),1:6) .< sqrt(eps())) == 6
+
+
+end
