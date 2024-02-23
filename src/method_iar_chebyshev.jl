7f2bf214621c8880847b97caf53e9e3daa8a61dd
Aditya Mahajan <aditya.mahajan@mcgill.ca>
2024-02-23 22:12:38 +0100

    Fix typo in documentation
    A minor error in the documentation

--- src/method_iar_chebyshev.jl
+++ src/method_iar_chebyshev.jl
@@ -372,7 +372,7 @@ Computes the vector y0 used in [`iar_chebyshev`](@ref) defined as
 ```math
  y_0 =\\left( \\sum_{i=0}^{N-1} B \\left( \\frac{d}{d \\theta} \\right) \\hat T_i(\\theta) x_i \\right)(0) - \\sum_{i=0}^{N} T_i(c) y_i
 ```
-where \$T_i\$ is the i-th Chebyshev polynomial of the first kind, \$ \\ hat T_i\$ is the i-th Chebyshev polynomial of the first kind for the interval [a,b]. For a generic `nep`, this quantity is computed by converting polynomials in monomial basis. This procedure may be numerical unstable if many iterations are required. If for the specific `nep` a closed formula is available, we suggest to overload this function.
+where \$T_i\$ is the i-th Chebyshev polynomial of the first kind, \$ \\hat T_i\$ is the i-th Chebyshev polynomial of the first kind for the interval [a,b]. For a generic `nep`, this quantity is computed by converting polynomials in monomial basis. This procedure may be numerical unstable if many iterations are required. If for the specific `nep` a closed formula is available, we suggest to overload this function.
 """
 function compute_y0_cheb(T,nep::NEPTypes.NEP,::Type{ComputeY0Cheb},X,Y,M0inv,precomp::AbstractPrecomputeData)
     # compute_y0_cheb computes y0 for the NEP
